7d0005916d5b49d520c6fed9f88d4b239065067b
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-03-10 11:13:07 -0400

    added a default unit system

---
+++ streamteam/units.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+import logging
+
+# Third-party
+import astropy.units as u
+
+# Create logger
+logger = logging.getLogger(__name__)
+
+# default unit system
+usys = dict()
+usys['length'] = u.kpc
+usys['speed'] = u.km/u.s
+usys['time'] = u.Myr
+usys['angular speed'] = u.mas/u.yr
+usys['angle'] = u.degree
+usys['mass'] = u.M_sun
