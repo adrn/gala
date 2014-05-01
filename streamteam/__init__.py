1ebee16e78d76fecc7d84bb596f15e3548cb4556
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-05-01 12:36:18 -0400

    monkey patch quantity

---
+++ streamteam/__init__.py
@@ -0,0 +1,14 @@
+# Monkey-patch Quantity
+import astropy.units as u
+
+@classmethod
+def _parse_quantity(cls, q):
+    try:
+        val,unit = q.split()
+    except AttributeError, ValueError:
+        val = q
+        unit = u.dimensionless_unscaled
+
+    return u.Quantity(float(val), unit)
+
+u.Quantity.from_string = _parse_quantity
