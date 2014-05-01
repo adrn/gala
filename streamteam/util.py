0e806c3cb054006649e6e94fad5c28f326c48a62
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-05-01 11:31:53 -0400

    pool

---
+++ streamteam/util.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+
+""" General utilities. """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+import logging
+
+__all__ = ['get_pool']
+
+# Create logger
+logger = logging.getLogger(__name__)
+
+def get_pool(mpi=False, threads=None):
+    """ Get a pool object to pass to emcee for parallel processing.
+        If mpi is False and threads is None, pool is None.
+
+        Parameters
+        ----------
+        mpi : bool
+            Use MPI or not. If specified, ignores the threads kwarg.
+        threads : int (optional)
+            If mpi is False and threads is specified, use a Python
+            multiprocessing pool with the specified number of threads.
+    """
+
+    if mpi:
+        from emcee.utils import MPIPool
+
+        # Initialize the MPI pool
+        pool = MPIPool()
+
+        # Make sure the thread we're running on is the master
+        if not pool.is_master():
+            pool.wait()
+            sys.exit(0)
+        logger.debug("Running with MPI...")
+
+    elif threads > 1:
+        logger.debug("Running with multiprocessing on {} cores..."\
+                    .format(threads))
+        pool = multiprocessing.Pool(threads)
+
+    else:
+        logger.debug("Running serial...")
+        pool = None
+
+    return pool
