b6a15b2e881c1efac069c620f5126feb32cf33a2
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-05-04 20:25:15 -0400

    test gram schmidt business

--- streamteam/dynamics/tests/test_nonlinear.py
+++ streamteam/dynamics/tests/test_nonlinear.py
@@ -21,6 +21,25 @@ plot_path = "plots/tests/dynamics"
 if not os.path.exists(plot_path):
     os.makedirs(plot_path)
 
+def test_gram(self):
+    arr = np.array([
+      [0.159947293111244, -0.402071263039210, 0.781989928439469, 0.157200868935014],
+      [-0.641365729386551, -0.589502248965056, -6.334333712737469E-002, -0.787834065229250],
+      [-0.734648540580147, 0.290410423487680, 0.200395494571665, 0.547331391068177],
+      [0.140516164496874, -0.649328455579330, -0.621863558066490, 0.402036027551737]])
+
+    fortran_end_arr = np.array([
+        [0.176260247376319, -0.443078334791891, 0.861744738228658, 0.173233716603048],
+        [-0.539192290034099, -0.517682965825117, -2.242518016864357E-002, -0.663907472888126],
+        [-0.823493801368422, 0.237505395299283, 0.194657453290375, 0.477030001352140],
+        [7.871383004849420E-003, -0.692298435168163, -0.467976060614355, 0.549235217994234]])
+
+    alf = gram(arr)
+    fortran_alf = np.array([0.907449612105405,1.17546413803123,0.974054532627089,0.962464733634354])
+
+    assert np.abs(fortran_alf - alf).sum() < 1E-13
+    assert np.abs(arr - fortran_end_arr).sum() < 1E-13
+
 class TestForcedPendulum(object):
 
     def setup(self):
@@ -38,6 +57,37 @@ class TestForcedPendulum(object):
         self.chaotic_integrator = DOPRI853Integrator(F, func_args=self.chaotic_par)
 
     def test_lyapunov(self):
+        nsteps = 10000
+        dt = 0.1
+
+        regular_LEs, t, regular_ws = lyapunov(self.regular_w0, self.regular_integrator,
+                                              dt=dt, nsteps=nsteps)
+
+        return
+
+        chaotic_LEs, t, chaotic_ws = lyapunov(self.chaotic_w0, self.chaotic_integrator,
+                                              dt=dt, nsteps=nsteps,
+                                              d0=d0, nsteps_per_pullback=nsteps_per_pullback,
+                                              noffset=noffset)
+        plt.clf()
+        plt.semilogy(regular_LEs, marker=None)
+        plt.semilogy(np.mean(regular_LEs,axis=1), marker=None, linewidth=2.)
+        plt.savefig(os.path.join(plot_path,"pend_le_regular.png"))
+
+        plt.clf()
+        plt.plot(t, regular_ws[:,0], marker=None)
+        plt.savefig(os.path.join(plot_path,"pend_orbit_regular.png"))
+
+        plt.clf()
+        plt.semilogy(chaotic_LEs, marker=None)
+        plt.semilogy(np.mean(chaotic_LEs,axis=1), marker=None, linewidth=2)
+        plt.savefig(os.path.join(plot_path,"pend_le_chaotic.png"))
+
+        plt.clf()
+        plt.plot(t, chaotic_ws[:,0], marker=None)
+        plt.savefig(os.path.join(plot_path,"pend_orbit_chaotic.png"))
+
+    def test_simple_lyapunov(self):
         nsteps = 10000
         dt = 0.1
         nsteps_per_pullback = 10
