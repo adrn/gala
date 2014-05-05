632c623aca512f96b621dbbb1c95cc9ff82a5acd
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-05-05 13:28:23 -0400

    test with henon heiles potential

--- streamteam/dynamics/tests/test_nonlinear.py
+++ streamteam/dynamics/tests/test_nonlinear.py
@@ -22,7 +22,7 @@ plot_path = "plots/tests/dynamics"
 if not os.path.exists(plot_path):
     os.makedirs(plot_path)
 
-def test_gram(self):
+def test_gram():
     arr = np.array([
       [0.159947293111244, -0.402071263039210, 0.781989928439469, 0.157200868935014],
       [-0.641365729386551, -0.589502248965056, -6.334333712737469E-002, -0.787834065229250],
@@ -44,6 +44,7 @@ def test_gram(self):
 class TestForcedPendulum(object):
 
     def setup(self):
+
         def F(t,x,A,omega_d):
             q,p = x.T
             return np.array([p,-np.sin(q) + A*np.cos(omega_d*t)]).T
@@ -78,44 +79,19 @@ class TestForcedPendulum(object):
 
         plt.clf()
         plt.loglog(regular_LEs, marker=None)
-        plt.savefig(os.path.join(plot_path,"pend_regular_lyap_max.png"))
+        plt.savefig(os.path.join(plot_path,"pend_regular.png"))
 
         plt.clf()
         plt.plot(t, regular_ws[:,0], marker=None)
-        plt.savefig(os.path.join(plot_path,"pend_orbit_regular_lyap_max.png"))
+        plt.savefig(os.path.join(plot_path,"pend_orbit_regular.png"))
 
         plt.clf()
         plt.loglog(chaotic_LEs, marker=None)
-        plt.savefig(os.path.join(plot_path,"pend_chaotic_lyap_max.png"))
+        plt.savefig(os.path.join(plot_path,"pend_chaotic.png"))
 
         plt.clf()
         plt.plot(t, chaotic_ws[:,0], marker=None)
-        plt.savefig(os.path.join(plot_path,"pend_orbit_chaotic_lyap_max.png"))
-
-    def test_lyapunov_spectrum(self):
-        nsteps = 10000
-        dt = 0.1
-        regular_LEs, t, regular_ws = lyapunov_spectrum(self.regular_w0, self.regular_integrator,
-                                                       dt=dt, nsteps=nsteps)
-
-        chaotic_LEs, t, chaotic_ws = lyapunov_spectrum(self.chaotic_w0, self.chaotic_integrator,
-                                                       dt=dt, nsteps=nsteps)
-
-        plt.clf()
-        plt.loglog(regular_LEs, marker=None)
-        plt.savefig(os.path.join(plot_path,"pend_regular_lyap_spectrum.png"))
-
-        plt.clf()
-        plt.plot(t, regular_ws[:,0], marker=None)
-        plt.savefig(os.path.join(plot_path,"pend_orbit_regular_lyap_spectrum.png"))
-
-        plt.clf()
-        plt.semilogy(chaotic_LEs, marker=None)
-        plt.savefig(os.path.join(plot_path,"pend_chaotic_lyap_spectrum.png"))
-
-        plt.clf()
-        plt.plot(t, chaotic_ws[:,0], marker=None)
-        plt.savefig(os.path.join(plot_path,"pend_orbit_chaotic_lyap_spectrum.png"))
+        plt.savefig(os.path.join(plot_path,"pend_orbit_chaotic.png"))
 
     def test_frequency(self):
         import scipy.signal as ss
@@ -146,3 +122,108 @@ class TestForcedPendulum(object):
         plt.axvline(1/(2*np.pi), linewidth=3., alpha=0.35, color='r')
         plt.semilogx(f[:,0], fft[:,0], marker=None)
         plt.savefig(os.path.join(plot_path,"pend_fft_chaotic.png"))
+
+# --------------------------------------------------------------------
+
+def potential(w,A,B,C,D):
+    x,y = w[...,:2].T
+    term1 = 0.5*(A*x**2 + B*y**2)
+    term2 = D*x**2*y - C/3.*y**3
+    return term1 + term2
+
+def acceleration(w,A,B,C,D):
+    x,y = w[...,:2].T
+    ax = -(A*x + 2*D*x*y)
+    ay = -(B*y + D*x*x - C*y*y)
+    return np.array([ax, ay]).T
+
+def jerk(w,A,B,C,D):
+    x,y = w[...,:2].T
+    dx,dy = w[...,4:6].T
+
+    dax = -(A+2*D*y)*dx - 2*D*x*dy
+    day = -2*D*x*dx - (B-2*C*y)*dy
+
+    return np.array([dax,day]).T
+
+def F_max(t,w,*args):
+    x,y,px,py = w.T
+    term1 = np.array([px, py]).T
+    term2 = acceleration(w, *args)
+
+    #print(term1, term2)
+    return np.hstack((term1,term2))
+
+def F_spec(t,w,*args):
+    x,y,px,py,dx,dy,dpx,dpy = w.T
+    term1 = np.array([px, py]).T
+    term2 = acceleration(w, *args)
+    term3 = np.array([dpx,dpy]).T
+    term4 = jerk(w, *args)
+    return np.hstack((term1,term2,term3,term4))
+
+class TestHenonHeiles(object):
+
+    def setup(self):
+        # parameter choices
+        self.par = (1.,1.,1.,1.)
+
+        # initial conditions from LP-VI documentation:
+        self.w0s = np.array([[0.,0.295456,0.407308431,0.], # stable, period 1
+                             [0., 0.483, 0.27898039, 0.], # stable, quasi-periodic
+                             [0., 0.46912, 0.291124891, 0.], # quasi-periodic, close to unstable
+                             [0., 0.509, 0.254624859, 0.], # irregular
+                             [0., 0.56, 0.164113781, 0.112]]) # irregular
+
+    def test_orbit(self):
+        nsteps = 10000
+        dt = 0.01
+
+        integrator = DOPRI853Integrator(F_max, func_args=self.par)
+
+        plt.clf()
+        for ii,w0 in enumerate(self.w0s):
+            t,w = integrator.run(w0, dt=dt, nsteps=nsteps)
+            plt.plot(w[:,0,0], w[:,0,1], marker=None)
+
+        plt.savefig(os.path.join(plot_path,"hh_orbits.png"))
+
+    def test_lyapunov_max(self):
+        nsteps = 10000
+        dt = 0.1
+        nsteps_per_pullback = 10
+        d0 = 1e-5
+        noffset = 8
+
+        integrator = DOPRI853Integrator(F_max, func_args=self.par)
+        for ii,w0 in enumerate(self.w0s):
+            lyap, t, ws = lyapunov_max(w0, integrator,
+                                       dt=dt, nsteps=nsteps,
+                                       d0=d0, noffset=noffset,
+                                       nsteps_per_pullback=nsteps_per_pullback)
+            lyap = np.mean(lyap, axis=1)
+
+            plt.clf()
+            plt.loglog(lyap, marker=None)
+            plt.savefig(os.path.join(plot_path,"hh_lyap_max_{}.png".format(ii)))
+
+            plt.clf()
+            plt.plot(ws[...,0], ws[...,1], marker=None)
+            plt.savefig(os.path.join(plot_path,"hh_orbit_lyap_max_{}.png".format(ii)))
+
+    def test_lyapunov_spectrum(self):
+        nsteps = 10000
+        dt = 0.1
+
+        integrator = DOPRI853Integrator(F_spec, func_args=self.par)
+        for ii,w0 in enumerate(self.w0s):
+            lyap, t, ws = lyapunov_spectrum(w0, integrator,
+                                            dt=dt, nsteps=nsteps)
+
+            plt.clf()
+            plt.loglog(lyap, marker=None)
+            plt.savefig(os.path.join(plot_path,"hh_lyap_spec_{}.png".format(ii)))
+
+            plt.clf()
+            plt.plot(ws[...,0], ws[...,1], marker=None)
+            plt.savefig(os.path.join(plot_path,"hh_orbit_lyap_spec_{}.png".format(ii)))
