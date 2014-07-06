3f5b74013d4c707440abdd1f5b16c30f832dadd0
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-07-06 20:21:56 +0200

    split out tests for 1d normal

--- streamteam/inference/tests/test_prior.py
+++ streamteam/inference/tests/test_prior.py
@@ -14,6 +14,7 @@ import numpy as np
 import pytest
 import astropy.units as u
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 from ..prior import *
 
@@ -24,50 +25,41 @@ if not os.path.exists(plot_path):
 def test_uniform():
 
     prior = LogUniformPrior(0.,1.)
-    assert prior.shape == (1,)
     assert prior(0.5) == 0.
     assert prior(1.5) == -np.inf
     assert prior.sample(size=10).shape == (10,1)
 
     prior = LogUniformPrior([0.,1],[1.,2])
-    assert prior.shape == (2,)
-    assert np.all(prior([0.5,1.5]) == 0.)
-    assert np.all(prior([1.5,0.5]) == -np.inf)
+    assert np.all(prior([0.5,1.5]) == np.array([0., 0.]))
+    assert np.all(prior([1.5,0.5]) == np.array([-np.inf, -np.inf]))
+    assert np.all(prior([1.5,1.5]) == np.array([-np.inf, 0.]))
     assert prior.sample(size=10).shape == (10,2)
 
-    prior = LogUniformPrior([0.,1]*u.kpc,[1.,2]*u.kpc)
-    assert prior.shape == (2,)
-    assert np.all(prior([0.5,1.5]*u.kpc) == 0.)
-    assert np.all(prior([1.5,0.5]*u.kpc) == -np.inf)
-    assert np.all(prior([1.5,0.5]*u.km) == -np.inf)
-    assert prior.sample(size=10).shape == (10,2)
+def test_normal1d():
+    prior = LogNormal1DPrior(mean=0., stddev=0.5)
+    assert prior.sample(11).shape == (11,)
+    assert np.allclose(prior(0.2), np.log(norm.pdf(0.2, loc=0., scale=0.5)))
 
-def test_normal():
-
-    prior = LogNormalPrior(mean=0., stddev=0.5)
-    print(prior(0.2))
-    assert prior.shape == (1,1)
-    assert prior.cov.shape == (1,1,1)
-    assert prior._norm.shape == (1,)
-    assert prior.sample(11).shape == (11,1,1)
     plt.clf()
     plt.hist(np.squeeze(prior.sample(1024)))
     plt.savefig(os.path.join(plot_path,"norm_hist1.png"))
 
     # 2 1D Gaussians
-    prior = LogNormalPrior(mean=np.array([0.,1.]),
-                           stddev=np.array([0.5,0.5]))
-    print(prior([0.2, 1.2]))
-    assert prior.shape == (2,1)
-    assert prior.cov.shape == (2,1,1)
-    assert prior._norm.shape == (2,)
-    assert prior.sample(11).shape == (11,2,1)
+    prior = LogNormal1DPrior(mean=[0.,1.214],
+                             stddev=[0.5,0.6])
+    one = np.log(norm.pdf(0.2, loc=0., scale=0.5))
+    two = np.log(norm.pdf(1.74, loc=1.214, scale=0.6))
+    assert np.allclose(prior([0.2, 1.74]), [one,two])
+    assert prior.sample(11).shape == (11,2)
+
     plt.clf()
     s = prior.sample(1024)
-    plt.hist(s[:,0,0])
-    plt.hist(s[:,1,0])
+    plt.hist(s[:,0])
+    plt.hist(s[:,1])
     plt.savefig(os.path.join(plot_path,"norm_hist2.png"))
 
+def test_normalnd():
+    return
     # 1 2D Gaussian
     prior = LogNormalPrior(mean=np.array([[0.,1.]]),
                            stddev=np.array([[0.5,0.5]]))
