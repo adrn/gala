c090b02154e25878d8e435edb5971c3174e5615c
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-07-06 12:14:14 +0200

    twweaks to parameter

--- streamteam/inference/parameter.py
+++ streamteam/inference/parameter.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+""" Parameter classes """
+
 from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
@@ -11,46 +13,50 @@ import logging
 # Third-party
 import numpy as np
 import astropy.units as u
-from astropy.utils import isiterable
+from astropy import log as logger
 
 # Project
 from .prior import LogPrior
 
 __all__ = ["ModelParameter"]
 
-logger = logging.getLogger(__name__)
-
 class ModelParameter(u.Quantity):
 
-    def __new__(cls, name, value=np.nan, prior=None, truth=None):
-        """ Represents a model parameter to be used in inference.
-            `value` must be set so the object knows the shape of the
-            parameter, e.g., whether it is a vector or scalar.
-
-            Parameters
-            ----------
-            name : str
-                Parameter name.
-            value : quantity_like
-            prior : LogPrior
-            truth : quantity_like
+    def __new__(cls, name, value=None, truth=None, prior=None):
+        """ Represents a model Parameter MCMC inference. This object is meant
+            to be used with a Model object. The value can be a vector or scalar,
+            and may also have units. The default value is NaN (numpy.nan) because
+            Python's built-in None cannot be used with Quantity objects, but this
+            is slightly wrong because NaN will operate with other numbers...
         """
 
-        value = np.atleast_1d(value)
-        if not hasattr(value, "unit"):
-            _value = value*u.dimensionless_unscaled
-        else:
-            _value = value
+        if value is None and truth is None:
+            value = np.array(np.nan)*u.dimensionless_unscaled
+            truth = np.array(np.nan)*u.dimensionless_unscaled
+
+        elif value is not None and truth is None:
+            if hasattr(value, "unit"):
+                unit = value.unit
+            else:
+                unit = u.dimensionless_unscaled
+            value = np.array(value)*unit
+            truth = np.ones_like(value)*np.nan*unit
+
+        elif value is None and truth is not None:
+            if hasattr(truth, "unit"):
+                unit = truth.unit
+            else:
+                unit = u.dimensionless_unscaled
+            truth = np.array(truth)*unit
+            value = np.ones_like(truth)*np.nan*unit
 
         self = super(ModelParameter, cls).__new__(cls, value)
 
-        # make sure input prior is a Prior, or a list of Prior objects
+        # assign a benign prior that always evaluates to 0. if none specified
         if prior is None:
             prior = LogPrior()
 
-        if truth is None:
-            truth = np.zeros_like(_value)*np.nan
-
+        # prior must be a LogPrior object
         if not isinstance(prior, LogPrior):
             raise TypeError("prior must be a LogPrior subclass, not {}."
                             .format(type(prior)))
@@ -59,17 +65,33 @@ class ModelParameter(u.Quantity):
         self.truth = truth
         self.name = str(name)
 
+        # whether the parameter is variable or not - default: not frozen
+        self.frozen = False
+
         return self
 
     def copy(self):
         """ Return a copy of this `ModelParameter` instance """
         return ModelParameter(name=self.name, value=self.value*self.unit,
-                              prior=self.prior, truth=self.truth)
+                              prior=self._prior, truth=self.truth)
 
     def __deepcopy__(self):
         """ Return a copy of this `ModelParameter` instance """
         return self.copy()
 
+    def __repr__(self):
+        extra = ""
+        if np.all(~np.isnan(self.truth)):
+            extra = " truth={}".format(self.truth)
+
+        return "<ModelParameter '{}'{}>".format(self.name, extra)
+
+    def __str__(self):
+        return self.name
+
+    # -------------------------------------------------------------------------- #
+    #   this stuff is so this object is picklable / can be used
+    #       with multiprocessing and MPI.
     def __reduce__(self):
         # patch to pickle ModelParameter objects (ndarray subclasses),
         # see http://www.mail-archive.com/numpy-discussion@scipy.org/msg02446.html
@@ -85,13 +107,4 @@ class ModelParameter(u.Quantity):
         nd_state, own_state = state
         super(ModelParameter, self).__setstate__(nd_state)
         self.__dict__.update(own_state)
-
-    def __repr__(self):
-        extra = ""
-        if np.all(~np.isnan(self.truth)):
-            extra = " truth={}".format(self.truth)
-
-        return "<ModelParameter '{}'{}>".format(self.name, extra)
-
-    def __str__(self):
-        return self.name
+    # -------------------------------------------------------------------------- #
