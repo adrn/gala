45dbfa3c0480b1635a6158a47029209dde552c1d
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-07-24 12:36:48 -0400

    added lm10 potential

---
+++ streamteam/potential/lm10.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+""" Potential used in Law & Majewski 2010 """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+
+# Third-party
+import numpy as np
+from astropy import log as logger
+import astropy.units as u
+
+# Project
+from .core import CompositePotential
+from .builtin import *
+
+__all__ = ['LM10Potential']
+
+class LM10Potential(CompositePotential):
+
+    def __init__(self, m_disk=1E11, a=6.5, b=0.26,
+                 m_spher=3.4E10, c=0.7,
+                 q1=1.38, q2=1., q3=1.36, phi=(97*u.degree).to(u.radian).value,
+                 v_c=np.sqrt(2)*(121.858*u.km/u.s).to(u.kpc/u.Myr).value, r_h=12.):
+
+        usys = (u.kpc, u.M_sun, u.Myr, u.radian)
+
+        kwargs = dict()
+        kwargs["disk"] = MiyamotoNagaiPotential(usys=usys,
+                                                m=m_disk, a=a, b=b)
+
+        kwargs["bulge"] = HernquistPotential(usys=usys,
+                                             m=m_spher, c=c)
+
+        kwargs["halo"] = TriaxialLogarithmicPotential(usys=usys,
+                                                      q1=q1, q2=q2, q3=q3,
+                                                      phi=phi, v_c=v_c, r_h=r_h)
+        super(LM10Potential,self).__init__(**kwargs)
---
+++ streamteam/potential/tests/test_lm10.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+"""
+    Test the LM10Potential
+"""
+
+from __future__ import absolute_import, unicode_literals, division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+import os
+import pytest
+import numpy as np
+from astropy.constants.si import G
+import astropy.units as u
+import matplotlib.pyplot as plt
+
+from ..core import *
+from ..builtin import *
+from ..lm10 import *
+
+top_path = "/tmp/streamteam"
+plot_path = os.path.join(top_path, "tests/potential")
+if not os.path.exists(plot_path):
+    os.makedirs(plot_path)
+
+from astropy.utils.console import color_print
+print()
+color_print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", "yellow")
+color_print("To view plots:", "green")
+print("    open {}".format(plot_path))
+color_print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", "yellow")
+
+def test_create():
+    potential = LM10Potential()
