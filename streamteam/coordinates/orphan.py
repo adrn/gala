a373d32d383b703e9f4319da7019b0ce3f125102
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-07-26 14:31:58 -0400

    add link to astropy docs

--- streamteam/coordinates/orphan.py
+++ streamteam/coordinates/orphan.py
@@ -22,6 +22,9 @@ class Orphan(coord.BaseCoordinateFrame):
     of the Orphan stream, as described in
     `this paper <http://iopscience.iop.org/0004-637X/711/1/32/pdf/apj_711_1_32.pdf>`_.
 
+    For more information about this class, see the Astropy documentation
+    on `Coordinate Frames <http://docs.astropy.org/en/latest/coordinates/frames.html>`_.
+
     Parameters
     ----------
     representation : `BaseRepresentation` or None
