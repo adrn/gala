70012691bb90022ecbd92f9dd5df2a8f5eca3b92
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-08-07 17:23:28 -0400

    add apw potential

---
+++ streamteam/potential/apw.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+""" Potential used in Price-Whelan et al. (in prep.) TODO """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+
+# Third-party
+import numpy as np
+from astropy import log as logger
+import astropy.units as u
+
+# Project
+from .core import CompositePotential
+from .builtin import *
+from .cbuiltin import MiyamotoNagaiPotential, LeeSutoNFWPotential
+
+__all__ = ['PW14Potential']
+
+class PW14Potential(CompositePotential):
+
+    def __init__(self, m_disk=1E11, a=6.5, b=0.26,
+                 m_spher=3.4E10, c=0.7,
+                 q1=1.4, q2=1., q3=0.6,
+                 v_h=0.15, r_h=12.):
+
+        usys = (u.kpc, u.M_sun, u.Myr, u.radian)
+        self.usys = usys
+
+        kwargs = dict()
+        kwargs["disk"] = MiyamotoNagaiPotential(usys=usys,
+                                                m=m_disk, a=a, b=b)
+
+        kwargs["bulge"] = HernquistPotential(usys=usys,
+                                             m=m_spher, c=c)
+
+        kwargs["halo"] = LeeSutoNFWPotential(usys=usys,
+                                             a=q1, b=q2, c=q3,
+                                             v_h=v_h, r_h=r_h)
+        super(PW14Potential,self).__init__(**kwargs)
