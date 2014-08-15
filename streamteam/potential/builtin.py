4e46471fbf6ef9e857309bfd301b0c26717f263d
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-08-15 13:43:31 -0400

    convert from action angle to xv

--- streamteam/potential/builtin.py
+++ streamteam/potential/builtin.py
@@ -13,9 +13,10 @@ import math
 import numpy as np
 import astropy.units as u
 from astropy.constants import G
+import scipy.optimize as so
 
 from .core import Potential, CartesianPotential, CompositePotential
-from ..coordinates import cartesian_to_spherical
+from ..coordinates import cartesian_to_spherical, spherical_to_cartesian
 
 __all__ = ["PointMassPotential", "MiyamotoNagaiPotential",\
            "HernquistPotential", "LogarithmicPotential",\
@@ -267,8 +268,7 @@ class IsochronePotential(CartesianPotential):
         psi = np.arctan2(np.cos(theta), -np.sin(theta)*r*vtheta/L)
         psi[np.abs(vtheta) <= 1e-10] = np.pi/2. # blows up for small vtheta
 
-        OmegaR = (-2*E)**1.5 / GM
-        OmegaPhi = 0.5*OmegaR * (1 + L/np.sqrt(L*L + 4*GM*b))
+        omega = 0.5 * (1 + L/np.sqrt(L*L + 4*GM*b))
 
         a = np.sqrt((1+e) / (1-e))
         ap = np.sqrt((1 + e + 2*b/c) / (1 - e + 2*b/c))
@@ -285,8 +285,8 @@ class IsochronePotential(CartesianPotential):
             z[ix] = np.arctan(x[ix]*np.tan(0.5*y[ix]))
             return z
 
-        thetaz = psi + OmegaPhi*thetar/OmegaR - F(a,eta) - \
-                 F(ap,eta)/np.sqrt(1 + 4*GM*b/L/L)
+        A = omega*thetar - F(a,eta) - F(ap,eta)/np.sqrt(1 + 4*GM*b/L/L)
+        thetaz = psi + A
 
         LR = Lz/L
         sinu = LR/np.sqrt(1.-LR*LR)/np.tan(theta)
@@ -298,9 +298,119 @@ class IsochronePotential(CartesianPotential):
 
         thetap = phi - u + np.sign(Lz)*thetaz
         angles = np.array([thetar, thetap, thetaz]).T
-        angles %= 2*np.pi
+        angles %= (2*np.pi)
 
         return actions, angles
+
+    def phase_space(self, actions, angles):
+        """ TODO: """
+        # def angact_to_xv_iso(act,ang,M,b)
+        """Calculate x and v for a given set of agtion angles using the analytically
+        solvable isochrone potential with best fit parameters b and M.
+        Function takes in array of action and angles (1x3,1x3) and the best fit parameters for b
+        and M in the isocrhone potential and returns X,V = [( x1,x2,x3), (v1,v2,v3)] for those action
+        angles.
+        We use Appendix 2 in McGill&Binney 1990
+        J1,2,3 = Jr,phi,theta = Jr,Lz,L-Lz"""
+
+
+        """ Binney & Tremaine (2008) """
+
+        actions = np.atleast_2d(actions)
+        angles = np.atleast_2d(angles)
+
+        _G = G.decompose(self.usys).value
+        GM = _G*self.parameters['m']
+        b = self.parameters['b']
+
+        # actions
+        Jr = actions[:,0]
+        Lz = actions[:,1]
+        L = actions[:,2] + np.abs(Lz)
+
+        # angles
+        theta_r,theta_phi,theta_theta = angles.T
+
+        # get longitude of ascending node
+        theta_1 = theta_phi - np.sign(Lz)*theta_theta
+        Omega = theta_1
+
+        Ly = -np.cos(Omega) * np.sqrt(L**2 - Lz**2)
+        Lx = np.sqrt(L**2 - Ly**2 - Lz**2)
+        cosi = Lz/L
+        sini = np.sqrt(1 - cosi**2)
+
+        # Hamiltonian (energy)
+        H = -2. * GM**2 / (2.*Jr + L + np.sqrt(4.*b*GM + L**2))**2
+
+        if np.any(H > 0.):
+            raise ValueError("Unbound particle. (E = {})".format(H))
+
+        # Eq. 3.240
+        c = -GM / (2.*H) - b
+        e = np.sqrt(1 - L*L*(1 + b/c) / GM / c)
+
+        # solve for eta
+        theta_3 = theta_r
+        eta_func = lambda x: x - e*c/(b+c)*np.sin(x) - theta_3
+        sol = so.root(eta_func, [np.pi/2]*len(theta_3), tol=1E-7)
+        eta = sol.x
+        if not sol.success:
+            raise ValueError("Unable to find solution for η.")
+
+        # TODO: when to do this???
+        eta -= 2*np.pi
+
+        r = c*np.sqrt((1-e*np.cos(eta)) * (1-e*np.cos(eta) + 2*b/c))
+        vr = np.sqrt(GM/(b+c))*(c*e*np.sin(eta))/r
+
+        theta_2 = theta_theta
+        Omega_23 = 0.5*(1 + L / np.sqrt(L**2 + 4*GM*b))
+        # A = L/np.sqrt(L**2 + 4*b*GM)
+        # arg1 = np.arctan(np.sqrt((1+e)/(1-e))*np.tan(0.5*eta))
+        # arg2 = A*np.arctan(np.sqrt((1+e+2*b/c)/(1-e+2*b/c))*np.tan(0.5*eta))
+        # psi = theta_2 - Omega_23*theta_3 + arg1 + arg2
+
+        a = np.sqrt((1+e) / (1-e))
+        ap = np.sqrt((1 + e + 2*b/c) / (1 - e + 2*b/c))
+        def F(x, y):
+            z = np.zeros_like(x)
+
+            ix = y>np.pi/2.
+            z[ix] = np.pi/2. - np.arctan(np.tan(np.pi/2.-0.5*y[ix])/x[ix])
+
+            ix = y<-np.pi/2.
+            z[ix] = -np.pi/2. + np.arctan(np.tan(np.pi/2.+0.5*y[ix])/x[ix])
+
+            ix = (y<=np.pi/2) & (y>=-np.pi/2)
+            z[ix] = np.arctan(x[ix]*np.tan(0.5*y[ix]))
+            return z
+
+        theta_2[Lz < 0] -= 2*np.pi
+        theta_3 -= 2*np.pi
+        A = Omega_23*theta_3 - F(a,eta) - F(ap,eta)/np.sqrt(1 + 4*GM*b/L/L)
+        psi = theta_2 - A
+
+        # theta
+        theta = np.arccos(np.sin(psi)*sini)
+        vtheta = L*sini*np.cos(psi)/np.cos(theta)
+
+        # phi
+        sinu = np.sin(psi)*cosi/np.sin(theta)
+        u = np.arcsin(sinu)
+        u[sinu > 1.] = np.pi/2.
+        u[sinu < -1.] = -np.pi/2.
+        u[vtheta > 0.] = np.pi - u[vtheta > 0.]
+
+        sinu = cosi/sini * np.cos(theta)/np.sin(theta)
+        phi = (u + Omega) % (2*np.pi)
+
+        vtheta = -L*sini*np.cos(psi)/np.sin(theta)/r
+        vphi = Lz / (r*np.sin(theta))
+
+        # We now need to convert from spherical polar coord to cart. coord.
+        x,v = spherical_to_cartesian(r,phi,theta,vr,vphi,vtheta)
+        return x,v
 
 ##############################################################################
 #    Miyamoto-Nagai Disk potential from Miyamoto & Nagai 1975
