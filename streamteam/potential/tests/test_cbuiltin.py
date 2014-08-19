c59af47018aca9157d40844efdd603de747dec2f
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-08-19 11:12:37 -0400

    subclass tests

--- streamteam/potential/tests/test_cbuiltin.py
+++ streamteam/potential/tests/test_cbuiltin.py
@@ -40,103 +40,107 @@ color_print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 niter = 1000
 nparticles = 1000
 
-class TestMiyamotoNagai(object):
-    usys = (u.kpc, u.M_sun, u.Myr, u.radian)
-    def setup(self):
-        print()
+class PotentialTestBase(object):
 
-    def test_create_plot(self):
-        from ..builtin import MiyamotoNagaiPotential as PyMiyamotoNagaiPotential
-
-        potential = MiyamotoNagaiPotential(usys=self.usys,
-                                           m=1.E11,
-                                           a=6.5,
-                                           b=0.26)
-        pypotential = PyMiyamotoNagaiPotential(usys=self.usys,
-                                               m=1.E11,
-                                               a=6.5,
-                                               b=0.26)
-
+    def test_method_call(self):
         # single
         r = [[1.,0.,0.]]
-        pot_val = potential.value(r)
-        acc_val = potential.acceleration(r)
+        pot_val = self.potential.value(r)
+        acc_val = self.potential.acceleration(r)
 
         # multiple
         r = np.random.uniform(size=(nparticles,3))
+        pot_val = self.potential.value(r)
+        acc_val = self.potential.acceleration(r)
 
+        if self.pypotential is not None:
+            py_pot_val = self.potential.value(r)
+            py_acc_val = self.potential.acceleration(r)
+
+            assert np.allclose(py_pot_val, pot_val)
+            assert np.allclose(py_acc_val, acc_val)
+            print("Cython and Python values match")
+
+    def test_orbit_integration(self):
+        w0 = self.w0
+        t1 = time.time()
+        t,w = self.potential.integrate_orbit(w0, dt=1., nsteps=10000)
+        print("Cython orbit integration time (10000 steps): {}".format(time.time() - t1))
+
+        if self.pypotential is not None:
+            t1 = time.time()
+            t,w = self.pypotential.integrate_orbit(w0, dt=1., nsteps=10000)
+            print("Python orbit integration time (10000 steps): {}".format(time.time() - t1))
+
+    def test_time_methods(self):
+        r = np.random.uniform(size=(nparticles,3))
         for func_name in ["value", "gradient", "acceleration"]:
             t1 = time.time()
             for ii in range(niter):
-                x = getattr(potential, func_name)(r)
+                x = getattr(self.potential, func_name)(r)
             print("Cython - {}: {:e} sec per call".format(func_name,
                             (time.time()-t1)/float(niter)))
 
-            t1 = time.time()
-            for ii in range(niter):
-                x = getattr(pypotential, func_name)(r)
-            print("Python - {}: {:e} sec per call".format(func_name,
-                            (time.time()-t1)/float(niter)))
-        return
-        # acc_val = potential.acceleration(r)
+            if self.pypotential is not None:
+                t1 = time.time()
+                for ii in range(niter):
+                    x = getattr(self.pypotential, func_name)(r)
+                print("Python - {}: {:e} sec per call".format(func_name,
+                                (time.time()-t1)/float(niter)))
 
+    def test_plot_contours(self):
+
+        # test plotting a grid
         grid = np.linspace(-20.,20, 200)
 
-        t1 = time.time()
-        fig,axes = potential.plot_contours(grid=(grid,0.,grid))
-        print(time.time() - t1)
+        fig,axes = plt.subplots(1,1)
 
         t1 = time.time()
-        fig,axes = pypotential.plot_contours(grid=(grid,0.,grid))
-        print(time.time() - t1)
-        fig.savefig(os.path.join(plot_path, "miyamoto_nagai_2d.png"))
+        fig,axes = self.potential.plot_contours(grid=(grid,0.,grid))
+        print("Cython plot_contours time", time.time() - t1)
+        fig.savefig(os.path.join(plot_path, "{}_2d_cy.png"\
+                        .format(self.potential.__class__.__name__)))
 
-class TestLeeSutoNFWPotential(object):
+        if self.pypotential is not None:
+            t1 = time.time()
+            fig,axes = self.pypotential.plot_contours(grid=(grid,0.,grid))
+            print("Python plot_contours time", time.time() - t1)
+            fig.savefig(os.path.join(plot_path, "{}_2d_py.png"\
+                         .format(self.pypotential.__class__.__name__)))
+
+class TestHernquist(PotentialTestBase):
     usys = (u.kpc, u.M_sun, u.Myr, u.radian)
     def setup(self):
         print()
+        from ..builtin import HernquistPotential as PyHernquistPotential
 
-    def test_create_plot(self):
-        from ...dynamics import plot_orbits
-        from ...integrate import DOPRI853Integrator
-        potential = LeeSutoNFWPotential(usys=self.usys,
-                                        v_h=0.35, r_h=12.,
-                                        a=1.4, b=1., c=0.6)
+        self.potential = HernquistPotential(usys=self.usys,
+                                            m=1.E11, c=0.26)
+        self.pypotential = PyHernquistPotential(usys=self.usys,
+                                                m=1.E11, c=0.26)
+        self.w0 = [1.,0.,0.,0.,0.1,0.1]
 
-        w0 = np.array([19.0,2.7,-6.9,0.0352238,-0.03579493,0.075])
+class TestMiyamotoNagai(PotentialTestBase):
+    usys = (u.kpc, u.M_sun, u.Myr, u.radian)
+    def setup(self):
+        print()
+        from ..builtin import MiyamotoNagaiPotential as PyMiyamotoNagaiPotential
 
-        t1 = time.time()
-        t,w = potential.integrate_orbit(w0, dt=1., nsteps=60000) #, Integrator=DOPRI853Integrator)
-        print("Time: {}".format(time.time() - t1))
+        self.potential = MiyamotoNagaiPotential(usys=self.usys,
+                                                m=1.E11, a=6.5, b=0.26)
+        self.pypotential = PyMiyamotoNagaiPotential(usys=self.usys,
+                                                    m=1.E11, a=6.5, b=0.26)
+        self.w0 = [8.,0.,0.,0.,0.22,0.1]
 
-        #plt.plot(w[:,0,0], w[:,0,2], marker=None)
-        fig = plot_orbits(w, triangle=True, marker=None)
-        plt.show()
-        return
+class TestLeeSutoNFWPotential(PotentialTestBase):
+    usys = (u.kpc, u.M_sun, u.Myr, u.radian)
+    def setup(self):
+        print()
+        self.potential = LeeSutoNFWPotential(usys=self.usys,
+                                             v_h=0.35, r_h=12.,
+                                             a=1.4, b=1., c=0.6)
 
-        # single
-        r = [[1.,0.,0.]]
-        pot_val = potential.value(r)
-        acc_val = potential.acceleration(r)
+        self.pypotential = None
 
-        # multiple
-        r = np.random.uniform(size=(nparticles,3))
-        pot_val = potential.value(r)
-        acc_val = potential.acceleration(r)
+        self.w0 = [19.0,2.7,-6.9,0.0352238,-0.03579493,0.075]
 
-        for func_name in ["value", "gradient", "acceleration"]:
-            t1 = time.time()
-            for ii in range(niter):
-                x = getattr(potential, func_name)(r)
-            print("Cython - {}: {:e} sec per call".format(func_name,
-                            (time.time()-t1)/float(niter)))
-
-        # acc_val = potential.acceleration(r)
-
-        grid = np.linspace(-50, 50, 200)
-
-        t1 = time.time()
-        fig,axes = potential.plot_contours(grid=(grid,0.,grid))
-        print(time.time() - t1)
-
-        fig.savefig(os.path.join(plot_path, "lee_suto_nfw_2d.png"))
