62a7197895646942e4eb441f6baa03998c15242c
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-08-19 10:45:56 -0400

    add hernquist potential

--- streamteam/potential/cbuiltin.pyx
+++ streamteam/potential/cbuiltin.pyx
@@ -37,6 +37,109 @@ cdef extern from "math.h":
 __all__ = ['MiyamotoNagaiPotential', 'LeeSutoNFWPotential']
 
 ##############################################################################
+#    Hernquist Spheroid potential from Hernquist 1990
+#    http://adsabs.harvard.edu/abs/1990ApJ...356..359H
+#
+cdef class _HernquistPotential(_CPotential):
+
+    # here need to cdef all the attributes
+    cdef public double G, GM
+    cdef public double m, a, b, b2
+
+    def __init__(self, double G, double m, double c):
+        """ Units of everything should be in the system:
+                kpc, Myr, radian, M_sun
+        """
+        # cdef double G = 4.499753324353494927e-12 # kpc^3 / Myr^2 / M_sun
+        # have to specify G in the correct units
+        self.G = G
+
+        # disk parameters
+        self.GM = G*m
+        self.m = m
+        self.c = c
+
+    @cython.boundscheck(False)
+    @cython.cdivision(True)
+    @cython.wraparound(False)
+    @cython.nonecheck(False)
+    cdef public inline void _value(self, double[:,::1] r,
+                                   double[::1] pot, int nparticles):
+
+        cdef double R
+        for i in range(nparticles):
+            R = sqrt(r[i,0]*r[i,0] + r[i,1]*r[i,1] + r[i,2]*r[i,2])
+            pot[i] = -self.GM / (R + self.c)
+
+    @cython.boundscheck(False)
+    @cython.cdivision(True)
+    @cython.wraparound(False)
+    @cython.nonecheck(False)
+    cdef public inline void _gradient(self, double[:,::1] r,
+                                      double[:,::1] grad, int nparticles):
+
+        cdef double x, y, z, R, fac
+        for i in range(nparticles):
+            x = r[i,0]
+            y = r[i,1]
+            z = r[i,2]
+            R = sqrt(x*x + y*y + z*z)
+
+            fac = self.GM / (pow(R + self.c,2) * R)
+
+            grad[i,0] = fac*x
+            grad[i,1] = fac*y
+            grad[i,2] = fac*z
+
+    # @cython.boundscheck(False)
+    # @cython.cdivision(True)
+    # @cython.wraparound(False)
+    # @cython.nonecheck(False)
+    # cdef public inline void _hessian(self, double[:,::1] r,
+    #                                  double[:,:,::1] hess, int nparticles):
+
+    #     cdef double x, y, z
+    #     cdef double sqrtz, zd, fac
+    #     for i in range(nparticles):
+    #         x = r[i,0]
+    #         y = r[i,1]
+    #         z = r[i,2]
+
+    #         sqrtz = sqrt(z*z + self.b2)
+    #         zd = self.a + sqrtz
+    #         fac = self.GM*pow(x*x + y*y + zd*zd, -1.5)
+
+    #         grad[i,0] = fac*x
+    #         grad[i,1] = fac*y
+    #         grad[i,2] = fac*z * (1. + self.a / sqrtz)
+
+class HernquistPotential(CPotential, CartesianPotential):
+    r"""
+    Hernquist potential for a spheroid.
+
+    .. math::
+
+        \Phi_{spher} = -\frac{GM_{spher}}{r + c}
+
+    Parameters
+    ----------
+    m : numeric
+        Mass.
+    c : numeric
+        Core concentration.
+    usys : iterable
+        Unique list of non-reducable units that specify (at minimum) the
+        length, mass, time, and angle units.
+
+    """
+    def __init__(self, m, c, usys):
+        self.usys = usys
+        _G = G.decompose(usys).value
+        parameters = dict(G=_G, m=m, c=c)
+        super(HernquistPotential, self).__init__(_HernquistPotential,
+                                                 parameters=parameters)
+
+##############################################################################
 #    Miyamoto-Nagai Disk potential from Miyamoto & Nagai 1975
 #    http://adsabs.harvard.edu/abs/1975PASJ...27..533M
 #
