8f65cc661c9d60db6ce5a9a58d6f85c7b77fb842
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-09-29 15:36:23 -0400

    test for harmonic osc

--- streamteam/dynamics/tests/test_analyticactionangle.py
+++ streamteam/dynamics/tests/test_analyticactionangle.py
@@ -35,6 +35,7 @@ if not os.path.exists(plot_path):
 class TestIsochrone(object):
 
     def setup(self):
+        logger.info("======== Isochrone ========")
         self.N = 100
         np.random.seed(42)
         x = np.random.uniform(-10., 10., size=(self.N,3))
@@ -80,7 +81,8 @@ class TestIsochrone(object):
 class TestHarmonicOscillator(object):
 
     def setup(self):
-        self.N = 10
+        logger.info("======== Harmonic Oscillator ========")
+        self.N = 100
         np.random.seed(42)
         x = np.random.uniform(-10., 10., size=(self.N,3))
         v = np.random.uniform(-1., 1., size=(self.N,3)) / 33.
@@ -88,50 +90,41 @@ class TestHarmonicOscillator(object):
 
         self.potential = HarmonicOscillatorPotential(omega=np.array([0.013, 0.02, 0.005]), usys=galactic)
         self.t,self.w = self.potential.integrate_orbit(w0, dt=0.1, nsteps=10000)
+        self.t = self.t[::10]
+        self.w = self.w[::10]
 
     def test(self):
+        """
+            !!!!! NOTE !!!!!
+            For Harmonic Oscillator, Sanders' code works for the units I use...
+        """
         for n in range(self.N):
+            logger.debug("Orbit {}".format(n))
+
             x,v = self.w[:,n,:3],self.w[:,n,3:]
+            ww = self.w[:,n]
             actions,angles = harmonic_oscillator_xv_to_aa(x, v, self.potential)
 
             for i in range(3):
                 assert np.allclose(actions[1:,i], actions[0,i], rtol=1E-5)
 
-            # x2,v2 = harmonic_oscillator_aa_to_xv(actions, angles, self.potential)
+            # Compare to genfunc
+            omega = self.potential.parameters['omega']
+            aa = np.array([toy_potentials.angact_ho(w, omega=omega) for w in ww])
+            s_actions = aa[:,:3]
+            s_angles = aa[:,3:]
 
-            # rel_err_x = np.abs((x2 - x) / x)
-            # rel_err_v = np.abs((v2 - v) / v)
+            assert np.allclose(actions, s_actions, rtol=1E-8)
+            assert np.allclose(angles, s_angles, rtol=1E-8)
 
-            # assert rel_err_x.max() < (1E-8)
-            # assert rel_err_v.max() < (1E-8)
+            # test roundtrip
+            x2,v2 = harmonic_oscillator_aa_to_xv(actions, angles, self.potential)
 
-# def test_roundtrip(self):
+            # TODO: figure out transform back
+            continue
 
-#     np.random.seed(4342)
-#     n = 10
-#     x = np.random.uniform(-10., 10., size=(n,3))
-#     v = np.random.uniform(-1., 1., size=(n,3)) / 33.
+            rel_err_x = np.abs((x2 - x) / x)
+            rel_err_v = np.abs((v2 - v) / v)
 
-#     potential = IsochronePotential(usys=self.usys, m=1.E11, b=5.)
-#     acc = lambda t,x: potential.acceleration(x)
-#     integrator = LeapfrogIntegrator(acc)
-#     t,ws = integrator.run(np.hstack((x,v)), dt=1., nsteps=10000)
-#     print()
-
-#     for i in range(n):
-#         xs = ws[:,i,:3]
-#         vs = ws[:,i,3:]
-
-#         # r,phi,theta,vr,vphi,vtheta = cartesian_to_spherical(xs,vs).T
-#         # print("True r", r)
-#         # print("True φ", phi)
-#         # print("True θ", theta)
-#         # print("True vr", vr)
-#         # print("True vφ", vphi)
-#         # print("True vθ", vtheta)
-
-#         actions,angles = potential.action_angle(xs, vs)
-#         x,v = potential.phase_space(actions, angles)
-
-#         assert np.allclose(x, xs, rtol=1E-8)
-#         assert np.allclose(v, vs, rtol=1E-8)
+            assert rel_err_x.max() < (1E-8)
+            assert rel_err_v.max() < (1E-8)
