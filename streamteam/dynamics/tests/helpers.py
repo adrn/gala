74134aeeb25f778c4ff4194baa57bd4e00de4305
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-09-29 09:36:32 -0400

    generate isotropic w0s

--- streamteam/dynamics/tests/helpers.py
+++ streamteam/dynamics/tests/helpers.py
@@ -10,8 +10,18 @@ __author__ = "adrn <adrn@astro.columbia.edu>"
 import os, sys
 
 # Third-party
+import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
+
+# Project
+from ..units import galactic
+from ..coordinates import spherical_to_cartesian
+
+# HACK:
+if "/Users/adrian/projects/genfunc" not in sys.path:
+    sys.path.append("/Users/adrian/projects/genfunc")
+import genfunc_3d
 
 def sanders_nvecs(N_max, dx, dy, dz):
     from itertools import product
@@ -81,3 +91,24 @@ def plot_angles(t, angles, freqs, subsample_factor=1000):
     axes[0].set_ylim(0,2)
     return fig
     # axes[1].scatter(theta[0,ix], theta[2], alpha=0.5, marker='o', c=t)
+
+def isotropic_w0(N=100):
+    # positions
+    d = np.random.lognormal(mean=np.log(25), sigma=0.5, size=N)
+    phi = np.random.uniform(0, 2*np.pi, size=N)
+    theta = np.arccos(np.random.uniform(size=N) - 0.5)
+
+    vr = np.random.normal(150., 40., size=N)*u.km/u.s
+    vt = np.random.normal(100., 40., size=N)
+    vt = np.vstack((vt,np.zeros_like(vt))).T
+
+    # rotate to be random position angle
+    pa = np.random.uniform(0, 2*np.pi, size=N)
+    M = np.array([[np.cos(pa), -np.sin(pa)],[np.sin(pa), np.cos(pa)]]).T
+    vt = np.array([vv.dot(MM) for (vv,MM) in zip(vt,M)])*u.km/u.s
+    vphi,vtheta = vt.T
+
+    x,v = spherical_to_cartesian(d, phi, theta,
+                                 vr.decompose(galactic).value,
+                                 vphi.decompose(galactic).value,
+                                 vtheta.decompose(galactic).value)
