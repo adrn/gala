86e2e2b34e0caea40dbd09c41fc2114febd3ed98
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-09-29 11:55:19 -0400

    copare with sanders

--- streamteam/dynamics/tests/test_analyticactionangle.py
+++ streamteam/dynamics/tests/test_analyticactionangle.py
@@ -14,11 +14,17 @@ import logging
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy import log as logger
+import astropy.units as u
 
 # Project
 from ..analyticactionangle import *
 from ...potential import IsochronePotential, HarmonicOscillatorPotential
 from ...units import galactic
+
+# HACK:
+if "/Users/adrian/projects/genfunc" not in sys.path:
+    sys.path.append("/Users/adrian/projects/genfunc")
+import genfunc_3d, toy_potentials
 
 logger.setLevel(logging.DEBUG)
 
@@ -29,7 +35,7 @@ if not os.path.exists(plot_path):
 class TestIsochrone(object):
 
     def setup(self):
-        self.N = 10
+        self.N = 100
         np.random.seed(42)
         x = np.random.uniform(-10., 10., size=(self.N,3))
         v = np.random.uniform(-1., 1., size=(self.N,3)) / 33.
@@ -37,15 +43,32 @@ class TestIsochrone(object):
 
         self.potential = IsochronePotential(usys=galactic, m=1.E11, b=5.)
         self.t,self.w = self.potential.integrate_orbit(w0, dt=0.1, nsteps=10000)
+        self.t = self.t[::10]
+        self.w = self.w[::10]
 
     def test(self):
         for n in range(self.N):
+            logger.debug("Orbit {}".format(n))
+
             x,v = self.w[:,n,:3],self.w[:,n,3:]
+            s_v = (v*u.kpc/u.Myr).to(u.km/u.s).value
+            s_w = np.hstack((x,s_v))
             actions,angles = isochrone_xv_to_aa(x, v, self.potential)
 
             for i in range(3):
                 assert np.allclose(actions[1:,i], actions[0,i], rtol=1E-5)
 
+            # Compare to genfunc
+            m = self.potential.parameters['m'] / 1E11
+            b = self.potential.parameters['b']
+            aa = np.array([toy_potentials.angact_iso(w, params=(m,b)) for w in s_w])
+            s_actions = (aa[:,:3]*u.km/u.s*u.kpc).decompose(galactic).value
+            s_angles = aa[:,3:]
+
+            assert np.allclose(actions, s_actions, rtol=1E-8)
+            assert np.allclose(angles, s_angles, rtol=1E-8)
+
+            # test roundtrip
             x2,v2 = isochrone_aa_to_xv(actions, angles, self.potential)
 
             rel_err_x = np.abs((x2 - x) / x)
