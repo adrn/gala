c2c37ccb11eeeb17578546c1510d7b1ca9cc6027
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-10-27 23:01:21 +0100

    implemented two potentials as ccomposite

--- streamteam/potential/apw.py
+++ streamteam/potential/apw.py
@@ -7,26 +7,27 @@ from __future__ import division, print_function
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
 # Third-party
-import astropy.units as u
 import numpy as np
 
 # Project
-from .core import CartesianCompositePotential
+# from .core import CartesianCompositePotential
+from .cpotential import CCompositePotential
 from .cbuiltin import HernquistPotential, MiyamotoNagaiPotential, LeeSutoNFWPotential
+from ..units import galactic
 
 __all__ = ['PW14Potential']
 
-
-class PW14Potential(CartesianCompositePotential):
+# change to CartesianCompositePotential for Pure-Python
+class PW14Potential(CCompositePotential):
 
     def __init__(self, m_disk=6.5E10, a=6.5, b=0.26,
                  m_spher=2E10, c=0.3,
                  q1=1.4, q2=1., q3=0.6,
                  v_h=0.562, r_h=30.,
-                 phi=np.pi/2., theta=np.pi/2., psi=np.pi/2.):
+                 phi=np.pi/2., theta=np.pi/2., psi=np.pi/2.,
+                 units=galactic):
 
         # Choice of v_h sets circular velocity at Sun to 220 km/s
-        units = (u.kpc, u.M_sun, u.Myr, u.radian)
         self.units = units
 
         kwargs = dict()
@@ -41,3 +42,4 @@ class PW14Potential(CartesianCompositePotential):
                                              v_h=v_h, r_h=r_h,
                                              phi=phi, theta=theta, psi=psi)
         super(PW14Potential,self).__init__(**kwargs)
+
