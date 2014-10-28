910ac193a6b78374458b6c1f713ee4bd1529ae6d
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-10-28 15:18:17 +0100

    added pal5 potential for gaia challenge

--- streamteam/potential/cbuiltin.pyx
+++ streamteam/potential/cbuiltin.pyx
@@ -459,3 +459,61 @@ class LogarithmicPotential(CPotential, CartesianPotential):
         parameters['R'] = R
         super(LogarithmicPotential, self).__init__(_LogarithmicPotential,
                                                    parameters=parameters)
+
+# ============================================================================
+#    Pal5 Challenge Halo (for Gaia Challenge)
+#
+cdef class _Pal5AxisymmetricNFWPotential(_CPotential):
+
+    # here need to cdef all the attributes
+    cdef public double M, Rh, qz
+    cdef public double G, GM, qz2
+
+    def __init__(self, double M, double Rh, double qz):
+        """ Units of everything should be in the system:
+                kpc, Myr, radian, M_sun
+        """
+        self.M = M
+        self.Rh = Rh
+        self.qz = qz
+
+        self.qz2 = self.qz*self.qz
+        self.G = 4.49975332435e-12  # kpc, Myr, Msun
+        self.GM = self.G * self.M
+
+    cdef public inline double _value(self, double[:,::1] r, int k) nogil:
+        cdef double R
+        R = r[k,0]*r[k,0] + r[k,1]*r[k,1] + r[k,2]*r[k,2]/self.qz2
+        return -self.GM / R * log(1. + R/self.Rh)
+
+    cdef public inline void _gradient(self, double[:,::1] r, double[:,::1] grad, int k) nogil:
+        cdef double R, dPhi_dR
+        R = sqrt(r[k,0]*r[k,0] + r[k,1]*r[k,1] + r[k,2]*r[k,2]/self.qz2)
+        dPhi_dR = self.GM / R / R * (log(1+R/self.Rh) - R/(R+self.Rh))
+        grad[k,0] += dPhi_dR * r[k,0] / R
+        grad[k,1] += dPhi_dR * r[k,1] / R
+        grad[k,2] += dPhi_dR * r[k,2] / R / self.qz2
+
+class Pal5AxisymmetricNFWPotential(CPotential, CartesianPotential):
+    r"""
+    Flattened, axisymmetric NFW potential Andreas used for the Pal 5 challenge.
+
+    .. math::
+
+        \Phi &=
+
+    Parameters
+    ----------
+    TODO:
+    qz : numeric
+        Flattening in Z direction.
+    units : iterable
+        Unique list of non-reducable units that specify (at minimum) the
+        length, mass, time, and angle units.
+
+    """
+    def __init__(self, M, Rh, qz, units=None):
+        self.units = units
+        parameters = dict(M=M, Rh=Rh, qz=qz)
+        super(Pal5AxisymmetricNFWPotential, self).__init__(_Pal5AxisymmetricNFWPotential,
+                                                           parameters=parameters)
