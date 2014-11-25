3b1ee7a41a01a1a510f9b4268fae9ffea06d95d6
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-11-25 16:40:41 -0500

    added naff module in dynamics package

--- misc/naff.py
+++ misc/naff.py
@@ -97,30 +97,6 @@ class NAFF(object):
             ans = simps(zreal, x=self.tz)
             return -(ans*signx*signo)/(2.*self.T)
 
-        # ----------------------------------------------------------------
-        # figure out if concave up or down
-        # vals = np.linspace(omin,omax,100)
-        # derp = np.zeros_like(vals)
-        # for i in range(1,len(vals)-1):
-        #     derp[i] = phi_w(vals[i-1]) + -2*phi_w(vals[i]) + phi_w(vals[i+1])
-
-        # if np.median(derp) < 0.:
-        #     print("concave down")
-        #     plt.title("Concave down")
-        # else:
-        #     print("concave up")
-        #     plt.title("Concave up")
-
-        # # plot
-        # vals = np.linspace(omin,omax,100)
-        # derp = np.zeros_like(vals)
-        # for i,val in enumerate(vals):
-        #     derp[i] = phi_w(val)
-        # plt.plot(vals, derp)
-        # plt.show()
-        # sys.exit(0)
-        # ----------------------------------------------------------------
-
         res = fmin_slsqp(phi_w, x0=(omax+omin)/2, acc=1E-12,
                          bounds=[(omin,omax)], disp=0, iter=50,
                          full_output=True)
@@ -130,48 +106,45 @@ class NAFF(object):
             raise ValueError("Function minimization to find best frequency failed with:\n"
                              "\t {} : {}".format(imode, smode))
 
-        return abs(freq[0])
+        return freq[0]
 
-    def frecoder(self, t, f, nvec=12):
+    def frecoder(self, f, nvec=12):
         """ Same as the subroutine FRECODER in Valluri's NAFF routines. """
 
         # initialize container arrays
         ecap = np.zeros((nvec,len(t)), dtype=np.complex64)
         nu = np.zeros(nvec)
-        ai = np.zeros(nvec, dtype=np.complex64)
-        # phi = np.zeros(nvec)
-
-        # put this in loop below if we do need phi:
-        # if ai[k].real != 0.:
-        #     phi[k] = np.arctan(ai[k].imag) / ai[k].real
-        # else:
-        #     phi[k] = 0.5*np.pi
+        A = np.zeros(nvec)
+        phi = np.zeros(nvec)
 
         fk = f.copy()
+        logger.info("-"*50)
+        logger.info("k    ωk    Ak    φk(deg)    ak")
         for k in range(nvec):
             nu[k] = self.frequency(fk)
-            logger.info("frequency ω = {} for k = {}".format(nu[k], k))
 
             if k == 0:
                 # compute exp(iωt) for first frequency
                 ecap[k] = np.cos(nu[k]*self.tz) + 1j*np.sin(nu[k]*self.tz)
+            else:
+                ecap[k] = self.gso(ecap, nu[k], k)
 
-                # get complex amplitude by projecting exp(iωt) on to f(t)
-                ai[k] = self.hanning_product(f, ecap[k])
+            # get complex amplitude by projecting exp(iωt) on to f(t)
+            ab = self.hanning_product(fk, ecap[k])
+            A[k] = np.abs(ab)
+            phi[k] = np.arctan2(ab.imag, ab.real)
 
-                # new fk has the previous frequency subtracted out
-                fk,fmax = self.sub_chi(fk, ecap[k], ai[k])
-                continue
+            # new fk has the previous frequency subtracted out
+            fk,fmax = self.sub_chi(fk, ecap[k], ab)
 
-            ecap[k] = self.gso(ecap, nu[k], k)
-            ai[k] = self.hanning_product(fk, ecap[k])
-            fk,fmax = self.sub_chi(fk, ecap[k], ai[k])
+            logger.info("{}  {:.6f}  {:.6f}  {:.2f}  {:.6f}"
+                        .format(k,nu[k],A[k],np.degrees(phi[k]),ab))
 
             # TODO: why?
-            if fmax < 1E-7 or np.abs(ai[k]) < 1E-7:
+            if fmax < 1E-7 or A[k] < 1E-6:
                 break
 
-        return nu[:k+1], ai[:k+1]
+        return nu[:k+1], A[:k+1], phi[:k+1]
 
     def hanning_product(self, u1, u2):
         # Routine to compute the scalar product of two vectors
@@ -237,81 +210,85 @@ class NAFF(object):
 
         return f_k, fmax
 
-def main(t, fs):
+    def find_fundamental_frequencies(self, w, nvec=15):
+        """ Solve for the fundamental frequencies of the given orbit, `w`.
+
+            TODO:
+        """
+
+        if w.ndim > 2:
+            raise ValueError("Input orbit must be a single orbit (have ndim=2).")
+
+        # containers
+        freqs = []
+        As = []
+        amps = []
+        phis = []
+        nqs = []
+
+        ntot = 0
+        ndim = w.shape[1]//2
+        for i in range(ndim):
+            nu,A,phi = self.frecoder(w[:,i] + 1j*w[:,i+ndim], nvec=nvec)
+            freqs.append(nu)
+            As.append(A)
+            amps.append(np.abs(A))
+            phis.append(phi)
+            nqs.append(np.zeros_like(nu) + i)
+            ntot += len(nu)
+
+        d = np.zeros(ntot, dtype=zip(('freq','A','|A|','phi','n'),
+                                     ('f8','c8','f8','f8',np.int)))
+        d['freq'] = np.concatenate(freqs)
+        d['A'] = np.concatenate(As)
+        d['|A|'] = np.concatenate(amps)
+        d['phi'] = np.concatenate(phis)
+        d['n'] = np.concatenate(nqs).astype(int)
+
+        # sort terms by amplitude
+        d = d[d['|A|'].argsort()[::-1]]
+
+        # assume largest amplitude is the first fundamental frequency
+        ffreq = np.zeros(ndim)
+        ffreq_ixes = np.zeros(ndim, dtype=int)
+        ffreq[0] = d[0]['freq']
+
+        # choose the next nontrivially related frequency as the 2nd fundamental:
+        #   TODO: why 1E-6? this isn't well described in the papers...
+        ixes = np.where((d['n'] != d[0]['n']) & ((np.abs(ffreq[0]) - np.abs(d['freq'])) > 1E-6))[0]
+        ffreq[1] = d[ixes[1]]['freq']
+        ffreq_ixes[1] = ixes[1]
+
+        # brute-force method for finding third frequency: find maximum error in
+        #   n*f1 + m*f2 - l*f3
+        n1 = np.zeros(ntot)
+        n2 = np.zeros(ntot)
+        err = np.zeros(ntot)
+
+        imax = 15
+        for i in range(ntot):
+            obji = 1E20
+            for in1 in range(-imax,imax+1,1):
+                for in2 in range(-imax,imax+1,1):
+                    # for in3 in range(-imax,imax+1,1):
+                    funi = np.abs(d[i]['freq'] - in1*ffreq[0] - in2*ffreq[1])
+                    if funi < obji:
+                        obji = funi
+                        n1[i] = in1
+                        n2[i] = in2
+                        err[i] = obji
+
+        ffreq[2] = d[err.argmax()]['freq']
+        ffreq_ixes[2] = err.argmax()
+
+        return ffreq, d, ffreq_ixes
+
+def main(t, w):
     naff = NAFF(t)
-
-    for f in fs:
-        nu_f,a_f = naff.frecoder(t, f, nvec=5)
-
-    sys.exit(0)
-
-    fq = np.abs(np.concatenate((nux, nuy, nuz)))
-    ampc = np.concatenate((aix, aiy, aiz))
-    amp = np.abs(ampc)
-    k = np.concatenate((range(nux.size),
-                        range(nuy.size),
-                        range(nuz.size)))
-    nq = np.concatenate((np.zeros_like(nux),
-                         np.ones_like(nuy),
-                         np.ones_like(nuz)+1.)).astype(int)
-
-    ktot = nux.shape[0] + nuy.shape[0] + nuz.shape[0]
-    ix = amp.argsort()[::-1]
-
-    fqs = fq[ix]
-    nq = nq[ix]
-    amp = amp[ix]
-    ampc = ampc[ix]
-
-    print(nux)
-    print(nuy)
-    print(nuz)
-    sys.exit(0)
-    print(fqs[:10])
-
-    f = np.zeros(3)
-    k3 = np.zeros(3)
-    ss = np.zeros(3, dtype=np.complex64)
-
-    f[0] = fqs[0]
-    k3[0] = nq[0]
-    ss[0] = ampc[0]
-
-    l1 = 0
-    # choose the next nontrivially related frequency as the 2nd fundamental:
-    for i in range(1,ktot):
-        if np.abs(np.abs(f[0]) - np.abs(fqs[i])) > 1E-6:
-            print(k3[0], nq[i])
-            if k3[0] != nq[i]:
-                f[1] = fqs[i]
-                k3[1] = nq[i]
-                ss[1] = ampc[i]
-                l2 = i
-                break
-
-    # brute-force method for finding third frequency
-    n1 = np.zeros_like(fqs)
-    n2 = np.zeros_like(fqs)
-    n3 = np.zeros_like(fqs)
-    err = np.zeros_like(fqs)
-
-    imax = 15
-
-    for i in range(fqs.shape[0]):
-        obji = 1E20
-        for in1 in range(-imax,imax+1,1):
-            for in2 in range(-imax,imax+1,1):
-                #for in3 in range(-imax,imax+1,1):
-                funi = np.abs(fqs[i] - in1*f[0] - in2*f[1])  #  - in3*f[2])
-                if funi < obji:
-                    obji = funi
-                    n1[i] = in1
-                    n2[i] = in2
-                    err[i] = obji
-
-    f[2] = fqs[err.argmax()]
-
+    f,d,ixes = naff.find_fundamental_frequencies(w, nvec=30)
     print(f)
+
+    return
 
     # ----------------------------------- (IP stuff)
     # # now, for each frequency in the list, try until the first unsolvable
@@ -322,6 +299,21 @@ def main(t, fs):
     # for i in range(l2,ktot):
     #     A[0,3] = -f[i]
     #     b = np.array([1.])
+
+def estimate_axisym_freqs(t, w):
+    from scipy.signal import argrelmax
+    R = np.sqrt(w[:,0,0]**2 + w[:,0,1]**2)
+    phi = np.arctan2(w[:,0,1], w[:,0,0])
+    z = w[:,0,2]
+
+    ix = argrelmax(R)[0]
+    fR = np.mean(2*np.pi / (t[ix[1:]] - t[ix[:-1]]))
+    ix = argrelmax(phi)[0]
+    fphi = np.mean(2*np.pi / (t[ix[1:]] - t[ix[:-1]]))
+    ix = argrelmax(z)[0]
+    fz = np.mean(2*np.pi / (t[ix[1:]] - t[ix[:-1]]))
+
+    return fR, fphi, fz
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -337,6 +329,8 @@ if __name__ == '__main__':
                         default=False, help="Be chatty! (default = False)")
     parser.add_argument("-q", "--quiet", action="store_true", dest="quiet",
                         default=False, help="Be quiet! (default = False)")
+    parser.add_argument("-o", "--overwrite", action="store_true", dest="overwrite",
+                        default=False, help="DESTROY. DESTROY.")
 
     # parser.add_argument("-f", dest="field_id", default=None, required=True,
     #                     type=int, help="Field ID")
@@ -381,6 +375,8 @@ if __name__ == '__main__':
 
     # fn = "orbit.npy"
     # if not os.path.exists(fn):
+    # if args.overwrite and os.path.exists(fn):
+    #     os.remove(fn)
     #     logger.debug("Integrating orbit...")
     #     potential = gp.HarmonicOscillatorPotential(freqs)
     #     t,w = potential.integrate_orbit([1,0,0.2,0.,0.1,-0.8], dt=dt, nsteps=nsteps,
@@ -390,30 +386,25 @@ if __name__ == '__main__':
     # w = wt[:6].T.copy()
     # t = wt[6,0].copy()
 
-    # fs = (w[:,0,0] + 1j*w[:,0,3],
-    #       w[:,0,1] + 1j*w[:,0,4],
-    #       w[:,0,2] + 1j*w[:,0,5])
-
     # -----------------------------------------------------------------------
 
     # -----------------------------------------------------------------------
     # Logarithmic potential, 2D orbit as in Papaphilippou & Laskar (1996):
     #   Table 1
-    fn = "orbit_log.npy"
+    # fn = "orbit_log.npy"
 
-    if not os.path.exists(fn):
-        logger.debug("Integrating orbit...")
-        potential = gp.LogarithmicPotential(v_c=np.sqrt(2.), r_h=0.1,
-                                            q1=1., q2=0.9, q3=1., units=galactic)
-        t,w = potential.integrate_orbit([0.49,0.,0.,1.3156,0.4788,0.], dt=0.005, nsteps=50000,
-                                        Integrator=gi.DOPRI853Integrator)
-        np.save(fn, np.vstack((w.T,t[np.newaxis,np.newaxis])))
-    wt = np.load(fn)
-    w = wt[:6].T.copy()
-    t = wt[6,0].copy()
-
-    fs = (w[:,0,0] + 1j*w[:,0,3],
-          w[:,0,1] + 1j*w[:,0,4])
+    # if not os.path.exists(fn):
+    # if args.overwrite and os.path.exists(fn):
+    #     os.remove(fn)
+    #     logger.debug("Integrating orbit...")
+    #     potential = gp.LogarithmicPotential(v_c=np.sqrt(2.), r_h=0.1,
+    #                                         q1=1., q2=0.9, q3=1., units=galactic)
+    #     t,w = potential.integrate_orbit([0.49,0.,0.,1.3156,0.4788,0.], dt=0.005, nsteps=50000,
+    #                                     Integrator=gi.DOPRI853Integrator)
+    #     np.save(fn, np.vstack((w.T,t[np.newaxis,np.newaxis])))
+    # wt = np.load(fn)
+    # w = wt[:6].T.copy()
+    # t = wt[6,0].copy()
 
     # -----------------------------------------------------------------------
 
@@ -423,6 +414,8 @@ if __name__ == '__main__':
     # fn = "orbit_log1d.npy"
 
     # if not os.path.exists(fn):
+    # if args.overwrite and os.path.exists(fn):
+    #     os.remove(fn)
     #     logger.debug("Integrating orbit...")
     #     potential = gp.LogarithmicPotential(v_c=np.sqrt(2.), r_h=0.1,
     #                                         q1=1., q2=0.9, q3=1., units=galactic)
@@ -433,14 +426,14 @@ if __name__ == '__main__':
     # w = wt[:6].T.copy()
     # t = wt[6,0].copy()
 
-    # fs = (w[:,0,0] + 1j*w[:,0,3], )
-
     # -----------------------------------------------------------------------
 
     # -----------------------------------------------------------------------
     # Harder? For loop orbit, align z axis with circulation?
     # fn = "orbit2.npy"
     # potential = gp.IsochronePotential(m=1E11, b=1., units=galactic)
+    # if args.overwrite and os.path.exists(fn):
+    #     os.remove(fn)
     # if not os.path.exists(fn):
     #     logger.debug("Integrating orbit...")
     #     t,w = potential.integrate_orbit([1.,0,0.1,0.,0.08,0.06], dt=0.05, nsteps=2**14,
@@ -449,10 +442,6 @@ if __name__ == '__main__':
     # wt = np.load(fn)
     # w = wt[:6].T.copy()
     # t = wt[6,0].copy()
-
-    # fs = (w[:,0,0] + 1j*w[:,0,3],
-    #       w[:,0,1] + 1j*w[:,0,4],
-    #       w[:,0,2] + 1j*w[:,0,5])
 
     # # Compute true frequencies
     # G,m,b = G.decompose(galactic).value, potential.parameters['m'], potential.parameters['b']
@@ -468,23 +457,71 @@ if __name__ == '__main__':
     # print("freq (r,φ)", freq_r, freq_phi)
     # print(np.abs(freq_r - freq_phi))
 
-    # compute frequencies with Sanders
+    # # compute frequencies with Sanders
     # actions,angles,freqs = gd.find_actions(t, w, N_max=10, units=galactic)
     # print(freqs)
     # sys.exit(0)
 
     # -----------------------------------------------------------------------
 
-    # # plot orbit
+    # -----------------------------------------------------------------------
+    # Axisymmetric
+    fn = "orbit_axisym.npy"
+    potential = gp.MiyamotoNagaiPotential(m=5e11, a=6.5, b=0.26, units=galactic)
+    w0 = [10.,0,0.,0.,0.15,0.005]
+    if args.overwrite and os.path.exists(fn):
+        os.remove(fn)
+    if not os.path.exists(fn):
+        logger.debug("Integrating orbit...")
+        t,w = potential.integrate_orbit(w0, dt=0.5, nsteps=2**15,
+                                        Integrator=gi.DOPRI853Integrator)
+        np.save(fn, np.vstack((w.T,t[np.newaxis,np.newaxis])))
+
+    t,w = potential.integrate_orbit(w0, dt=0.05, nsteps=2**15,
+                                    Integrator=gi.DOPRI853Integrator)
+    freqs = estimate_axisym_freqs(t,w)
+    print(freqs[1], freqs[1] - freqs[0], freqs[2])
+
+    wt = np.load(fn)
+    w = wt[:6].T.copy()
+    t = wt[6,0].copy()
+
+    actions,angles,freqs = gd.find_actions(t[::2], w[::2], N_max=6, units=galactic)
+    print("sanders", freqs)
+
+    # -----------------------------------------------------------------------
+
+    # -----------------------------------------------------------------------
+    # Triaxial
+    # fn = "orbit_triaxial.npy"
+    # potential = gp.LeeSutoTriaxialNFWPotential(v_h=0.5, r_h=10.,
+    #                                            a=1.3, b=1., c=0.8,
+    #                                            units=galactic)
+    # if args.overwrite and os.path.exists(fn):
+    #     os.remove(fn)
+    # if not os.path.exists(fn):
+    #     logger.debug("Integrating orbit...")
+    #     t,w = potential.integrate_orbit([10.,0,5.,0.,0.12,-0.1], dt=1., nsteps=2**15,
+    #                                     Integrator=gi.DOPRI853Integrator)
+    #     np.save(fn, np.vstack((w.T,t[np.newaxis,np.newaxis])))
+    # wt = np.load(fn)
+    # w = wt[:6].T.copy()
+    # t = wt[6,0].copy()
+
+    # actions,angles,freqs = gd.find_actions(t[::10], w[::10], N_max=6, units=galactic)
+    # print("sanders", freqs)
+
+    # -----------------------------------------------------------------------
+
+    # plot orbit
     # fig = gd.plot_orbits(w, marker='.', linestyle='none', alpha=0.2)
     # plt.show()
 
-    # # plot energy conservation
+    # plot energy conservation
     # E = potential.total_energy(w[:,0,:3],w[:,0,3:])
     # plt.semilogy(t[1:], np.abs(E[1:]-E[:-1]), marker=None)
     # plt.show()
-
     # sys.exit(0)
 
     logger.debug("Done integrating orbit, starting frequency analysis...")
-    main(t, fs)
+    main(t, w[:,0])
