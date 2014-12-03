3e6a8d73d0f745cd2aa3e9d9e6acfbe127c57030
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-12-02 23:16:11 -0500

    horribly inefficient but much more convenient

--- gary/potential/cpotential.pyx
+++ gary/potential/cpotential.pyx
@@ -67,7 +67,7 @@ class CPotential(CartesianPotential):
             Position to compute the value of the potential.
         """
         tmp = np.zeros(len(q))
-        self.c_instance.value(np.asarray(q), tmp)
+        self.c_instance.value(np.atleast_2d(q).copy(), tmp)
         return tmp
 
     def gradient(self, q):
@@ -82,7 +82,7 @@ class CPotential(CartesianPotential):
             Position to compute the gradient.
         """
         try:
-            return self.c_instance.gradient(np.asarray(q))
+            return self.c_instance.gradient(np.atleast_2d(q).copy())
         except AttributeError,TypeError:
             raise ValueError("Potential C instance has no defined "
                              "gradient function")
@@ -99,7 +99,7 @@ class CPotential(CartesianPotential):
             Position to compute the Hessian.
         """
         try:
-            return self.c_instance.hessian(np.asarray(q))
+            return self.c_instance.hessian(np.atleast_2d(q).copy())
         except AttributeError,TypeError:
             raise ValueError("Potential C instance has no defined "
                              "Hessian function")
@@ -119,7 +119,7 @@ class CPotential(CartesianPotential):
             Position to compute the acceleration.
         """
         try:
-            return -self.c_instance.gradient(np.asarray(q))
+            return -self.c_instance.gradient(np.atleast_2d(q).copy())
         except AttributeError,TypeError:
             raise ValueError("Potential C instance has no defined "
                              "gradient function")
@@ -137,7 +137,7 @@ class CPotential(CartesianPotential):
             Position to compute the mass enclosed.
         """
         try:
-            return self.c_instance.mass_enclosed(np.asarray(q))
+            return self.c_instance.mass_enclosed(np.atleast_2d(q).copy())
         except AttributeError,TypeError:
             raise ValueError("Potential C instance has no defined "
                              "mass_enclosed function")
