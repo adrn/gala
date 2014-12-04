d6f524fbc443ef1a622e9d13ec2018797f79ed0b
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-12-04 11:43:38 -0500

    test new plotting featurez

--- gary/dynamics/tests/test_plot.py
+++ gary/dynamics/tests/test_plot.py
@@ -41,11 +41,19 @@ def test_orbits():
     fig = plot_orbits(w, ix=0, linestyle='none', marker='.', alpha=0.25)
     fig.savefig(os.path.join(plot_path, "one_orbit.png"))
 
+    fig = plot_orbits(w, ix=0, linestyle='none', marker='.', alpha=0.25,
+                      labels=("herp","derp","merp"))
+    fig.savefig(os.path.join(plot_path, "one_orbit_labels.png"))
+
     fig = plot_orbits(w, triangle=True, linestyle='-', marker=None)
     fig.savefig(os.path.join(plot_path, "all_orbits_triangle.png"))
 
     fig = plot_orbits(w, ix=0, triangle=True, linestyle='-', marker=None)
     fig.savefig(os.path.join(plot_path, "one_orbit_triangle.png"))
+
+    fig = plot_orbits(w, ix=0, triangle=True, linestyle='-', marker=None,
+                      labels=("herp","derp","merp"))
+    fig.savefig(os.path.join(plot_path, "one_orbit_triangle_labels.png"))
 
 def test_three_panel():
 
@@ -64,4 +72,25 @@ def test_three_panel():
     fig = three_panel(q, relative_to=q0, triangle=True, symbol=r'\Omega')
     fig.savefig(os.path.join(plot_path, "three-panel-random-relative_triangle.png"))
 
+def test_1d():
 
+    t = np.linspace(0,100.,1000)
+    q = np.cos(2*np.pi*t/10.)
+    q = q[:,np.newaxis]
+
+    fig = plot_orbits(q, labels=(r"$\theta$",))
+    fig.savefig(os.path.join(plot_path, "1d-orbit-labels.png"))
+
+    fig = plot_orbits(q, t=t, labels=(r"$\theta$",))
+    fig.savefig(os.path.join(plot_path, "1d-orbit-labels-time.png"))
+
+def test_2d():
+
+    t = np.linspace(0,100.,1000)
+
+    q = np.zeros((len(t),1,2))
+    q[:,0,0] = np.cos(2*np.pi*t/10.)
+    q[:,0,1] = np.sin(2*np.pi*t/5.5)
+
+    fig = plot_orbits(q, labels=(r"$\theta$",r"$\omega$"))
+    fig.savefig(os.path.join(plot_path, "2d-orbit-labels.png"))
