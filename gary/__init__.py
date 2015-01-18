45b0029f23cb60b0dab4c4d8fbae7bcbeb245a53
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-01-18 00:53:02 -0500

    remove logging from namespace

--- gary/__init__.py
+++ gary/__init__.py
@@ -33,3 +33,4 @@ def debug_factory(logger, debug_level):
 logging.addLevelName(logging.INFO+5, 'IMPORTANT')
 setattr(logger, 'important', debug_factory(logger, logging.INFO+5))
 logger.setLevel(logging.INFO+1)
+del logging
