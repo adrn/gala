caf86970a5a0337971360891a19d25bcb207b902
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-01-18 20:39:18 -0500

    fix transforms to actually use input velocities...doh

--- gary/coordinates/tests/test_velocity_transforms.py
+++ gary/coordinates/tests/test_velocity_transforms.py
@@ -22,7 +22,7 @@ from ..velocity_transforms import *
 
 logger.setLevel(logging.DEBUG)
 
-class TestCartesianToAll(object):
+class TestTransforms(object):
 
     def setup(self):
         self.pos = ([[10.,0,0], [10,0,0], [4,4,0]] * u.kpc).T
@@ -30,7 +30,7 @@ class TestCartesianToAll(object):
 
         self.pos_repr = coord.CartesianRepresentation(self.pos)
 
-    def test_to_spherical(self):
+    def test_cartesian_to(self):
         for i,func in enumerate([cartesian_to_spherical,
                                  cartesian_to_physicsspherical,
                                  cartesian_to_cylindrical]):
@@ -60,3 +60,33 @@ class TestCartesianToAll(object):
 
             assert vsph1[0,2] > 0.  # vr
             assert np.sign(vsph1[2,2]) == np.sign(true_v[2,2])
+            np.testing.assert_allclose(np.sqrt(np.sum(vsph1**2,axis=0)),
+                                       np.sqrt(np.sum(true_v**2,axis=0)))
+
+    def test_to_cartesian(self):
+        for i,func in enumerate([spherical_to_cartesian,
+                                 physicsspherical_to_cartesian,
+                                 cylindrical_to_cartesian]):
+            true_v = ([[0.,100,500], [0,-100,-500], [0.,np.sqrt(2)*100,-500]] * u.km/u.s).T
+
+            # dimensionless
+            vsph1 = func(self.pos.value * u.dimensionless_unscaled,
+                         self.vel.value * u.dimensionless_unscaled)
+            assert vsph1.unit == u.dimensionless_unscaled
+
+            # astropy coordinates
+            cpos = coord.SkyCoord(self.pos_repr)
+            vsph2 = func(cpos, self.vel)
+            assert vsph2.unit == u.km/u.s
+
+            # astropy representation
+            vsph3 = func(self.pos_repr, self.vel)
+            assert vsph3.unit == u.km/u.s
+
+            np.testing.assert_allclose(vsph1, vsph2, atol=1E-10)
+            np.testing.assert_allclose(vsph1, vsph3, atol=1E-10)
+
+            if func == physicsspherical_to_cartesian:
+                true_v[2] *= -1.
+
+            np.testing.assert_allclose(vsph1, true_v, atol=1E-10)
