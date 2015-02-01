a8d7ce38845afed04cf6f654e17850feb39e6cea
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-01-31 20:04:23 -0500

    import

--- gary/potential/builtin.py
+++ gary/potential/builtin.py
@@ -4,9 +4,6 @@ from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
-# Standard library
-import math
-
 # Third-party
 import numpy as np
 from astropy.constants import G
