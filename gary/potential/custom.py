e2a089c778ebefcda6fb47de33c64e75dbfdf5d2
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-02-02 00:08:03 -0500

    new way to handle dicts of dicts for kwargs

--- gary/potential/custom.py
+++ gary/potential/custom.py
@@ -22,11 +22,24 @@ __all__ = ['PW14Potential', 'LM10Potential', 'TriaxialMWPotential']
 
 class PW14Potential(CompositePotential):
 
-    def __init__(self, units=galactic,
-                 disk=dict(m=6.5E10, a=6.5, b=0.26),
-                 bulge=dict(m=2E10, c=0.3),
-                 halo=dict(a=1.4, b=1., c=0.6, v_c=0.247, r_s=30.,
-                           phi=np.pi/2., theta=np.pi/2., psi=np.pi/2.)):
+    def __init__(self, units=galactic, disk=dict(), bulge=dict(), halo=dict()):
+
+        default_disk = dict(m=6.5E10, a=6.5, b=0.26)
+        default_bulge = dict(m=2E10, c=0.3)
+        default_halo = dict(a=1.4, b=1., c=0.6, v_c=0.247, r_s=30.,
+                            phi=np.pi/2., theta=np.pi/2., psi=np.pi/2.)
+
+        for k,v in default_disk.items():
+            if k not in disk:
+                disk[k] = v
+
+        for k,v in default_bulge.items():
+            if k not in bulge:
+                bulge[k] = v
+
+        for k,v in default_halo.items():
+            if k not in halo:
+                halo[k] = v
 
         kwargs = dict()
         kwargs["disk"] = MiyamotoNagaiPotential(units=units, **disk)
@@ -43,13 +56,25 @@ class PW14Potential(CompositePotential):
 
 class LM10Potential(CompositePotential):
 
-    def __init__(self, units=galactic,
-                 disk=dict(m=1E11, a=6.5, b=0.26),
-                 bulge=dict(m=3.4E10, c=0.7),
-                 halo=dict(q1=1.38, q2=1., q3=1.36,
-                           phi=(97*u.degree).to(u.radian).value,
-                           v_c=np.sqrt(2)*(121.858*u.km/u.s).to(u.kpc/u.Myr).value,
-                           r_h=12.)):
+    def __init__(self, units=galactic, disk=dict(), bulge=dict(), halo=dict()):
+
+        default_disk = dict(m=1E11, a=6.5, b=0.26)
+        default_bulge = dict(m=3.4E10, c=0.7)
+        default_halo = dict(q1=1.38, q2=1., q3=1.36, r_h=12.,
+                            phi=(97*u.degree).to(u.radian).value,
+                            v_c=np.sqrt(2)*(121.858*u.km/u.s).to(u.kpc/u.Myr).value)
+
+        for k,v in default_disk.items():
+            if k not in disk:
+                disk[k] = v
+
+        for k,v in default_bulge.items():
+            if k not in bulge:
+                bulge[k] = v
+
+        for k,v in default_halo.items():
+            if k not in halo:
+                halo[k] = v
 
         kwargs = dict()
         kwargs["disk"] = MiyamotoNagaiPotential(units=units, **disk)
@@ -60,15 +85,29 @@ class LM10Potential(CompositePotential):
 class TriaxialMWPotential(CompositePotential):
 
     def __init__(self, units=galactic,
-                 disk=dict(m=7E10, a=3.5, b=0.14),
-                 bulge=dict(m=1E10, c=1.1),
-                 halo=dict(a=1., b=0.75, c=0.55,
-                           v_c=0.239225, r_s=30.,
-                           phi=0., theta=0., psi=0.)):
+                 disk=dict(), bulge=dict(), halo=dict()):
         """ Axis ratio values taken from Jing & Suto (2002). Other
             parameters come from a by-eye fit to Bovy's MW2014Potential.
             Choice of v_c sets circular velocity at Sun to 220 km/s
         """
+
+        default_disk = dict(m=7E10, a=3.5, b=0.14)
+        default_bulge = dict(m=1E10, c=1.1)
+        default_halo = dict(a=1., b=0.75, c=0.55,
+                            v_c=0.239225, r_s=30.,
+                            phi=0., theta=0., psi=0.)
+
+        for k,v in default_disk.items():
+            if k not in disk:
+                disk[k] = v
+
+        for k,v in default_bulge.items():
+            if k not in bulge:
+                bulge[k] = v
+
+        for k,v in default_halo.items():
+            if k not in halo:
+                halo[k] = v
 
         kwargs = dict()
         kwargs["disk"] = MiyamotoNagaiPotential(units=units, **disk)
