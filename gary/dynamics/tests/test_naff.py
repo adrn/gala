0f8316166918566069067d74b07d08369046ec90
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-02-18 14:27:04 -0500

    added debug option to naff

--- gary/dynamics/tests/test_naff.py
+++ gary/dynamics/tests/test_naff.py
@@ -465,3 +465,23 @@ class TestInterAxisTube(MonicaBase):
 class TestShortAxisTube(MonicaBase):
     def setup_class(self):
         self.filename = 'short-axis-tube.txt'
+
+# ------------------------------------------------------------------------------
+
+def test_weird_bump():
+    t = np.load(os.path.join(test_data_path, "naff/t.npy"))
+    w = np.load(os.path.join(test_data_path, "naff/w.npy"))
+
+    naff = NAFF(t[0:200000], debug=True,
+                debug_path=os.path.join(plot_path, "debug-naff-window1"))
+    fs = poincare_polar(w[0:200000,0])
+    f,d,ixes = naff.find_fundamental_frequencies(fs, nintvec=2,
+                                                 break_condition=None)
+
+    # window 2
+    naff = NAFF(t[100000:300000], debug=True,
+                debug_path=os.path.join(plot_path, "debug-naff-window2"))
+    fs = poincare_polar(w[100000:300000,0])
+    f,d,ixes = naff.find_fundamental_frequencies(fs, nintvec=2,
+                                                 break_condition=None)
+
