e5e98d33a7280aa20ab781d15e1a043608492cdf
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-02-24 00:19:30 -0500

    rearranging how potential C stuff is defined

---
+++ gary/potential/_cbuiltin.c
@@ -0,0 +1,20 @@
+#include <math.h>
+
+/*
+    Hernquist spheroid potential from Hernquist (1990)
+*/
+double hernquist_value(double *pars, double *q) {
+    double R;
+    R = sqrt(q[0]*q[0] + q[1]*q[1] + q[2]*q[2]);
+    return -pars[0] * pars[1] / (R + pars[2]);
+}
+
+void hernquist_gradient(double *pars, double *q, double *grad) {
+    double R, fac;
+    R = sqrt(q[0]*q[0] + q[1]*q[1] + q[2]*q[2]);
+    fac = pars[0] * pars[1] / ((R + pars[2]) * (R + pars[2]) * R);
+
+    grad[0] = fac*q[0];
+    grad[1] = fac*q[1];
+    grad[2] = fac*q[2];
+}
