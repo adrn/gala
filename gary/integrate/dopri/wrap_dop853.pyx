c5f6e71a73966436450f1d35b5ecdf700dac1ea7
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-02-24 17:54:45 -0500

    modified to take my grad function stuff

--- gary/integrate/dopri/wrap_dop853.pyx
+++ gary/integrate/dopri/wrap_dop853.pyx
@@ -22,12 +22,14 @@ cdef extern from "math.h":
     double sqrt(double x) nogil
 
 cdef extern from "dop853.h":
+    ctypedef void (*GradFn)(double *pars, double *q, double *grad);
     ctypedef void (*SolTrait)(long nr, double xold, double x, double* y, unsigned n, int* irtrn)
-    ctypedef void (*FcnEqDiff)(unsigned n, double x, double *y, double *f);
+    ctypedef void (*FcnEqDiff)(unsigned n, double x, double *y, double *f, GradFn gradfunc, double *gpars);
     double contd8 (unsigned ii, double x)
 
     # See dop853.h for full description of all input parameters
-    int dop853 (unsigned n, FcnEqDiff fcn, double x, double* y, double xend,
+    int dop853 (unsigned n, FcnEqDiff fcn, GradFn gradfunc, double *gpars,
+                double x, double* y, double xend,
                 double* rtoler, double* atoler, int itoler, SolTrait solout,
                 int iout, FILE* fileout, double uround, double safe, double fac1,
                 double fac2, double beta, double hmax, double h, long nmax, int meth,
@@ -44,22 +46,13 @@ cdef extern from "stdio.h":
     ctypedef struct FILE
     FILE *stdout
 
-# custom typedef
-ctypedef void (*GradFn)(double *pars, double *q, double *grad);
-
 cdef void F(unsigned ndim, double t, double *w, double *f,
             GradFn func, double *pars):
-    cdef int i, half_ndim
-    half_ndim = ndim / 2
-    print("ndim", ndim)
-    print("half_ndim", half_ndim)
+    cdef int k
+    cdef unsigned half_ndim = ndim / 2
 
     # call gradient function
     func(pars, w, &f[half_ndim])
-    print("no f")
-    print(f[0],f[1],f[2])
-    print(f[3],f[4],f[5])
-    print()
 
     for k in range(half_ndim):
         f[k] = w[k+half_ndim]
@@ -86,31 +79,25 @@ cpdef main(_CPotential cpotential, double[::1] w0):
     cdef double[::1] w = w0.copy()
     cdef double[::1] f = np.zeros(ndim)
 
-    for i in range(ndim):
-        print(w[i], f[i])
-    print()
+    F(ndim, 0., &w[0], &f[0],
+      <GradFn>cpotential.c_gradient, &(cpotential._parameters[0]))
 
-    F(ndim, 0., &w[0], &f[0], <GradFn>cpotential.c_gradient, &(cpotential._parameters[0]))
+    iout = 2
+    x = 0.0
+    xend = 1000.0
+    itoler = 0
+    rtoler = 1.0E-6
+    atoler = rtoler
 
-    for i in range(ndim):
-        print(w[i], f[i])
+    print("winding up")
 
+    res = dop853(ndim, F, <GradFn>cpotential.c_gradient, &(cpotential._parameters[0]),
+        x, &w[0], xend, &rtoler, &atoler, itoler, solout, iout,
+        stdout, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0, 0, 1, ndim, NULL, 0);
 
-    # iout = 2
-    # x = 0.0
-    # xend = 1000.0
-    # itoler = 0
-    # rtoler = 1.0E-6
-    # atoler = rtoler
+    print("End w: {}".format(np.array(w)))
+    # print("x=xend  y={0:12.10f} {1:12.10f}".format(y[0], y[1]))
+    # printf ("rtol=%12.10f   fcn=%li   step=%li   accpt=%li   rejct=%li\r\n",
+    #   rtoler, nfcnRead(), nstepRead(), naccptRead(), nrejctRead());
 
-    # print("winding up")
-
-    # res = dop853 (ndim, F, x, &w[0], xend, &rtoler, &atoler, itoler, solout, iout,
-    #     stdout, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0, 0, 1, ndim, NULL, 0);
-
-    # print("End w: {}".format(np.array(w)))
-    # # print("x=xend  y={0:12.10f} {1:12.10f}".format(y[0], y[1]))
-    # # printf ("rtol=%12.10f   fcn=%li   step=%li   accpt=%li   rejct=%li\r\n",
-    # #   rtoler, nfcnRead(), nstepRead(), naccptRead(), nrejctRead());
-
-    # # return 0;
+    return 0;
