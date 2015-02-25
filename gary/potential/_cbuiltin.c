63f40b246ebf07082b98534d79137e03deb62f81
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-02-24 23:58:11 -0500

    implemented some spherical

--- gary/potential/_cbuiltin.c
+++ gary/potential/_cbuiltin.c
@@ -1,7 +1,7 @@
 #include <math.h>
 
-/*
-    Hernquist spheroid potential from Hernquist (1990)
+/* ---------------------------------------------------------------------------
+    Hernquist sphere
 */
 double hernquist_value(double *pars, double *q) {
     /*  pars:
@@ -14,18 +14,74 @@ double hernquist_value(double *pars, double *q) {
     return -pars[0] * pars[1] / (R + pars[2]);
 }
 
-void hernquist_gradient(double *pars, double *q, double *grad) {
+void hernquist_gradient(double *pars, double *r, double *grad) {
     /*  pars:
             - G (Gravitational constant)
             - m (mass scale)
             - c (length scale)
     */
     double R, fac;
-    R = sqrt(q[0]*q[0] + q[1]*q[1] + q[2]*q[2]);
+    R = sqrt(r[0]*r[0] + r[1]*r[1] + r[2]*r[2]);
     fac = pars[0] * pars[1] / ((R + pars[2]) * (R + pars[2]) * R);
 
-    grad[0] = fac*q[0];
-    grad[1] = fac*q[1];
-    grad[2] = fac*q[2];
+    grad[0] = fac*r[0];
+    grad[1] = fac*r[1];
+    grad[2] = fac*r[2];
 }
 
+/* ---------------------------------------------------------------------------
+    Plummer sphere
+*/
+double plummer_value(double *pars, double *r) {
+    /*  pars:
+            - G (Gravitational constant)
+            - m (mass scale)
+            - b (length scale)
+    */
+    double R2 = r[0]*r[0] + r[1]*r[1] + r[2]*r[2];
+    return -pars[0]*pars[1] / sqrt(R2 + pars[2]*pars[2]);
+}
+
+void plummer_gradient(double *pars, double *r, double *grad) {
+    /*  pars:
+            - G (Gravitational constant)
+            - m (mass scale)
+            - b (length scale)
+    */
+    double R2b, fac;
+    R2b = r[0]*r[0] + r[1]*r[1] + r[2]*r[2] + pars[2]*pars[2];
+    fac = pars[0] * pars[1] / sqrt(R2b) / R2b;
+
+    grad[0] = fac*r[0];
+    grad[1] = fac*r[1];
+    grad[2] = fac*r[2];
+}
+
+/* ---------------------------------------------------------------------------
+    Jaffe sphere
+*/
+double jaffe_value(double *pars, double *r) {
+    /*  pars:
+            - G (Gravitational constant)
+            - m (mass scale)
+            - c (length scale)
+    */
+    double R;
+    R = sqrt(r[0]*r[0] + r[1]*r[1] + r[2]*r[2]);
+    return pars[0] * pars[1] / pars[2] * log(R / (R + pars[2]));
+}
+
+void jaffe_gradient(double *pars, double *r, double *grad){
+    /*  pars:
+            - G (Gravitational constant)
+            - m (mass scale)
+            - c (length scale)
+    */
+    double R, fac;
+    R = sqrt(r[0]*r[0] + r[1]*r[1] + r[2]*r[2]);
+    fac = pars[0] * pars[1] / ((R + pars[2]) * R * R);
+
+    grad[0] = fac*r[0];
+    grad[1] = fac*r[1];
+    grad[2] = fac*r[2];
+}
--- gary/potential/_cbuiltin.h
+++ gary/potential/_cbuiltin.h
@@ -1,2 +1,8 @@
 extern double hernquist_value(double *pars, double *q);
 extern void hernquist_gradient(double *pars, double *q, double *grad);
+
+extern double plummer_value(double *pars, double *q);
+extern void plummer_gradient(double *pars, double *q, double *grad);
+
+extern double jaffe_value(double *pars, double *q);
+extern void jaffe_gradient(double *pars, double *q, double *grad);
