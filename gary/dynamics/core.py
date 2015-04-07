487ade3dfc36b0306ca91e032f6f2699a17198b8
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-04-07 19:26:36 -0400

    added function for estimating period of input time series

--- gary/dynamics/core.py
+++ gary/dynamics/core.py
@@ -9,6 +9,7 @@ __author__ = "adrn <adrn@astro.columbia.edu>"
 # Third-party
 import numpy as np
 from astropy import log as logger
+from scipy.signal import argrelmax, argrelmin
 
 __all__ = ['angular_momentum', 'classify_orbit', 'align_circulation_with_z', 'check_for_primes']
 
@@ -191,3 +192,48 @@ def check_for_primes(n, max_prime=41):
         n = check_for_primes(n)
 
     return n
+
+def peak_to_peak_period(t, f, tol=1E-2):
+    """
+    Estimate the period of the input time series by measuring the average
+    peak-to-peak time.
+
+    Parameters
+    ----------
+    t : array_like
+        Time grid aligned with the input time series.
+    f : array_like
+        A periodic time series.
+
+    Returns
+    -------
+    period : float
+        The mean peak-to-peak period.
+    """
+
+    # find peaks
+    max_ix = argrelmax(f, mode='wrap')[0]
+    max_ix = max_ix[(max_ix != 0) & (max_ix != (len(f)-1))]
+
+    # find troughs
+    min_ix = argrelmin(f, mode='wrap')[0]
+    min_ix = min_ix[(min_ix != 0) & (min_ix != (len(f)-1))]
+
+    # neglect minor oscillations
+    if abs(np.mean(f[max_ix]) - np.mean(f[min_ix])) < tol:
+        return np.nan
+
+    # compute mean peak-to-peak
+    if len(max_ix) > 0:
+        T_max = np.mean(t[max_ix[1:]] - t[max_ix[:-1]])
+    else:
+        T_max = np.nan
+
+    # now compute mean trough-to-trough
+    if len(min_ix) > 0:
+        T_min = np.mean(t[min_ix[1:]] - t[min_ix[:-1]])
+    else:
+        T_min = np.nan
+
+    # then take the mean of these two
+    return np.mean([T_max, T_min])
