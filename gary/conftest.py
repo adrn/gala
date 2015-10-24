1594556dd4dfd2e9ba712ac640f935a847932fe9
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-10-24 19:24:31 -0400

    moving to package template

--- gary/conftest.py
+++ gary/conftest.py
@@ -1,24 +1,34 @@
-def pytest_addoption(parser):
-    """ add a command line option """
-    parser.addoption("--overwrite", action="store_true", default=False,
-                     help="Destroy.")
+# this contains imports plugins that configure py.test for astropy tests.
+# by importing them here in conftest.py they are discoverable by py.test
+# no matter how it is invoked within the source tree.
 
-def pytest_configure(config):
-    """ called after command line options have been parsed
-        and all plugins and initial conftest files been loaded.
-    """
+from astropy.tests.pytest_plugins import *
 
-    import logging
-    from astropy import log as logger
+## Uncomment the following line to treat all DeprecationWarnings as
+## exceptions
+# enable_deprecations_as_exceptions()
 
-    if config.getoption('verbose') == 2:
-        logger.setLevel(logging.DEBUG)
+## Uncomment and customize the following lines to add/remove entries
+## from the list of packages for which version numbers are displayed
+## when running the tests
+# try:
+#     PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
+#     PYTEST_HEADER_MODULES['scikit-image'] = 'skimage'
+#     del PYTEST_HEADER_MODULES['h5py']
+# except NameError:  # needed to support Astropy < 1.0
+#     pass
 
-    elif config.getoption('verbose') == 1:
-        logger.setLevel(logging.INFO)
-
-    elif config.getoption('quiet'):
-        logger.setLevel(logging.WARN)
-
-    else:
-        logger.setLevel(logging.INFO+1)
+## Uncomment the following lines to display the version number of the
+## package rather than the version number of Astropy in the top line when
+## running the tests.
+# import os
+#
+## This is to figure out the affiliated package version, rather than
+## using Astropy's
+# from . import version
+#
+# try:
+#     packagename = os.path.basename(os.path.dirname(__file__))
+#     TESTED_VERSIONS[packagename] = version.version
+# except NameError:   # Needed to support Astropy <= 1.0.0
+#     pass
