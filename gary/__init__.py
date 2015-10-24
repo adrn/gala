3272419e50f8b0393d1f80292d5236bada122345
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-10-24 19:24:31 -0400

    moving to package template

--- gary/__init__.py
+++ gary/__init__.py
@@ -1,40 +1,30 @@
-# Monkey-patch Quantity
-import astropy.units as u
+"""
+Gary.
+"""
 
-@classmethod
-def _parse_quantity(cls, q):
-    try:
-        val,unit = q.split()
-    except:
-        val = q
-        unit = u.dimensionless_unscaled
+from ._astropy_init import *
 
-    return u.Quantity(float(val), unit)
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from . import coordinates
+    from . import dynamics
+    from . import integrate
+    from . import io
+    from . import potential
 
-u.Quantity.from_string = _parse_quantity
+    from .potential.custom import stuff
+    exec(stuff)
 
-from . import coordinates
-from . import dynamics
-from . import integrate
-from . import io
-from . import potential
+    # Monkey-patch Quantity
+    import astropy.units as u
 
-# Add a custom log level
-from astropy import log as logger
-import logging
+    @classmethod
+    def _parse_quantity(cls, q):
+        try:
+            val,unit = q.split()
+        except:
+            val = q
+            unit = u.dimensionless_unscaled
 
-def debug_factory(logger, debug_level):
-    def custom_debug(msg, *args, **kwargs):
-        if logger.level >= debug_level:
-           return
-        logger._log(debug_level, msg, args, kwargs)
-    return custom_debug
-
-logging.addLevelName(logging.INFO+5, 'IMPORTANT')
-setattr(logger, 'important', debug_factory(logger, logging.INFO+5))
-logger.setLevel(logging.INFO+1)
-
-from potential.custom import stuff
-exec(stuff)
-
-del logging, debug_factory, u
+        return u.Quantity(float(val), unit)
+    u.Quantity.from_string = _parse_quantity
