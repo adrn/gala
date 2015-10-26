d3b1e113caa7625663d3f28528ed76f603e34c63
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-10-25 21:36:45 -0400

    copy pasta mistake

--- gary/potential/setup_package.py
+++ gary/potential/setup_package.py
@@ -29,7 +29,7 @@ def get_extensions():
     cfg['include_dirs'].append('numpy')
     cfg['include_dirs'].append(mac_incl_path)
     cfg['extra_compile_args'].append('--std=gnu99')
-    cfg['sources'].append('gary/potential/cpotential.pyx')
+    cfg['sources'].append('gary/potential/cbuiltin.pyx')
     cfg['sources'].append('gary/potential/_cbuiltin.c')
     exts.append(Extension('gary.potential.cbuiltin', **cfg))
 
