5c62237ebcb39515bcbf9c6bb2d673622e56bf6e
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-15 17:59:14 -0500

    switch to matrix transforms

--- gary/coordinates/orphan.py
+++ gary/coordinates/orphan.py
@@ -56,52 +56,19 @@ psi = np.radians(90.70)
 D = rotation_matrix(phi, "z", unit=u.radian)
 C = rotation_matrix(theta, "x", unit=u.radian)
 B = rotation_matrix(psi, "z", unit=u.radian)
-sgr_matrix = np.array(B.dot(C).dot(D))
+R = np.array(B.dot(C).dot(D))
 
-# Galactic to Sgr coordinates
-@frame_transform_graph.transform(coord.FunctionTransform, coord.Galactic, Orphan)
-def galactic_to_orp(gal_coord, sgr_frame):
+@frame_transform_graph.transform(coord.StaticMatrixTransform, coord.Galactic, Orphan)
+def galactic_to_orp():
     """ Compute the transformation from Galactic spherical to
-        heliocentric Sgr coordinates.
+        heliocentric Orphan coordinates.
     """
+    return R
 
-    l = np.atleast_1d(gal_coord.l.radian)
-    b = np.atleast_1d(gal_coord.b.radian)
-
-    X = np.cos(b)*np.cos(l)
-    Y = np.cos(b)*np.sin(l)
-    Z = np.sin(b)
-
-    # Calculate X,Y,Z,distance in the Sgr system
-    Xs, Ys, Zs = sgr_matrix.dot(np.array([X, Y, Z]))
-
-    # Calculate the angular coordinates lambda,beta
-    Lambda = np.arctan2(Ys, Xs)*u.radian
-    Lambda[Lambda < 0] = Lambda[Lambda < 0] + 2.*np.pi*u.radian
-    Beta = np.arcsin(Zs/np.sqrt(Xs*Xs+Ys*Ys+Zs*Zs))*u.radian
-
-    return Orphan(Lambda=Lambda, Beta=Beta,
-                  distance=gal_coord.distance)
-
-
-# Sgr to Galactic coordinates
-@frame_transform_graph.transform(coord.FunctionTransform, Orphan, coord.Galactic)
-def orp_to_galactic(orp_coord, gal_frame):
-    """ Compute the transformation from heliocentric Sgr coordinates to
+# Oph to Galactic coordinates
+@frame_transform_graph.transform(coord.StaticMatrixTransform, Orphan, coord.Galactic)
+def oph_to_galactic():
+    """ Compute the transformation from heliocentric Orphan coordinates to
         spherical Galactic.
     """
-    L = np.atleast_1d(orp_coord.Lambda.radian)
-    B = np.atleast_1d(orp_coord.Beta.radian)
-
-    Xs = np.cos(B)*np.cos(L)
-    Ys = np.cos(B)*np.sin(L)
-    Zs = np.sin(B)
-
-    X, Y, Z = sgr_matrix.T.dot(np.array([Xs, Ys, Zs]))
-
-    l = np.arctan2(Y, X)*u.radian
-    b = np.arcsin(Z/np.sqrt(X*X+Y*Y+Z*Z))*u.radian
-
-    l[l<0] += 2*np.pi*u.radian
-
-    return coord.Galactic(l=l, b=b, distance=orp_coord.distance)
+    return galactic_to_orp().T
