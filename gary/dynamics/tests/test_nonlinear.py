1fdabe824f93b1c24527991b999ea16ba64fce5f
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-23 11:42:33 -0500

    don't just rely on plots

--- gary/dynamics/tests/test_nonlinear.py
+++ gary/dynamics/tests/test_nonlinear.py
@@ -6,11 +6,9 @@ __author__ = "adrn <adrn@astro.columbia.edu>"
 
 # Standard library
 import os
-import sys
 
 # Third-party
-import astropy.units as u
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
 import numpy as np
 import pytest
 
@@ -21,18 +19,20 @@ from ...integrate import DOPRI853Integrator
 from ...util import gram_schmidt
 from ...units import galactic
 
-def test_gram():
+def test_gram_schmidt():
     arr = np.array([
-      [0.159947293111244, -0.402071263039210, 0.781989928439469, 0.157200868935014],
-      [-0.641365729386551, -0.589502248965056, -6.334333712737469E-002, -0.787834065229250],
-      [-0.734648540580147, 0.290410423487680, 0.200395494571665, 0.547331391068177],
-      [0.140516164496874, -0.649328455579330, -0.621863558066490, 0.402036027551737]])
+        [0.159947293111244, -0.402071263039210, 0.781989928439469, 0.157200868935014],
+        [-0.641365729386551, -0.589502248965056, -6.334333712737469E-002, -0.787834065229250],
+        [-0.734648540580147, 0.290410423487680, 0.200395494571665, 0.547331391068177],
+        [0.140516164496874, -0.649328455579330, -0.621863558066490, 0.402036027551737]
+    ])
 
     fortran_end_arr = np.array([
         [0.176260247376319, -0.443078334791891, 0.861744738228658, 0.173233716603048],
         [-0.539192290034099, -0.517682965825117, -2.242518016864357E-002, -0.663907472888126],
         [-0.823493801368422, 0.237505395299283, 0.194657453290375, 0.477030001352140],
-        [7.871383004849420E-003, -0.692298435168163, -0.467976060614355, 0.549235217994234]])
+        [7.871383004849420E-003, -0.692298435168163, -0.467976060614355, 0.549235217994234]
+    ])
 
     alf = gram_schmidt(arr)
     fortran_alf = np.array([0.907449612105405,1.17546413803123,0.974054532627089,0.962464733634354])
@@ -58,73 +58,47 @@ class TestForcedPendulum(object):
         self.chaotic_integrator = DOPRI853Integrator(F, func_args=self.chaotic_par)
 
     def test_lyapunov_max(self, tmpdir):
-        nsteps = 100000
-        dt = 0.1
+        nsteps = 20000
+        dt = 1.
         nsteps_per_pullback = 10
         d0 = 1e-5
-        noffset = 8
+        noffset = 2
 
         regular_LEs, t, regular_ws = lyapunov_max(self.regular_w0, self.regular_integrator,
                                                   dt=dt, nsteps=nsteps,
                                                   d0=d0, nsteps_per_pullback=nsteps_per_pullback,
                                                   noffset_orbits=noffset)
         regular_LEs = np.mean(regular_LEs, axis=1)
+        assert regular_LEs[-1] < 1E-3
 
         chaotic_LEs, t, chaotic_ws = lyapunov_max(self.chaotic_w0, self.chaotic_integrator,
                                                   dt=dt, nsteps=nsteps,
                                                   d0=d0, nsteps_per_pullback=nsteps_per_pullback,
                                                   noffset_orbits=noffset)
         chaotic_LEs = np.mean(chaotic_LEs, axis=1)
+        assert chaotic_LEs[-1] > 1E-2
 
-        plt.clf()
-        plt.loglog(regular_LEs, marker=None)
-        plt.savefig(os.path.join(str(tmpdir),"pend_regular.png"))
+        pl.figure()
+        pl.loglog(regular_LEs, marker=None)
+        pl.savefig(os.path.join(str(tmpdir),"pend_regular.png"))
 
-        plt.clf()
-        plt.plot(t, regular_ws[:,0], marker=None)
-        plt.savefig(os.path.join(str(tmpdir),"pend_orbit_regular.png"))
+        pl.figure()
+        pl.plot(t, regular_ws[:,0], marker=None)
+        pl.savefig(os.path.join(str(tmpdir),"pend_orbit_regular.png"))
 
-        plt.clf()
-        plt.loglog(chaotic_LEs, marker=None)
-        plt.savefig(os.path.join(str(tmpdir),"pend_chaotic.png"))
+        pl.figure()
+        pl.loglog(chaotic_LEs, marker=None)
+        pl.savefig(os.path.join(str(tmpdir),"pend_chaotic.png"))
 
-        plt.clf()
-        plt.plot(t, chaotic_ws[:,0], marker=None)
-        plt.savefig(os.path.join(str(tmpdir),"pend_orbit_chaotic.png"))
+        pl.figure()
+        pl.plot(t, chaotic_ws[:,0], marker=None)
+        pl.savefig(os.path.join(str(tmpdir),"pend_orbit_chaotic.png"))
 
-    def test_frequency(self, tmpdir):
-        import scipy.signal as ss
-        nsteps = 10000
-        dt = 0.1
-
-        t,w = self.regular_integrator.run(self.regular_w0, dt=dt, nsteps=nsteps)
-        f,fft = fft_orbit(t, w)
-
-        peak_ix = ss.find_peaks_cwt(fft[:,0], widths=np.linspace(dt*2, dt*100, 10))
-        print(peak_ix)
-
-        plt.clf()
-        plt.axvline(self.regular_par[1]/(2*np.pi), linewidth=3., alpha=0.35, color='b')
-        plt.axvline(1/(2*np.pi), linewidth=3., alpha=0.35, color='r')
-        plt.semilogx(f[:,0], fft[:,0], marker=None)
-        plt.savefig(os.path.join(str(tmpdir),"pend_fft_regular.png"))
-
-        # ----------------------------------------------------------------------
-        t,w = self.chaotic_integrator.run(self.chaotic_w0, dt=dt, nsteps=nsteps)
-        f,fft = fft_orbit(t, w)
-
-        peak_ix = ss.find_peaks_cwt(fft[:,0], widths=np.linspace(dt*2, dt*100, 10))
-        print(peak_ix)
-
-        plt.clf()
-        plt.axvline(self.chaotic_par[1]/(2*np.pi), linewidth=3., alpha=0.35, color='b')
-        plt.axvline(1/(2*np.pi), linewidth=3., alpha=0.35, color='r')
-        plt.semilogx(f[:,0], fft[:,0], marker=None)
-        plt.savefig(os.path.join(str(tmpdir),"pend_fft_chaotic.png"))
+        pl.close('all')
 
 # --------------------------------------------------------------------
 
-class TestHenonHeiles(object):
+class HenonHeilesBase(object):
 
     def potential(self,w,A,B,C,D):
         x,y = w[...,:2].T
@@ -164,62 +138,82 @@ class TestHenonHeiles(object):
     def setup(self):
         # parameter choices
         self.par = (1.,1.,1.,1.)
+        self.nsteps = 2000
+        self.dt = 2.
 
-        # initial conditions from LP-VI documentation:
-        self.w0s = np.array([[0.,0.295456,0.407308431,0.], # stable, period 1
-                             [0., 0.483, 0.27898039, 0.], # stable, quasi-periodic
-                             [0., 0.46912, 0.291124891, 0.], # quasi-periodic, close to unstable
-                             [0., 0.509, 0.254624859, 0.], # irregular
-                             [0., 0.56, 0.164113781, 0.112]]) # irregular
-
-        self.nsteps = 10000
-        self.dt = 1.
-
-    def test_orbit(self, tmpdir):
+    def test_integrate_orbit(self, tmpdir):
         integrator = DOPRI853Integrator(self.F_max, func_args=self.par)
 
-        plt.clf()
-        for ii,w0 in enumerate(self.w0s):
-            t,w = integrator.run(w0, dt=self.dt, nsteps=self.nsteps)
-            plt.plot(w[:,0,0], w[:,0,1], marker=None)
+        pl.clf()
+        t,w = integrator.run(self.w0, dt=self.dt, nsteps=self.nsteps)
+        pl.plot(w[:,0,0], w[:,0,1], marker=None)
 
-        plt.savefig(os.path.join(str(tmpdir),"hh_orbits.png"))
+        pl.savefig(os.path.join(str(tmpdir),"hh_orbit_{}.png".format(self.__class__.__name__)))
 
     def test_lyapunov_max(self, tmpdir):
         nsteps_per_pullback = 10
         d0 = 1e-5
-        noffset = 8
+        noffset = 2
 
         integrator = DOPRI853Integrator(self.F_max, func_args=self.par)
-        for ii,w0 in enumerate(self.w0s):
-            lyap, t, ws = lyapunov_max(w0, integrator,
-                                       dt=self.dt, nsteps=self.nsteps,
-                                       d0=d0, noffset_orbits=noffset,
-                                       nsteps_per_pullback=nsteps_per_pullback)
-            lyap = np.mean(lyap, axis=1)
+        lyap, t, ws = lyapunov_max(self.w0, integrator,
+                                   dt=self.dt, nsteps=self.nsteps,
+                                   d0=d0, noffset_orbits=noffset,
+                                   nsteps_per_pullback=nsteps_per_pullback)
+        lyap = np.mean(lyap, axis=1)
 
-            plt.clf()
-            plt.loglog(lyap, marker=None)
-            plt.savefig(os.path.join(str(tmpdir),"hh_lyap_max_{}.png".format(ii)))
+        pl.clf()
+        pl.loglog(lyap, marker=None)
+        pl.savefig(os.path.join(str(tmpdir),"hh_lyap_max_{}.png".format(self.__class__.__name__)))
 
-            plt.clf()
-            plt.plot(ws[...,0], ws[...,1], marker=None)
-            plt.savefig(os.path.join(str(tmpdir),"hh_orbit_lyap_max_{}.png".format(ii)))
+        pl.clf()
+        pl.plot(ws[...,0], ws[...,1], marker=None)
+        pl.savefig(os.path.join(str(tmpdir),"hh_orbit_lyap_max_{}.png".format(self.__class__.__name__)))
 
     def test_lyapunov_spectrum(self, tmpdir):
 
         integrator = DOPRI853Integrator(self.F_spec, func_args=self.par)
-        for ii,w0 in enumerate(self.w0s):
-            lyap, t, ws = lyapunov_spectrum(w0, integrator,
-                                            dt=self.dt, nsteps=self.nsteps)
+        lyap, t, ws = lyapunov_spectrum(self.w0, integrator,
+                                        dt=self.dt, nsteps=self.nsteps)
 
-            plt.clf()
-            plt.loglog(lyap, marker=None)
-            plt.savefig(os.path.join(str(tmpdir),"hh_lyap_spec_{}.png".format(ii)))
+        pl.clf()
+        pl.loglog(lyap, marker=None)
+        pl.savefig(os.path.join(str(tmpdir),"hh_lyap_spec_{}.png".format(self.__class__.__name__)))
 
-            plt.clf()
-            plt.plot(ws[...,0], ws[...,1], marker=None)
-            plt.savefig(os.path.join(str(tmpdir),"hh_orbit_lyap_spec_{}.png".format(ii)))
+        pl.clf()
+        pl.plot(ws[...,0], ws[...,1], marker=None)
+        pl.savefig(os.path.join(str(tmpdir),"hh_orbit_lyap_spec_{}.png".format(self.__class__.__name__)))
+
+# initial conditions from LP-VI documentation:
+class TestHenonHeilesStablePeriodic(HenonHeilesBase):
+    def setup(self):
+        super(TestHenonHeilesStablePeriodic, self).setup()
+        self.w0 = np.array([0.,0.295456,0.407308431,0.])
+        self.check = lambda x: x < 1E-3
+
+class TestHenonHeilesStableQuasi1(HenonHeilesBase):
+    def setup(self):
+        super(TestHenonHeilesStableQuasi1, self).setup()
+        self.w0 = np.array([0., 0.483, 0.27898039, 0.])
+        self.check = lambda x: x < 2E-3
+
+class TestHenonHeilesStableQuasi2(HenonHeilesBase):
+    def setup(self):
+        super(TestHenonHeilesStableQuasi2, self).setup()
+        self.w0 = np.array([0., 0.46912, 0.291124891, 0.])
+        self.check = lambda x: x < 2E-3
+
+class TestHenonHeilesStableChaos1(HenonHeilesBase):
+    def setup(self):
+        super(TestHenonHeilesStableChaos1, self).setup()
+        self.w0 = np.array([0., 0.509, 0.254624859, 0.])
+        self.check = lambda x: x > 2E-3
+
+class TestHenonHeilesStableChaos2(HenonHeilesBase):
+    def setup(self):
+        super(TestHenonHeilesStableChaos2, self).setup()
+        self.w0 = np.array([0., 0.56, 0.164113781, 0.112])
+        self.check = lambda x: x > 1E-2
 
 # --------------------------------------------------------------------
 
@@ -270,9 +264,9 @@ class TestLogarithmic(object):
                                                    Integrator=DOPRI853Integrator)
 
             # lyapunov exp
-            plt.clf()
-            plt.loglog(lyap, marker=None)
-            plt.savefig(os.path.join(str(tmpdir),"log_lyap_max_{}.png".format(ii)))
+            pl.clf()
+            pl.loglog(lyap, marker=None)
+            pl.savefig(os.path.join(str(tmpdir),"log_lyap_max_{}.png".format(ii)))
 
             # energy conservation
             E = self.potential.total_energy(ww[:,0,:3], ww[:,0,3:])
@@ -281,14 +275,14 @@ class TestLogarithmic(object):
             E = self.potential.total_energy(ws[:,:3], ws[:,3:])
             dE = np.abs(E[1:] - E[0])
 
-            plt.clf()
-            plt.semilogy(dE_ww, marker=None)
-            plt.semilogy(dE, marker=None)
-            plt.savefig(os.path.join(str(tmpdir),"log_dE_{}.png".format(ii)))
+            pl.clf()
+            pl.semilogy(dE_ww, marker=None)
+            pl.semilogy(dE, marker=None)
+            pl.savefig(os.path.join(str(tmpdir),"log_dE_{}.png".format(ii)))
 
-            plt.figure(figsize=(6,6))
-            plt.plot(ws[:,0], ws[:,1], marker='.', linestyle='none', alpha=0.1)
-            plt.savefig(os.path.join(str(tmpdir),"log_orbit_lyap_max_{}.png".format(ii)))
+            pl.figure(figsize=(6,6))
+            pl.plot(ws[:,0], ws[:,1], marker='.', linestyle='none', alpha=0.1)
+            pl.savefig(os.path.join(str(tmpdir),"log_orbit_lyap_max_{}.png".format(ii)))
 
     def test_compare(self, tmpdir):
         nsteps_per_pullback = 10
@@ -308,10 +302,10 @@ class TestLogarithmic(object):
                                           nsteps_per_pullback=nsteps_per_pullback)
 
             # lyapunov exp
-            plt.clf()
-            plt.loglog(t1[1:-10:10], lyap1, marker=None)
-            plt.loglog(t2[1:-10:10], lyap2, marker=None)
-            plt.savefig(os.path.join(str(tmpdir),"log_lyap_compare_{}.png".format(ii)))
+            pl.clf()
+            pl.loglog(t1[1:-10:10], lyap1, marker=None)
+            pl.loglog(t2[1:-10:10], lyap2, marker=None)
+            pl.savefig(os.path.join(str(tmpdir),"log_lyap_compare_{}.png".format(ii)))
 
             continue
 
@@ -322,14 +316,14 @@ class TestLogarithmic(object):
             E = self.potential.total_energy(ws[:,:3], ws[:,3:])
             dE = np.abs(E[1:] - E[0])
 
-            plt.clf()
-            plt.semilogy(dE_ww, marker=None)
-            plt.semilogy(dE, marker=None)
-            plt.savefig(os.path.join(str(tmpdir),"log_dE_{}.png".format(ii)))
+            pl.clf()
+            pl.semilogy(dE_ww, marker=None)
+            pl.semilogy(dE, marker=None)
+            pl.savefig(os.path.join(str(tmpdir),"log_dE_{}.png".format(ii)))
 
-            plt.figure(figsize=(6,6))
-            plt.plot(ws[:,0], ws[:,1], marker='.', linestyle='none', alpha=0.1)
-            plt.savefig(os.path.join(str(tmpdir),"log_orbit_lyap_max_{}.png".format(ii)))
+            pl.figure(figsize=(6,6))
+            pl.plot(ws[:,0], ws[:,1], marker='.', linestyle='none', alpha=0.1)
+            pl.savefig(os.path.join(str(tmpdir),"log_orbit_lyap_max_{}.png".format(ii)))
 
 def test_surface_of_section(tmpdir):
     from mpl_toolkits.mplot3d import Axes3D
@@ -345,7 +339,7 @@ def test_surface_of_section(tmpdir):
     sos = surface_of_section(w, plane_ix=1)
 
     # plot in 3D
-    fig = plt.figure(figsize=(10,10))
+    fig = pl.figure(figsize=(10,10))
     ax = fig.add_subplot(111, projection='3d')
 
     ax.scatter(np.concatenate(sos[:,0]), # x
