1b3456737f2a29681244b66f35233e2407c412f7
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-24 20:12:37 -0500

    get working

--- gary/inference/tests/test_parameter.py
+++ gary/inference/tests/test_parameter.py
@@ -4,42 +4,29 @@ from __future__ import absolute_import, unicode_literals, division, print_functi
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
-# Standard library
-import os, sys
-import time as pytime
-import copy
-
 # Third-party
-import emcee
 import numpy as np
 import pytest
 import astropy.units as u
-from astropy.io.misc import fnpickle
-import matplotlib.pyplot as plt
 
-from ..core import *
 from ..parameter import *
 from ..prior import *
-
-plot_path = "plots/tests/inference"
-if not os.path.exists(plot_path):
-    os.makedirs(plot_path)
 
 class TestModelParameter(object):
 
     def test_string(self):
-        p = ModelParameter("m", truth=1.5, prior=LogUniformPrior(1.,2.))
+        p = ModelParameter("m", truth=1.5, prior=UniformPrior(1.,2.))
         assert repr(p) == "<ModelParameter 'm'>"
         assert str(p) == "m"
 
-        p = ModelParameter("m", prior=LogUniformPrior(1.,2.))
+        p = ModelParameter("m", prior=UniformPrior(1.,2.))
         assert repr(p) == "<ModelParameter 'm'>"
         assert str(p) == "m"
 
     def test_dumb(self):
         p = ModelParameter("test")
         assert p.name == "test"
-        assert p.frozen == False
+        assert p.frozen is False
 
     def test_truthvalue(self):
         p = ModelParameter("test", truth=15.)
@@ -63,5 +50,5 @@ class TestModelParameter(object):
             p = ModelParameter("test", truth=11*u.km, shape=(15,))
 
     def test_prior(self):
-        prior = LogUniformPrior(a=10., b=25.)
+        prior = UniformPrior(a=10., b=25.)
         p = ModelParameter("test", truth=21., prior=prior)
