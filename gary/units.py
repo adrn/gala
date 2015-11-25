72bd7b89abc301f620a43da5af81b74ccf70ebca
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-25 13:57:58 -0500

    added docstring for unit system

--- gary/units.py
+++ gary/units.py
@@ -10,9 +10,47 @@ from astropy.units.physical import _physical_unit_mapping
 
 class UnitSystem(object):
     """
+    Represents a system of units. At minimum, this consists of a set of
+    length, time, mass, and angle units, but may also contain preferred
+    representations for composite units. For example, the base unit system
+    could be ``{kpc, Myr, Msun, radian}``, but you can also specify a preferred
+    speed, such as ``km/s``.
+
+    This class functions like a dictionary with keys set by physical types.
+    If a unit for a particular physical type is not specified on creation,
+    a composite unit will be created with the base units. See Examples below
+    for some demonstrations.
+
+    Parameters
+    ----------
+    *units
+        The units that define the unit system. At minimum, this must
+        contain length, time, mass, and angle units.
+
+    Examples
+    --------
+    If only base units are specified, any physical type specified as a key
+    to this object will be composed out of the base units::
+
+        >>> usys = UnitSystem(u.m, u.s, u.kg, u.radian)
+        >>> usys['energy']
+        Unit("kg m2 / s2")
+
+    However, custom representations for composite units can also be specified
+    when initializing::
+
+        >>> usys = UnitSystem(u.m, u.s, u.kg, u.radian, u.erg)
+        >>> usys['energy']
+        Unit("erg")
+
+    This is useful for Galactic dynamics where lengths and times are usually
+    given in terms of ``kpc`` and ``Myr``, but speeds are given in ``km/s``::
+
+        >>> usys = UnitSystem(u.kpc, u.Myr, u.Msun, u.radian, u.km/u.s)
+        >>> usys['speed']
+        Unit("km / s")
 
     """
-
     def __init__(self, *units):
 
         self._required_physical_types = ['length', 'time', 'mass', 'angle']
