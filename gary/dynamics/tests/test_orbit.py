ee59d7a90a65de237adc0e82098e3bbf237cd6da
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-25 17:16:14 -0500

    alright trying again with an orbit class

---
+++ gary/dynamics/tests/test_orbit.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+""" Test ...  """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Third-party
+import numpy as np
+import pytest
+
+# Project
+from ..orbit import *
+
+def test_initialize():
+
+    with pytest.raises(ValueError):
+        x = np.random.random(size=(3,10))
+        v = np.random.random(size=(3,8))
+        CartesianOrbit(pos=x, vel=v)
+
+    with pytest.raises(ValueError):
+        x = np.random.random(size=(3,10))
+        v = np.random.random(size=(3,10))
+        t = np.arange(8)
+        CartesianOrbit(pos=x, vel=v, t=t)
+
+def test_slice():
+
+    # simple
+    x = np.random.random(size=(3,10))
+    v = np.random.random(size=(3,10))
+    o = CartesianOrbit(pos=x, vel=v)
+    new_o = o[:5]
+    assert new_o.pos.shape == (3,5)
+
+    # 1d slice on 3d
+    x = np.random.random(size=(3,100,8))
+    v = np.random.random(size=(3,100,8))
+    t = np.arange(x.shape[1])
+    o = CartesianOrbit(pos=x, vel=v, t=t)
+    new_o = o[:5]
+    assert new_o.pos.shape == (3,5,8)
+    assert new_o.t.shape == (5,)
+
+    # 3d slice on 3d
+    o = CartesianOrbit(pos=x, vel=v, t=t)
+    new_o = o[:5,:4]
+    assert new_o.pos.shape == (3,5,4)
+    assert new_o.t.shape == (5,)
