064cdaf54c405d0a481a7698536c7766c4f465d3
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-25 17:35:47 -0500

    changed the meaning of the axis numbers

--- gary/dynamics/tests/test_orbit.py
+++ gary/dynamics/tests/test_orbit.py
@@ -16,46 +16,46 @@ from ..orbit import *
 def test_initialize():
 
     with pytest.raises(ValueError):
-        x = np.random.random(size=(3,10))
-        v = np.random.random(size=(3,8))
+        x = np.random.random(size=(10,3))
+        v = np.random.random(size=(8,3))
         CartesianOrbit(pos=x, vel=v)
 
     with pytest.raises(ValueError):
-        x = np.random.random(size=(3,10))
-        v = np.random.random(size=(3,10))
+        x = np.random.random(size=(10,3))
+        v = np.random.random(size=(10,3))
         t = np.arange(8)
         CartesianOrbit(pos=x, vel=v, t=t)
 
-    x = np.random.random(size=(3,10))
-    v = np.random.random(size=(3,10))
+    x = np.random.random(size=(10,3))
+    v = np.random.random(size=(10,3))
     o = CartesianOrbit(pos=x, vel=v)
     assert o.ndim == 3
 
-    x = np.random.random(size=(2,10))
-    v = np.random.random(size=(2,10))
+    x = np.random.random(size=(10,2))
+    v = np.random.random(size=(10,2))
     o = CartesianOrbit(pos=x, vel=v)
     assert o.ndim == 2
 
 def test_slice():
 
     # simple
-    x = np.random.random(size=(3,10))
-    v = np.random.random(size=(3,10))
+    x = np.random.random(size=(10,3))
+    v = np.random.random(size=(10,3))
     o = CartesianOrbit(pos=x, vel=v)
     new_o = o[:5]
-    assert new_o.pos.shape == (3,5)
+    assert new_o.pos.shape == (5,3)
 
     # 1d slice on 3d
-    x = np.random.random(size=(3,100,8))
-    v = np.random.random(size=(3,100,8))
-    t = np.arange(x.shape[1])
+    x = np.random.random(size=(100,8,3))
+    v = np.random.random(size=(100,8,3))
+    t = np.arange(x.shape[0])
     o = CartesianOrbit(pos=x, vel=v, t=t)
     new_o = o[:5]
-    assert new_o.pos.shape == (3,5,8)
+    assert new_o.pos.shape == (5,8,3)
     assert new_o.t.shape == (5,)
 
     # 3d slice on 3d
     o = CartesianOrbit(pos=x, vel=v, t=t)
     new_o = o[:5,:4]
-    assert new_o.pos.shape == (3,5,4)
+    assert new_o.pos.shape == (5,4,3)
     assert new_o.t.shape == (5,)
