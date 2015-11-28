1b64b7285cd751f45cb5b79ce83c38a29ce766f0
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-28 12:31:47 -0500

    change meaning of axes

--- gary/dynamics/tests/test_orbit.py
+++ gary/dynamics/tests/test_orbit.py
@@ -16,46 +16,46 @@ from ..orbit import *
 def test_initialize():
 
     with pytest.raises(ValueError):
-        x = np.random.random(size=(10,3))
-        v = np.random.random(size=(8,3))
+        x = np.random.random(size=(3,10))
+        v = np.random.random(size=(3,8))
         CartesianOrbit(pos=x, vel=v)
 
     with pytest.raises(ValueError):
-        x = np.random.random(size=(10,3))
-        v = np.random.random(size=(10,3))
+        x = np.random.random(size=(3,10))
+        v = np.random.random(size=(3,10))
         t = np.arange(8)
         CartesianOrbit(pos=x, vel=v, t=t)
 
-    x = np.random.random(size=(10,3))
-    v = np.random.random(size=(10,3))
+    x = np.random.random(size=(3,10))
+    v = np.random.random(size=(3,10))
     o = CartesianOrbit(pos=x, vel=v)
     assert o.ndim == 3
 
-    x = np.random.random(size=(10,2))
-    v = np.random.random(size=(10,2))
+    x = np.random.random(size=(2,10))
+    v = np.random.random(size=(2,10))
     o = CartesianOrbit(pos=x, vel=v)
     assert o.ndim == 2
 
 def test_slice():
 
     # simple
-    x = np.random.random(size=(10,3))
-    v = np.random.random(size=(10,3))
+    x = np.random.random(size=(3,10))
+    v = np.random.random(size=(3,10))
     o = CartesianOrbit(pos=x, vel=v)
     new_o = o[:5]
-    assert new_o.pos.shape == (5,3)
+    assert new_o.pos.shape == (3,5)
 
     # 1d slice on 3d
-    x = np.random.random(size=(100,8,3))
-    v = np.random.random(size=(100,8,3))
-    t = np.arange(x.shape[0])
+    x = np.random.random(size=(3,100,8))
+    v = np.random.random(size=(3,100,8))
+    t = np.arange(x.shape[1])
     o = CartesianOrbit(pos=x, vel=v, t=t)
     new_o = o[:5]
-    assert new_o.pos.shape == (5,8,3)
+    assert new_o.pos.shape == (3,5,8)
     assert new_o.t.shape == (5,)
 
     # 3d slice on 3d
     o = CartesianOrbit(pos=x, vel=v, t=t)
     new_o = o[:5,:4]
-    assert new_o.pos.shape == (5,4,3)
+    assert new_o.pos.shape == (3,5,4)
     assert new_o.t.shape == (5,)
