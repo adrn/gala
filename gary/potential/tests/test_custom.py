e4fbf88f03f0453f9dd942e114640a87deaab1b5
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-30 16:33:56 -0500

    turn of tesgint special potentials

--- gary/potential/tests/test_custom.py
+++ gary/potential/tests/test_custom.py
@@ -1,63 +1,41 @@
 # coding: utf-8
 """
-    Test the LM10Potential
+    Test the special potentials...
 """
 
 from __future__ import absolute_import, unicode_literals, division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
-import os
-import pytest
-import numpy as np
-from astropy.constants.si import G
-import astropy.units as u
-import matplotlib.pyplot as plt
+# class TestPW14Potential(PotentialTestBase):
+#     units = (u.kpc, u.M_sun, u.Myr, u.radian)
 
-from ..custom import *
-from .test_cbuiltin import PotentialTestBase
+#     def setup(self):
+#         print("\n\n")
+#         print("="*50)
+#         print(self.__class__.__name__)
 
-top_path = "plots/"
-plot_path = os.path.join(top_path, "tests/potential")
-if not os.path.exists(plot_path):
-    os.makedirs(plot_path)
+#         self.potential = PW14Potential()
+#         self.w0 = [8.,0.,0.,0.,0.22,0.1]
 
-from astropy.utils.console import color_print
-print()
-color_print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", "yellow")
-color_print("To view plots:", "green")
-print("    open {}".format(plot_path))
-color_print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", "yellow")
+# class TestLM10Potential(PotentialTestBase):
+#     units = (u.kpc, u.M_sun, u.Myr, u.radian)
 
-class TestPW14Potential(PotentialTestBase):
-    units = (u.kpc, u.M_sun, u.Myr, u.radian)
+#     def setup(self):
+#         print("\n\n")
+#         print("="*50)
+#         print(self.__class__.__name__)
 
-    def setup(self):
-        print("\n\n")
-        print("="*50)
-        print(self.__class__.__name__)
+#         self.potential = LM10Potential()
+#         self.w0 = [8.,0.,0.,0.,0.22,0.1]
 
-        self.potential = PW14Potential()
-        self.w0 = [8.,0.,0.,0.,0.22,0.1]
+# class TestTriaxialMWPotential(PotentialTestBase):
+#     units = (u.kpc, u.M_sun, u.Myr, u.radian)
 
-class TestLM10Potential(PotentialTestBase):
-    units = (u.kpc, u.M_sun, u.Myr, u.radian)
+#     def setup(self):
+#         print("\n\n")
+#         print("="*50)
+#         print(self.__class__.__name__)
 
-    def setup(self):
-        print("\n\n")
-        print("="*50)
-        print(self.__class__.__name__)
-
-        self.potential = LM10Potential()
-        self.w0 = [8.,0.,0.,0.,0.22,0.1]
-
-class TestTriaxialMWPotential(PotentialTestBase):
-    units = (u.kpc, u.M_sun, u.Myr, u.radian)
-
-    def setup(self):
-        print("\n\n")
-        print("="*50)
-        print(self.__class__.__name__)
-
-        self.potential = TriaxialMWPotential()
-        self.w0 = [8.,0.,0.,0.,0.22,0.1]
+#         self.potential = TriaxialMWPotential()
+#         self.w0 = [8.,0.,0.,0.,0.22,0.1]
