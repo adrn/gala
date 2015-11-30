a9d07cf09518915ec85f54002e8d617c85eef5f2
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-30 17:07:25 -0500

    fix names of functions

--- gary/observation/tests/test_rrlyrae.py
+++ gary/observation/tests/test_rrlyrae.py
@@ -20,11 +20,11 @@ from ..rrlyrae import *
 
 def test_gaia_rv_error():
     d = np.linspace(1.,50.,100)*u.kpc
-    rv_errs = gaia_rv_error(d)
+    rv_errs = gaia_radial_velocity_error(d)
 
 def test_gaia_pm_error():
     d = np.linspace(1.,50.,100)*u.kpc
-    pm_errs = gaia_pm_error(d)
+    pm_errs = gaia_proper_motion_error(d)
 
     vtan_errs = pm_errs.to(u.rad/u.yr).value/u.yr*d
     vtan_errs = vtan_errs.to(u.km/u.s)
