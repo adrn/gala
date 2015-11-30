049a58e5c4f9bc788761df7639ad3ebd737ce41f
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-30 17:20:38 -0500

    added way to get a constant from a unit system

---
+++ gary/tests/test_units.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+"""
+    Test the unit system.
+"""
+
+from __future__ import absolute_import, unicode_literals, division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Third party
+import astropy.units as u
+from astropy.constants import G,c
+import numpy as np
+import pytest
+
+# This package
+from ..units import UnitSystem
+
+def test_create():
+    # dumb
+    usys = UnitSystem(u.kpc, u.Myr, u.radian, u.Msun)
+
+    with pytest.raises(ValueError):
+        UnitSystem(u.kpc, u.Myr, u.radian) # no mass
+
+    with pytest.raises(ValueError):
+        UnitSystem(u.kpc, u.Myr, u.Msun)
+
+    with pytest.raises(ValueError):
+        UnitSystem(u.kpc, u.radian, u.Msun)
+
+    with pytest.raises(ValueError):
+        UnitSystem(u.Myr, u.radian, u.Msun)
+
+def test_constants():
+    usys = UnitSystem(u.kpc, u.Myr, u.radian, u.Msun)
+    assert np.allclose(usys.get_constant('G'), G.decompose([u.kpc, u.Myr, u.radian, u.Msun]).value)
+    assert np.allclose(usys.get_constant('c'), c.decompose([u.kpc, u.Myr, u.radian, u.Msun]).value)
