7acfcfe8112c7d5359eb7d50d4d019555ce1e48e
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-30 13:55:57 -0500

    test all potentials the same

--- gary/potential/tests/test_builtin.py
+++ gary/potential/tests/test_builtin.py
@@ -7,64 +7,17 @@ from __future__ import absolute_import, unicode_literals, division, print_functi
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
+# Standard library
 import os
+
+# Third party
 import pytest
 import numpy as np
-from astropy.constants.si import G
-import astropy.units as u
-import matplotlib.pyplot as plt
 
+# This project
 from ..core import *
 from ..builtin import *
-from ...units import solarsystem, galactic
-
-top_path = "plots/"
-plot_path = os.path.join(top_path, "tests/potential")
-if not os.path.exists(plot_path):
-    os.makedirs(plot_path)
-
-from astropy.utils.console import color_print
-print()
-color_print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", "yellow")
-color_print("To view plots:", "green")
-print("    open {}".format(plot_path))
-color_print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", "yellow")
-
-class TestHarmonicOscillator(object):
-
-    def test_eval(self):
-        potential = HarmonicOscillatorPotential(omega=1.)
-
-        # 1D oscillator, a single position
-        r = 1.
-        pot_val = potential.value(r)
-        assert np.allclose(pot_val, 0.5, atol=5)
-
-        acc_val = potential.acceleration(r)
-        assert np.allclose(acc_val, -1., atol=5)
-
-        # 2D oscillator, single position
-        r = [1.,0.75]
-        potential = HarmonicOscillatorPotential(omega=[1.,2.])
-        pot_val = potential.value(r)
-        assert np.allclose(pot_val, 1.625)
-
-        # 2D oscillator, multiple positions
-        r = [[1.,0.75],[2.,1.4],[1.5,0.1]]
-        pot_val = potential.value(r)
-        assert np.allclose(pot_val, [1.625,5.92,1.145])
-        acc_val = potential.acceleration(r)
-        assert acc_val.shape == (3,2)
-
-    def test_plot(self):
-        potential = HarmonicOscillatorPotential(omega=[1.,2.])
-        grid = np.linspace(-5.,5)
-
-        fig = potential.plot_contours(grid=(grid,0.))
-        fig.savefig(os.path.join(plot_path, "harmonic_osc_1d.png"))
-
-        fig = potential.plot_contours(grid=(grid,grid))
-        fig.savefig(os.path.join(plot_path, "harmonic_osc_2d.png"))
+from ...units import solarsystem
 
 class TestComposite(object):
     units = solarsystem
