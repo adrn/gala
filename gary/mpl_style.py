2530e9bd5dda64811e4e1077b00b80bae0d8c572
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-12-02 22:45:23 -0500

    change to prop_cycle

--- gary/mpl_style.py
+++ gary/mpl_style.py
@@ -16,7 +16,7 @@ for older versions of matplotlib the following works::
     >>> mpl.rcParams.update(mpl_style)
 
 """
-
+from cycler import cycler
 mpl_style = {
 
     # Lines
@@ -49,15 +49,15 @@ mpl_style = {
     'axes.labelsize': 'large',
     'axes.labelcolor': 'k',
     'axes.axisbelow': True,
-    'axes.color_cycle': ['1A1A1A', # black
-                         '2166AC', # blue
-                         '006837', # green
-                         'B2182B', # red
-                         '762A83', # purple
-                         'E08214',
-                         '80CDC1',
-                         'C51B7D',
-                         'FEE08B'],
+    'axes.prop_cycle': cycler('color', ['1A1A1A', # black
+                                        '2166AC', # blue
+                                        '006837', # green
+                                        'B2182B', # red
+                                        '762A83', # purple
+                                        'E08214',
+                                        '80CDC1',
+                                        'C51B7D',
+                                        'FEE08B']),
 
     # Ticks
     'xtick.major.size': 8,
