fbf22f745f5af446084c11c51c9b3d31bedbe29c
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-12-03 12:02:21 -0500

    only put bracket if units exist

--- gary/dynamics/orbit.py
+++ gary/dynamics/orbit.py
@@ -356,12 +356,16 @@ class CartesianOrbit(CartesianPhaseSpacePosition, Orbit):
         fig : `~matplotlib.Figure`
 
         """
+        _label_unit = ''
+        if self.pos.unit != u.dimensionless_unscaled:
+            _label_unit = ' [{}]'.format(self.pos.unit)
+
         default_kwargs = {
             'marker': None,
             'linestyle': '-',
-            'labels': ('$x$ [{}]'.format(self.pos.unit),
-                       '$y$ [{}]'.format(self.pos.unit),
-                       '$z$ [{}]'.format(self.pos.unit))
+            'labels': ('$x${}'.format(_label_unit),
+                       '$y${}'.format(_label_unit),
+                       '$z${}'.format(_label_unit))
         }
 
         for k,v in default_kwargs.items():
