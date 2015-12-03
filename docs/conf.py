3f9b777032093b7962b1b7c6edd4bf3e749c1a9f
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-12-03 10:21:30 -0500

    path to icon

--- docs/conf.py
+++ docs/conf.py
@@ -126,7 +126,8 @@ html_theme_options = {
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'm104.ico'
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '_static'))
+html_favicon = os.path.join(path, 'm104.ico')
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
