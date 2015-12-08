7db2105df6706132346fcec01cfe07721aae5626
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-12-08 16:09:44 -0500

    move specialized methods to Python and fix stupid bug

--- gary/dynamics/mockstream/core.py
+++ gary/dynamics/mockstream/core.py
@@ -4,10 +4,6 @@ from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
-# Standard library
-import os
-import sys
-
 # Third-party
 from astropy import log as logger
 import astropy.units as u
@@ -19,7 +15,7 @@ from ...potential import CPotentialBase
 from ...integrate import DOPRI853Integrator, LeapfrogIntegrator
 from ._mockstream import _mock_stream_dop853#, _mock_stream_leapfrog
 
-__all__ = ['mock_stream']
+__all__ = ['mock_stream', 'streakline_stream', 'fardal_stream', 'dissolved_fardal_stream']
 
 def mock_stream(potential, w0, prog_mass, k_mean, k_disp,
                 t_f, dt=1., t_0=0., release_every=1,
@@ -59,6 +55,12 @@ def mock_stream(potential, w0, prog_mass, k_mean, k_disp,
         Integrator to use.
     Integrator_kwargs : dict (optional)
         Any extra keyword argumets to pass to the integrator function.
+
+    Returns
+    -------
+    prog_orbit : `~gary.dynamics.CartesianOrbit`
+    stream : `~gary.dynamics.CartesianPhaseSpacePosition`
+
     """
 
     # ------------------------------------------------------------------------
@@ -111,3 +113,200 @@ def mock_stream(potential, w0, prog_mass, k_mean, k_disp,
         raise RuntimeError("Should never get here...")
 
     return prog_orbit, CartesianPhaseSpacePosition.from_w(w=stream_w.T, units=potential.units)
+
+def streakline_stream(potential, w0, prog_mass, t_f, dt=1., t_0=0., release_every=1,
+                      Integrator=LeapfrogIntegrator, Integrator_kwargs=dict()):
+    """
+    Generate a mock stellar stream in the specified potential with a
+    progenitor system that ends up at the specified position.
+
+    This uses the Streakline method from Kuepper et al. (2012).
+
+    Parameters
+    ----------
+    potential : `~gary.potential.PotentialBase`
+        The gravitational potential.
+    w0 : `~gary.dynamics.PhaseSpacePosition`, array_like
+        Initial conditions.
+    prog_mass : numeric, array_like
+        A single mass or an array of masses if the progenitor mass evolves
+        with time.
+    t_f : numeric
+        The final time for integrating.
+    t_0 : numeric (optional)
+        The initial time for integrating -- the time at which ``w0`` is the position.
+    dt : numeric (optional)
+        The time-step.
+    release_every : int (optional)
+        Release particles at the Lagrange points every X timesteps.
+    Integrator : `~gary.integrate.Integrator` (optional)
+        Integrator to use.
+    Integrator_kwargs : dict (optional)
+        Any extra keyword argumets to pass to the integrator function.
+
+    Returns
+    -------
+    prog_orbit : `~gary.dynamics.CartesianOrbit`
+    stream : `~gary.dynamics.CartesianPhaseSpacePosition`
+
+    """
+    k_mean = np.zeros(6)
+    k_disp = np.zeros(6)
+
+    k_mean[0] = 1. # R
+    k_disp[0] = 0.
+
+    k_mean[1] = 0. # phi
+    k_disp[1] = 0.
+
+    k_mean[2] = 0. # z
+    k_disp[2] = 0.
+
+    k_mean[3] = 0. # vR
+    k_disp[3] = 0.
+
+    k_mean[4] = 1. # vt
+    k_disp[4] = 0.
+
+    k_mean[5] = 0. # vz
+    k_disp[5] = 0.
+
+    return mock_stream(potential=potential, w0=w0, prog_mass=prog_mass,
+                       k_mean=k_mean, k_disp=k_disp,
+                       t_f=t_f, dt=dt, t_0=t_0, release_every=release_every,
+                       Integrator=Integrator, Integrator_kwargs=Integrator_kwargs)
+
+def fardal_stream(potential, w0, prog_mass, t_f, dt=1., t_0=0., release_every=1,
+                  Integrator=LeapfrogIntegrator, Integrator_kwargs=dict()):
+    """
+    Generate a mock stellar stream in the specified potential with a
+    progenitor system that ends up at the specified position.
+
+    This uses the prescription from Fardal et al. (2015).
+
+    Parameters
+    ----------
+    potential : `~gary.potential.PotentialBase`
+        The gravitational potential.
+    w0 : `~gary.dynamics.PhaseSpacePosition`, array_like
+        Initial conditions.
+    prog_mass : numeric, array_like
+        A single mass or an array of masses if the progenitor mass evolves
+        with time.
+    t_f : numeric
+        The final time for integrating.
+    t_0 : numeric (optional)
+        The initial time for integrating -- the time at which ``w0`` is the position.
+    dt : numeric (optional)
+        The time-step.
+    release_every : int (optional)
+        Release particles at the Lagrange points every X timesteps.
+    Integrator : `~gary.integrate.Integrator` (optional)
+        Integrator to use.
+    Integrator_kwargs : dict (optional)
+        Any extra keyword argumets to pass to the integrator function.
+
+    Returns
+    -------
+    prog_orbit : `~gary.dynamics.CartesianOrbit`
+    stream : `~gary.dynamics.CartesianPhaseSpacePosition`
+
+    """
+    k_mean = np.zeros(6)
+    k_disp = np.zeros(6)
+
+    k_mean[0] = 2. # R
+    k_disp[0] = 0.5
+
+    k_mean[1] = 0. # phi
+    k_disp[1] = 0.
+
+    k_mean[2] = 0. # z
+    k_disp[2] = 0.5
+
+    k_mean[3] = 0. # vR
+    k_disp[3] = 0.
+
+    k_mean[4] = 0.3 # vt
+    k_disp[4] = 0.5
+
+    k_mean[5] = 0. # vz
+    k_disp[5] = 0.5
+
+    return mock_stream(potential=potential, w0=w0, prog_mass=prog_mass,
+                       k_mean=k_mean, k_disp=k_disp,
+                       t_f=t_f, dt=dt, t_0=t_0, release_every=release_every,
+                       Integrator=Integrator, Integrator_kwargs=Integrator_kwargs)
+
+def dissolved_fardal_stream(potential, w0, prog_mass, t_disrupt, t_f, dt=1., t_0=0.,
+                            release_every=1, Integrator=LeapfrogIntegrator, Integrator_kwargs=dict()):
+    """
+    Generate a mock stellar stream in the specified potential with a
+    progenitor system that ends up at the specified position.
+
+    This uses the prescription from Fardal et al. (2015), but at a specified
+    time the progenitor completely dissolves and the radial offset of the
+    tidal radius is reduced to 0 at fixed dispersion.
+
+    Parameters
+    ----------
+    potential : `~gary.potential.PotentialBase`
+        The gravitational potential.
+    w0 : `~gary.dynamics.PhaseSpacePosition`, array_like
+        Initial conditions.
+    prog_mass : numeric, array_like
+        A single mass or an array of masses if the progenitor mass evolves
+        with time.
+    t_disrupt : numeric
+        The time that the progenitor completely disrupts.
+    t_f : numeric
+        The final time for integrating.
+    t_0 : numeric (optional)
+        The initial time for integrating -- the time at which ``w0`` is the position.
+    dt : numeric (optional)
+        The time-step.
+    release_every : int (optional)
+        Release particles at the Lagrange points every X timesteps.
+    Integrator : `~gary.integrate.Integrator` (optional)
+        Integrator to use.
+    Integrator_kwargs : dict (optional)
+        Any extra keyword argumets to pass to the integrator function.
+
+    Returns
+    -------
+    prog_orbit : `~gary.dynamics.CartesianOrbit`
+    stream : `~gary.dynamics.CartesianPhaseSpacePosition`
+
+    """
+
+    # the time index closest to when the disruption happens
+    nsteps = int(round(np.abs((t_f-t_0)/dt)))
+    t = np.linspace(t_0, t_f, nsteps)
+    disrupt_ix = np.abs(t - t_disrupt).argmin()
+
+    k_mean = np.zeros((nsteps,6))
+    k_disp = np.zeros((nsteps,6))
+
+    k_mean[:,0] = 2. # R
+    k_mean[disrupt_ix:,0] = 0.
+    k_disp[:,0] = 0.5
+
+    k_mean[:,1] = 0. # phi
+    k_disp[:,1] = 0.
+
+    k_mean[:,2] = 0. # z
+    k_disp[:,2] = 0.5
+
+    k_mean[:,3] = 0. # vR
+    k_disp[:,3] = 0.
+
+    k_mean[:,4] = 0.3 # vt
+    k_disp[:,4] = 0.5
+
+    k_mean[:,5] = 0. # vz
+    k_disp[:,5] = 0.5
+
+    return mock_stream(potential=potential, w0=w0, prog_mass=prog_mass,
+                       k_mean=k_mean, k_disp=k_disp,
+                       t_f=t_f, dt=dt, t_0=t_0, release_every=release_every,
+                       Integrator=Integrator, Integrator_kwargs=Integrator_kwargs)
