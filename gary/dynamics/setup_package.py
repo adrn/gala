992289715f4554d05ebab09f73809d1aec979421
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-12-08 15:18:18 -0500

    build the mockstream sub-subpackage

--- gary/dynamics/setup_package.py
+++ gary/dynamics/setup_package.py
@@ -17,4 +17,23 @@ def get_extensions():
     cfg['sources'].append('gary/dynamics/lyapunov/dop853_lyapunov.pyx')
     exts.append(Extension('gary.dynamics.lyapunov.dop853_lyapunov', **cfg))
 
+    cfg = setup_helpers.DistutilsExtensionArgs()
+    cfg['include_dirs'].append('numpy')
+    cfg['include_dirs'].append(mac_incl_path)
+    cfg['sources'].append('gary/dynamics/mockstream/_coord.pyx')
+    cfg['extra_compile_args'].append('--std=gnu99')
+    exts.append(Extension('gary.dynamics.mockstream._coord', **cfg))
+
+    cfg = setup_helpers.DistutilsExtensionArgs()
+    cfg['include_dirs'].append('numpy')
+    cfg['include_dirs'].append('gary/integrate/cyintegrators/dopri/')
+    cfg['include_dirs'].append(mac_incl_path)
+    cfg['sources'].append('gary/dynamics/mockstream/_mockstream.pyx')
+    cfg['sources'].append('gary/integrate/cyintegrators/dopri/dop853.c')
+    cfg['extra_compile_args'].append('--std=gnu99')
+    exts.append(Extension('gary.dynamics.mockstream._mockstream', **cfg))
+
     return exts
+
+def get_package_data():
+    return {'gary': ['gary/dynamics/mockstream/_coord.pxd']}
