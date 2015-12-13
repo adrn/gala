ce29fe1aef835fea3f5ef65a373ce906cedc97b6
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-12-12 22:12:08 -0500

    an option to not save an orbit when computing lyapunov exponent

--- gary/dynamics/lyapunov/dop853_lyapunov.pyx
+++ gary/dynamics/lyapunov/dop853_lyapunov.pyx
@@ -129,3 +129,73 @@ cpdef dop853_lyapunov_max(_CPotential cpotential, double[::1] w0,
 
     LEs = np.array([np.sum(LEs[:j],axis=0)/t[j*nsteps_per_pullback] for j in range(1,niter)])
     return np.asarray(t), np.asarray(all_w), np.asarray(LEs)
+
+cpdef dop853_lyapunov_max_dont_save(_CPotential cpotential, double[::1] w0,
+                                    double dt, int nsteps, double t0,
+                                    double d0, int nsteps_per_pullback, int noffset_orbits,
+                                    double atol=1E-10, double rtol=1E-10, int nmax=0):
+    cdef:
+        int i, j, k, jiter
+        int res
+        unsigned ndim = w0.size
+        unsigned norbits = noffset_orbits + 1
+        unsigned niter = nsteps // nsteps_per_pullback
+        double[::1] w = np.empty(norbits*ndim)
+
+        # define full array of times
+        double t_end = (<double>nsteps) * dt
+        double[::1] t = np.linspace(t0, t_end, nsteps) # TODO: should be nsteps+1
+
+        double d1_mag, norm
+        double[:,::1] d1 = np.empty((norbits,ndim))
+        double[:,::1] LEs = np.zeros((niter,noffset_orbits))
+
+        # temp stuff
+        double[:,::1] d0_vec = np.random.uniform(size=(noffset_orbits,ndim))
+
+    # store initial conditions
+    for i in range(norbits):
+        if i == 0:  # store initial conditions for parent orbit
+            for k in range(ndim):
+                w[i*ndim + k] = w0[k]
+
+        else:  # offset orbits
+            norm = np.linalg.norm(d0_vec[i-1])
+            for k in range(ndim):
+                d0_vec[i-1,k] *= d0/norm  # rescale offset vector
+                w[i*ndim + k] = w0[k] + d0_vec[i-1,k]
+
+    # dummy counter for storing Lyapunov stuff, which only happens every few steps
+    jiter = 0
+    for j in range(1,nsteps,1):
+        res = dop853(ndim*norbits, <FcnEqDiff> Fwrapper,
+                     <GradFn>cpotential.c_gradient, &(cpotential._parameters[0]), norbits,
+                     t[j-1], &w[0], t[j], &rtol, &atol, 0, solout, 0,
+                     NULL, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, dt, nmax, 0, 1, 0, NULL, 0);
+
+        if res == -1:
+            raise RuntimeError("Input is not consistent.")
+        elif res == -2:
+            raise RuntimeError("Larger nmax is needed.")
+        elif res == -3:
+            raise RuntimeError("Step size becomes too small.")
+        elif res == -4:
+            raise RuntimeError("The problem is probably stff (interrupted).")
+
+        if (j % nsteps_per_pullback) == 0:
+            # get magnitude of deviation vector
+            for i in range(1,norbits):
+                for k in range(ndim):
+                    d1[i,k] = w[i*ndim + k] - w[k]
+
+                d1_mag = six_norm(&d1[i,0])
+                LEs[jiter,i-1] = log(d1_mag / d0)
+
+                # renormalize offset orbits
+                for k in range(ndim):
+                    w[i*ndim + k] = w[k] + d0 * d1[i,k] / d1_mag
+
+            jiter += 1
+
+    LEs = np.array([np.sum(LEs[:j],axis=0)/t[j*nsteps_per_pullback] for j in range(1,niter)])
+    return np.asarray(LEs)
