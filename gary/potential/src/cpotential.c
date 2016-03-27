03d8f8672b46c68bdb3ceec307c55b41236d9fa3
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-03-27 12:45:32 -0700

    c evaluation functions

--- gary/potential/src/cpotential.c
+++ gary/potential/src/cpotential.c
@@ -2,26 +2,22 @@
 
 double c_value(CPotential *p, double t, double *q) {
     double v = 0;
-
     for (int i=0; i < p->n_components; i++) {
         v = v + (p->value)[i](t, (p->parameters)[i], q);
-        printf("%f %f\n", (p->parameters)[i][0], (p->parameters)[i][1]);
     }
     return v;
 }
 
-// double c_density(CPotential p, double t, double *q) {
-//     double v = 0;
-//     for (int i=0; i < p.n_components; i++) {
-//         v = v + p.density[i](t, p.parameters[i], q);
-//     }
-//     return v;
-// }
+double c_density(CPotential *p, double t, double *q) {
+    double v = 0;
+    for (int i=0; i < p->n_components; i++) {
+        v = v + (p->density)[i](t, (p->parameters)[i], q);
+    }
+    return v;
+}
 
-// void c_gradient(CPotential p, double t, double *q, double *grad) {
-//     double v = 0;
-//     for (int i=0; i < p.n_components; i++) {
-//         v = v + p.gradient[i](t, p.parameters[i], q, grad);
-//     }
-//     return v;
-// }
+void c_gradient(CPotential *p, double t, double *q, double *grad) {
+    for (int i=0; i < p->n_components; i++) {
+        (p->gradient)[i](t, (p->parameters)[i], q, grad);
+    }
+}
