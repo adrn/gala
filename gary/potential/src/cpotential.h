c4829d45de30cdb2ccff27438c165a07146e948c
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-03-27 10:36:28 -0700

    Add new cpotential c files

---
+++ gary/potential/src/cpotential.h
@@ -0,0 +1,25 @@
+enum {MAX_N_COMPONENTS = 16};
+
+typedef double (*densityfunc)(double t, double *pars, double *q);
+typedef double (*valuefunc)(double t, double *pars, double *q);
+typedef void (*gradientfunc)(double t, double *pars, double *q, double *grad);
+
+typedef struct _CPotential CPotential;
+struct _CPotential {
+    int n_components; // number of potential components
+
+    // arrays of pointers to each of the function types above
+    densityfunc density[MAX_N_COMPONENTS];
+    valuefunc value[MAX_N_COMPONENTS];
+    gradientfunc gradient[MAX_N_COMPONENTS];
+
+    // array containing the number of parameters in each component
+    int n_params[MAX_N_COMPONENTS];
+
+    // pointer to array of pointers to the parameter arrays for each component
+    double *parameters[MAX_N_COMPONENTS];
+};
+
+extern double c_value(CPotential *p, double t, double *q);
+extern double c_density(CPotential *p, double t, double *q);
+extern void c_gradient(CPotential *p, double t, double *q, double *grad);
