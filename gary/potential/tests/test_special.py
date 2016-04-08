6a351e61fd305f58f38def01df7f5f614bff209d
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-08 11:25:30 -0400

    use the composite test base to skip repr

--- gary/potential/tests/test_special.py
+++ gary/potential/tests/test_special.py
@@ -11,19 +11,14 @@ __author__ = "adrn <adrn@astro.columbia.edu>"
 import astropy.units as u
 
 # This project
-from .helpers import PotentialTestBase
+from .helpers import CompositePotentialTestBase
 from ..builtin.special import *
-# from ...units import galactic
 
-class SpecialPotentialTestBase(PotentialTestBase):
-    def test_repr(self):
-        pass
-
-class TestLM10Potential(SpecialPotentialTestBase):
+class TestLM10Potential(CompositePotentialTestBase):
     potential = LM10Potential()
     w0 = [8.,0.,0.,0.,0.22,0.1]
 
-class TestLM10Potential2(PotentialTestBase):
+class TestLM10Potential2(CompositePotentialTestBase):
     potential = LM10Potential(disk={'m': 5E10*u.Msun}, bulge={'m': 5E10*u.Msun})
     w0 = [8.,0.,0.,0.,0.22,0.1]
 
