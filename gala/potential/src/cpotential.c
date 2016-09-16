4702629103fb20c2134b36366d7113376fb442c9
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-15 20:10:00 -0400

    trying to make C89 compliant [ci skip]

--- gala/potential/src/cpotential.c
+++ gala/potential/src/cpotential.c
@@ -3,7 +3,9 @@
 
 double c_value(CPotential *p, double t, double *q) {
     double v = 0;
-    for (int i=0; i < p->n_components; i++) {
+    int i;
+
+    for (i=0; i < p->n_components; i++) {
         v = v + (p->value)[i](t, (p->parameters)[i], q);
     }
     return v;
@@ -11,7 +13,9 @@ double c_value(CPotential *p, double t, double *q) {
 
 double c_density(CPotential *p, double t, double *q) {
     double v = 0;
-    for (int i=0; i < p->n_components; i++) {
+    int i;
+
+    for (i=0; i < p->n_components; i++) {
         v = v + (p->density)[i](t, (p->parameters)[i], q);
     }
     return v;
