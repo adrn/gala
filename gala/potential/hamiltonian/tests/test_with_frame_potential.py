ef6ce0c87a706ee0faf6ce89b5827f07e8409900
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-11-02 08:15:50 -0400

    tests for hamiltonian

---
+++ gala/potential/hamiltonian/tests/test_with_frame_potential.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+# Project
+from .. import Hamiltonian
+from ...potential.builtin import SphericalNFWPotential
+from ...frame.builtin import StaticFrame
+from ...tests.helpers import _TestBase
+from ....units import galactic
+
+class TestLogPotentialWithStaticFrame(_TestBase):
+    obj = Hamiltonian(SphericalNFWPotential(v_c=0.2, r_s=20., units=galactic),
+                      StaticFrame(units=galactic))
