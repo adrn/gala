fbc44ebccc4bea712e85558242091c71a1a5e51e
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-11-17 16:34:45 -0500

    skip hessian test

--- gala/potential/hamiltonian/tests/test_with_frame_potential.py
+++ gala/potential/hamiltonian/tests/test_with_frame_potential.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import pytest
+
 # Project
 from .. import Hamiltonian
 from ...potential.builtin import SphericalNFWPotential
@@ -10,3 +12,7 @@ from ....units import galactic
 class TestLogPotentialWithStaticFrame(_TestBase):
     obj = Hamiltonian(SphericalNFWPotential(v_c=0.2, r_s=20., units=galactic),
                       StaticFrame(units=galactic))
+
+    @pytest.mark.skip("Not implemented")
+    def test_hessian(self):
+        pass
