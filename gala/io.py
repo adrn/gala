59566c16a9b5e122f926a0db2e0fe29c5ce0cfd2
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-07-13 21:46:41 -0400

    dump and load a quantity from/to hdf5

---
+++ gala/io.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+
+from __future__ import division, print_function
+
+# Third-party
+import astropy.units as u
+
+def quantity_from_hdf5(dset):
+    """
+    Return an Astropy Quantity object from a key in an HDF5 file,
+    group, or dataset. This checks to see if the input file/group/dataset
+    contains a ``'unit'`` attribute (e.g., in `f.attrs`).
+
+    Parameters
+    ----------
+    dset : :class:`h5py.DataSet`
+
+    Returns
+    -------
+    q : `astropy.units.Quantity`, `numpy.ndarray`
+        If a unit attribute exists, this returns a Quantity. Otherwise, it
+        returns a numpy array.
+    """
+    if 'unit' in dset.attrs and dset.attrs['unit'] is not None:
+        unit = u.Unit(dset.attrs['unit'])
+    else:
+        unit = 1.
+
+    return dset[:] * unit
+
+def quantity_to_hdf5(f, key, q):
+    """
+    Turn an Astropy Quantity object into something we can write out to
+    an HDF5 file.
+
+    Parameters
+    ----------
+    f : :class:`h5py.File`, :class:`h5py.Group`, :class:`h5py.DataSet`
+    key : str
+        The name.
+    q : float, `astropy.units.Quantity`
+        The quantity.
+
+    """
+
+    if hasattr(q, 'unit'):
+        f[key] = q.value
+        f[key].attrs['unit'] = str(q.unit)
+
+    else:
+        f[key] = q
+        f[key].attrs['unit'] = ""
