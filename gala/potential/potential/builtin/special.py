8b20b337d4c109e8065c2966d68ebe1b429412be
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-07-14 22:53:05 -0400

    added a MilkyWay potential

--- gala/potential/potential/builtin/special.py
+++ gala/potential/potential/builtin/special.py
@@ -12,11 +12,12 @@ import numpy as np
 # from ..core import CompositePotential
 from .cybuiltin import (HernquistPotential,
                         MiyamotoNagaiPotential,
-                        LogarithmicPotential)
+                        LogarithmicPotential,
+                        NFWPotential)
 from ..ccompositepotential import CCompositePotential
 from ....units import galactic
 
-__all__ = ['LM10Potential'] # ['TriaxialMWPotential']
+__all__ = ['LM10Potential', 'MilkyWayPotential']
 
 class LM10Potential(CCompositePotential):
     """
@@ -72,6 +73,76 @@ class LM10Potential(CCompositePotential):
         self["disk"] = MiyamotoNagaiPotential(units=units, **disk)
         self["bulge"] = HernquistPotential(units=units, **bulge)
         self["halo"] = LogarithmicPotential(units=units, **halo)
+        self.lock = True
+
+
+class MilkyWayPotential(CCompositePotential):
+    """
+    A simple mass-model for the Milky Way consisting of a spherical nucleus and
+    bulge, a Miyamoto-Nagai disk, and a spherical NFW dark matter halo.
+
+    Default parameters are fixed by fitting to a compilation of recent mass
+    measurements of the Milky Way, from 10 pc to ~150 pc.
+
+    Parameters
+    ----------
+    units : `~gala.units.UnitSystem` (optional)
+        Set of non-reducable units that specify (at minimum) the
+        length, mass, time, and angle units.
+    disk : dict (optional)
+        Parameters to be passed to the :class:`~gala.potential.MiyamotoNagaiPotential`.
+    bulge : dict (optional)
+        Parameters to be passed to the :class:`~gala.potential.HernquistPotential`.
+    halo : dict (optional)
+        Parameters to be passed to the :class:`~gala.potential.LogarithmicPotential`.
+    nucleus : dict (optional)
+        Parameters to be passed to the :class:`~gala.potential.HernquistPotential`.
+
+    Note: in subclassing, order of arguments must match order of potential
+    components added at bottom of init.
+    """
+    def __init__(self, units=galactic,
+                 disk=None, halo=None, bulge=None, nucleus=None):
+
+        default_disk = dict(m=6.8E10*u.Msun, a=3.*u.kpc, b=0.28*u.kpc)
+        default_bulge = dict(m=5E9*u.Msun, c=1.0*u.kpc)
+        default_nucl = dict(m=1.71E9*u.Msun, c=0.07*u.kpc)
+        default_halo = dict(m=5.4E11*u.Msun, r_s=15.62*u.kpc)
+
+        if disk is None:
+            disk = dict()
+
+        if halo is None:
+            halo = dict()
+
+        if bulge is None:
+            bulge = dict()
+
+        if nucleus is None:
+            nucleus = dict()
+
+        for k,v in default_disk.items():
+            if k not in disk:
+                disk[k] = v
+
+        for k,v in default_bulge.items():
+            if k not in bulge:
+                bulge[k] = v
+
+        for k,v in default_halo.items():
+            if k not in halo:
+                halo[k] = v
+
+        for k,v in default_nucl.items():
+            if k not in nucleus:
+                nucleus[k] = v
+
+        super(MilkyWayPotential, self).__init__()
+
+        self["disk"] = MiyamotoNagaiPotential(units=units, **disk)
+        self["bulge"] = HernquistPotential(units=units, **bulge)
+        self["nucleus"] = HernquistPotential(units=units, **nucleus)
+        self["halo"] = NFWPotential(units=units, **halo)
         self.lock = True
 
 # --------------------------------------------------------------------
--- gala/potential/potential/tests/test_special.py
+++ gala/potential/potential/tests/test_special.py
@@ -11,7 +11,7 @@ import astropy.units as u
 
 # This project
 from .helpers import CompositePotentialTestBase
-from ..builtin.special import *
+from ..builtin.special import LM10Potential, MilkyWayPotential
 
 class TestLM10Potential(CompositePotentialTestBase):
     potential = LM10Potential()
@@ -21,6 +21,6 @@ class TestLM10Potential2(CompositePotentialTestBase):
     potential = LM10Potential(disk={'m': 5E10*u.Msun}, bulge={'m': 5E10*u.Msun})
     w0 = [8.,0.,0.,0.,0.22,0.1]
 
-# class TestTriaxialMW(PotentialTestBase):
-#     potential = TriaxialMWPotential()
-#     w0 = [8.,0.,0.,0.,0.22,0.1]
+class TestMilkyWayPotential(CompositePotentialTestBase):
+    potential = MilkyWayPotential()
+    w0 = [8.,0.,0.,0.,0.22,0.1]
