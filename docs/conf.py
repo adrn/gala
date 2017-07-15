87334e26962ceca1ff53794417a92face60e37b3
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-07-15 18:38:16 -0400

    change nbsphinx timeout

--- docs/conf.py
+++ docs/conf.py
@@ -186,3 +186,6 @@ automodsumm_inherited_members = True
 # Add nbsphinx
 extensions += ['nbsphinx', 'IPython.sphinxext.ipython_console_highlighting']
 exclude_patterns += ['_build', '**.ipynb_checkpoints']
+
+# Custom setting for nbsphinx - timeout for executing one cell
+nbsphinx_timeout = 600
