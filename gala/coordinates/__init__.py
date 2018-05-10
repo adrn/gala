537d5e9162635e5b76ccd078a5ccd87341e83dae
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-05-10 09:55:15 -0400

    ophiuchus

--- gala/coordinates/__init__.py
+++ gala/coordinates/__init__.py
@@ -1,6 +1,7 @@
 from .sgr import *
 from .orphan import *
 from .gd1 import *
+from .oph import *
 from .pal5 import *
 from .propermotion import *
 from .velocity_frame_transforms import *
--- gala/coordinates/oph.py
+++ gala/coordinates/oph.py
@@ -5,6 +5,7 @@
 # Third-party
 import numpy as np
 
+import astropy.units as u
 import astropy.coordinates as coord
 from astropy.coordinates import frame_transform_graph
 from astropy.coordinates.matrix_utilities import matrix_transpose
@@ -49,41 +50,34 @@ class Ophiuchus(coord.BaseCoordinateFrame):
         coord.SphericalRepresentation: [
             coord.RepresentationMapping('lon', 'phi1'),
             coord.RepresentationMapping('lat', 'phi2'),
-            coord.RepresentationMapping('distance', 'distance')],
-        coord.SphericalCosLatDifferential: [
-            coord.RepresentationMapping('d_lon_coslat', 'pm_phi1_cosphi2'),
-            coord.RepresentationMapping('d_lat', 'pm_phi2'),
-            coord.RepresentationMapping('d_distance', 'radial_velocity')],
-        coord.SphericalDifferential: [
-            coord.RepresentationMapping('d_lon', 'pm_phi1'),
-            coord.RepresentationMapping('d_lat', 'pm_phi2'),
-            coord.RepresentationMapping('d_distance', 'radial_velocity')]
+            coord.RepresentationMapping('distance', 'distance')]
     }
 
-    frame_specific_representation_info[coord.UnitSphericalRepresentation] = \
-        frame_specific_representation_info[coord.SphericalRepresentation]
-    frame_specific_representation_info[coord.UnitSphericalCosLatDifferential] = \
-        frame_specific_representation_info[coord.SphericalCosLatDifferential]
-    frame_specific_representation_info[coord.UnitSphericalDifferential] = \
-        frame_specific_representation_info[coord.SphericalDifferential]
+    _default_wrap_angle = 180*u.deg
 
-# Rotation matrix as defined in the Appendix of Koposov et al. (2010)
+    def __init__(self, *args, **kwargs):
+        wrap = kwargs.pop('wrap_longitude', True)
+        if wrap and isinstance(self._data, (coord.UnitSphericalRepresentation,
+                                            coord.SphericalRepresentation)):
+            self._data.lon.wrap_angle = self._default_wrap_angle
+
+# Rotation matrix
 R = np.array([[0.84922096554, 0.07001279040, 0.52337554476],
-              [-0.27043653641 -0.79364259852 0.54497294023],
-              [0.45352820359 -0.60434231606 -0.65504391727]])
+              [-0.27043653641, -0.79364259852, 0.54497294023],
+              [0.45352820359, -0.60434231606, -0.65504391727]])
 
 @frame_transform_graph.transform(coord.StaticMatrixTransform,
-                                 coord.ICRS, Ophiuchus)
-def icrs_to_oph():
+                                 coord.Galactic, Ophiuchus)
+def gal_to_oph():
     """ Compute the transformation from Galactic spherical to
         heliocentric Ophiuchus coordinates.
     """
     return R
 
 @frame_transform_graph.transform(coord.StaticMatrixTransform,
-                                 Ophiuchus, coord.ICRS)
-def oph_to_icrs():
+                                 Ophiuchus, coord.Galactic)
+def oph_to_gal():
     """ Compute the transformation from heliocentric Ophiuchus coordinates to
         spherical Galactic.
     """
-    return matrix_transpose(icrs_to_gd1())
+    return matrix_transpose(gal_to_oph())
