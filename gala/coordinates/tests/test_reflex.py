41d9af962da836ce9378b5d2966f2d2903d8e53c
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-05-11 10:25:36 -0400

    basic unit test

---
+++ gala/coordinates/tests/test_reflex.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+# Third-party
+import astropy.coordinates as coord
+import astropy.units as u
+import numpy as np
+
+# This package
+from ..reflex import reflex_correct
+
+def test_reflex():
+    c = coord.SkyCoord(ra=162*u.deg,
+                       dec=-17*u.deg,
+                       distance=172*u.pc,
+                       pm_ra_cosdec=-11*u.mas/u.yr,
+                       pm_dec=4*u.mas/u.yr,
+                       radial_velocity=110*u.km/u.s)
+    c2 = reflex_correct(c)
+    c3 = reflex_correct(c, coord.Galactocentric(z_sun=0*u.pc))
