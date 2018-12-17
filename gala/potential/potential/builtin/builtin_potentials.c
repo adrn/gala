24fb0ecbe54aebb74f3a97d6e3038a4a88edf9b7
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-12-17 16:03:57 -0500

    typo

--- gala/potential/potential/builtin/builtin_potentials.c
+++ gala/potential/potential/builtin/builtin_potentials.c
@@ -409,7 +409,7 @@ double powerlawcutoff_value(double t, double *pars, double *q, int n_dim) {
     if (r == 0.) {
         return -INFINITY;
     } else {
-        return -(pars[0] * pars[1] / (pars[3] * r) / gamma((3-pars[2])/2) *
+        return -(pars[0] * pars[1] / (pars[3] * r) / gsl_sf_gamma((3-pars[2])/2) *
             (pars[3] * gsl_sf_gamma((3.-pars[2]) / 2) * gsl_sf_gamma_inc_P((3.-pars[2]) / 2, r*r/(pars[3]*pars[3])) -
                    r * gsl_sf_gamma((2.-pars[2]) / 2) * gsl_sf_gamma_inc_P((2.-pars[2]) / 2, r*r/(pars[3]*pars[3]))));
     }
