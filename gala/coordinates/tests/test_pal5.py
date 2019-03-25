35681e4041ecec9718d1df839c04b1ee50010992
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-03-24 22:19:08 -0700

    [ci skip] use new pal5 class in test

--- gala/coordinates/tests/test_pal5.py
+++ gala/coordinates/tests/test_pal5.py
@@ -9,24 +9,24 @@ from ..pal5 import Pal5PriceWhelan18, Pal5
 def test_simple():
     c = coord.ICRS(coord.Angle(217.2141, u.degree),
                    coord.Angle(-11.4351, u.degree))
-    c.transform_to(Pal5)
+    c.transform_to(Pal5PriceWhelan18)
 
     c = coord.Galactic(coord.Angle(217.2141, u.degree),
                        coord.Angle(-11.4351, u.degree))
-    c.transform_to(Pal5)
+    c.transform_to(Pal5PriceWhelan18)
 
-    c = Pal5(217.2141*u.degree, -11.4351*u.degree)
+    c = Pal5PriceWhelan18(217.2141*u.degree, -11.4351*u.degree)
     c.transform_to(coord.ICRS)
     c.transform_to(coord.Galactic)
 
     c = coord.Galactic(coord.Angle(217.2141, u.degree),
                        coord.Angle(-11.4351, u.degree))
-    s = c.transform_to(Pal5)
+    s = c.transform_to(Pal5PriceWhelan18)
 
     # with distance
-    c = Pal5(coord.Angle(217.2141, u.degree),
-             coord.Angle(-11.4351, u.degree),
-             distance=15*u.kpc)
+    c = Pal5PriceWhelan18(coord.Angle(217.2141, u.degree),
+                          coord.Angle(-11.4351, u.degree),
+                          distance=15*u.kpc)
     c.transform_to(coord.ICRS)
     c2 = c.transform_to(coord.Galactic)
     assert np.allclose(c2.distance.value, c.distance.value)
