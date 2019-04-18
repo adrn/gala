7b5df4ac91abffb47b2eddc695f9480adb3087fd
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-04-17 17:11:57 -0700

    add test for nbody

---
+++ gala/dynamics/nbody/tests/test_nbody.py
@@ -0,0 +1,118 @@
+# Third-party
+import astropy.units as u
+from astropy.tests.helper import quantity_allclose
+import numpy as np
+import pytest
+
+# Custom
+from ....potential import (Hamiltonian, NullPotential, NFWPotential,
+                           HernquistPotential, KuzminPotential)
+from ....dynamics import PhaseSpacePosition, combine
+from ....units import UnitSystem, galactic
+
+# Project
+from ..core import DirectNBody
+
+class TestDirectNBody:
+
+    def setup(self):
+        self.usys = UnitSystem(u.pc, u.Unit(1e-5*u.Myr),
+                               u.Unit(1e6*u.Msun), u.radian)
+        pot_particle2 = HernquistPotential(m=1e6*u.Msun, c=0.1*u.pc,
+                                           units=self.usys)
+        vcirc = pot_particle2.circular_velocity([1, 0, 0.]*u.pc).to(u.km/u.s)
+
+        self.particle_potentials = [NullPotential(self.usys), pot_particle2]
+
+        w0_2 = PhaseSpacePosition(pos=[0, 0, 10] * u.kpc,
+                                  vel=[0, 83, 0] * u.km/u.s)
+        w0_1 = PhaseSpacePosition(pos=w0_2.xyz + [1, 0, 0] * u.pc,
+                                  vel=w0_2.v_xyz + [0, 1., 0] * vcirc)
+        self.w0 = combine((w0_1, w0_2))
+
+        self.ext_pot = NFWPotential(m=1e11, r_s=10, units=galactic)
+
+    def test_directnbody_init(self):
+        # another unit system for testing
+        usys2 = UnitSystem(u.pc, u.Unit(1e-3*u.Myr),
+                           u.Unit(1e6*u.Msun), u.radian)
+
+        particle_potentials_None = [None] + self.particle_potentials[1:]
+
+        # Different VALID ways to initialize
+        nbody = DirectNBody(self.w0,
+                            particle_potentials=self.particle_potentials)
+        nbody = DirectNBody(self.w0,
+                            particle_potentials=particle_potentials_None)
+        nbody = DirectNBody(self.w0,
+                            particle_potentials=self.particle_potentials,
+                            external_potential=self.ext_pot)
+        nbody = DirectNBody(self.w0,
+                            particle_potentials=self.particle_potentials,
+                            external_potential=self.ext_pot, units=usys2)
+        nbody = DirectNBody(self.w0, particle_potentials=[None, None],
+                            units=usys2)
+        nbody = DirectNBody(self.w0, particle_potentials=[None, None],
+                            external_potential=self.ext_pot)
+
+        # Different INVALID ways to initialize
+        with pytest.raises(TypeError):
+            DirectNBody("sdf", particle_potentials=self.particle_potentials)
+
+        with pytest.raises(ValueError):
+            DirectNBody(self.w0,
+                        particle_potentials=self.particle_potentials[:1])
+
+        w01025 = combine([self.w0[0]]*1025)
+        with pytest.raises(NotImplementedError):
+            DirectNBody(w01025, particle_potentials=[None]*1025)
+
+        with pytest.raises(ValueError):
+            DirectNBody(self.w0, particle_potentials=[None, None])
+
+        py_ext_pot = KuzminPotential(m=1e10*u.Msun, a=0.5*u.kpc, units=galactic)
+        with pytest.raises(ValueError):
+            DirectNBody(self.w0, particle_potentials=self.particle_potentials,
+                        external_potential=py_ext_pot)
+
+    def test_directnbody_integrate(self):
+        # TODO: this is really a unit test, but we should have some functional tests
+        # that check that the orbit integration is making sense!
+
+        # First, compare with/without mass with no external potential:
+        nbody1 = DirectNBody(self.w0,
+                             particle_potentials=[None, None],
+                             units=self.usys)
+        nbody2 = DirectNBody(self.w0,
+                             particle_potentials=self.particle_potentials,
+                             units=self.usys)
+
+        orbits1 = nbody1.integrate_orbit(dt=1*self.usys['time'],
+                                         t1=0, t2=1*u.Myr)
+        orbits2 = nbody2.integrate_orbit(dt=1*self.usys['time'],
+                                         t1=0, t2=1*u.Myr)
+
+        dx0 = orbits1[:, 0].xyz - orbits2[:, 0].xyz
+        dx1 = orbits1[:, 1].xyz - orbits2[:, 1].xyz
+        assert quantity_allclose(np.abs(dx1), 0*u.pc, atol=1e-13*u.pc)
+        assert np.abs(dx0).max() > 50*u.pc
+
+        # Now compare with/without mass with external potential:
+        nbody1 = DirectNBody(self.w0,
+                             particle_potentials=[None, None],
+                             units=self.usys,
+                             external_potential=self.ext_pot)
+        nbody2 = DirectNBody(self.w0,
+                             particle_potentials=self.particle_potentials,
+                             units=self.usys,
+                             external_potential=self.ext_pot)
+
+        orbits1 = nbody1.integrate_orbit(dt=1*self.usys['time'],
+                                         t1=0, t2=1*u.Myr)
+        orbits2 = nbody2.integrate_orbit(dt=1*self.usys['time'],
+                                         t1=0, t2=1*u.Myr)
+
+        dx0 = orbits1[:, 0].xyz - orbits2[:, 0].xyz
+        dx1 = orbits1[:, 1].xyz - orbits2[:, 1].xyz
+        assert quantity_allclose(np.abs(dx1), 0*u.pc, atol=1e-13*u.pc)
+        assert np.abs(dx0).max() > 50*u.pc
