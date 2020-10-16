245b1c14a6a858531f9865f629040b64cb5ddeee
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-10-16 09:01:08 -0400

    add function to compute focal length for staeckel fudge

--- gala/dynamics/__init__.py
+++ gala/dynamics/__init__.py
@@ -2,6 +2,7 @@ from .core import PhaseSpacePosition
 from .orbit import Orbit
 from .analyticactionangle import *
 from .actionangle import *
+from .actionangle_staeckel import *
 from .nonlinear import *
 from .plot import *
 from .util import *
