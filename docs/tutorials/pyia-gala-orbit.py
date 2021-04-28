9df7de817fea0a64f2327522f3a1f348b04a082d
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-04-28 16:18:54 -0400

    convert to py script

---
+++ docs/tutorials/pyia-gala-orbit.py
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.1
+#   kernelspec:
+#     display_name: Python [conda env:root] *
+#     language: python
+#     name: conda-root-py
+# ---
+
+# TODO: real world example of retrieving a star's Gaia data and computing an orbit
+
+
