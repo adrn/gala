e29d5d5487e84ac544df3a66be5f643cbb45f0f3
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-12-01 16:38:38 -0500

    reformat to make code style check happy

--- gala/potential/scf/core.py
+++ gala/potential/scf/core.py
@@ -5,30 +5,46 @@ import numpy as np
 import scipy.integrate as si
 
 # Project
-from ._computecoeff import (Snlm_integrand, Tnlm_integrand,
-                            STnlm_discrete, STnlm_var_discrete)
+from ._computecoeff import (
+    Snlm_integrand,
+    Tnlm_integrand,
+    STnlm_discrete,
+    STnlm_var_discrete,
+)
 
-__all__ = ['compute_coeffs', 'compute_coeffs_discrete']
+__all__ = ["compute_coeffs", "compute_coeffs_discrete"]
 
 
-def compute_coeffs(density_func, nmax, lmax, M, r_s, args=(),
-                   skip_odd=False, skip_even=False, skip_m=False,
-                   S_only=False, progress=False, **nquad_opts):
+def compute_coeffs(
+    density_func,
+    nmax,
+    lmax,
+    M,
+    r_s,
+    args=(),
+    skip_odd=False,
+    skip_even=False,
+    skip_m=False,
+    S_only=False,
+    progress=False,
+    **nquad_opts
+):
     """
-    Compute the expansion coefficients for representing the input density function using a basis
-    function expansion.
+    Compute the expansion coefficients for representing the input density
+    function using a basis function expansion.
 
-    Computing the coefficients involves computing triple integrals which are computationally
-    expensive. For an example of how to parallelize the computation of the coefficients, see
-    ``examples/parallel_compute_Anlm.py``.
+    Computing the coefficients involves computing triple integrals which are
+    computationally expensive. For an example of how to parallelize the
+    computation of the coefficients, see ``examples/parallel_compute_Anlm.py``.
 
     Parameters
     ----------
     density_func : function, callable
-        A function or callable object that evaluates the density at a given position. The call
-        format must be of the form: ``density_func(x, y, z, M, r_s, args)`` where ``x, y, z`` are
-        cartesian coordinates, ``M`` is a scale mass, ``r_s`` a scale radius, and ``args`` is an
-        iterable containing any other arguments needed by the density function.
+        A function or callable object that evaluates the density at a given
+        position. The call format must be of the form: ``density_func(x, y, z,
+        M, r_s, args)`` where ``x, y, z`` are cartesian coordinates, ``M`` is a
+        scale mass, ``r_s`` a scale radius, and ``args`` is an iterable
+        containing any other arguments needed by the density function.
     nmax : int
         Maximum value of ``n`` for the radial expansion.
     lmax : int
@@ -41,11 +57,11 @@ def compute_coeffs(density_func, nmax, lmax, M, r_s, args=(),
         A list or iterable of any other arguments needed by the density
         function.
     skip_odd : bool (optional)
-        Skip the odd terms in the angular portion of the expansion. For example, only
-        take :math:`l=0, 2, 4, ...`
+        Skip the odd terms in the angular portion of the expansion. For example,
+        only take :math:`l=0, 2, 4, ...`
     skip_even : bool (optional)
-        Skip the even terms in the angular portion of the expansion. For example, only
-        take :math:`l=1, 3, 5, ...`
+        Skip the even terms in the angular portion of the expansion. For
+        example, only take :math:`l=1, 3, 5, ...`
     skip_m : bool (optional)
         Ignore terms with :math:`m > 0`.
     S_only : bool (optional)
@@ -69,11 +85,14 @@ def compute_coeffs(density_func, nmax, lmax, M, r_s, args=(),
 
     """
     from gala._cconfig import GSL_ENABLED
+
     if not GSL_ENABLED:
-        raise ValueError("Gala was compiled without GSL and so this function "
-                         "will not work.  See the gala documentation for more "
-                         "information about installing and using GSL with "
-                         "gala: http://gala.adrian.pw/en/latest/install.html")
+        raise ValueError(
+            "Gala was compiled without GSL and so this function "
+            "will not work.  See the gala documentation for more "
+            "information about installing and using GSL with "
+            "gala: http://gala.adrian.pw/en/latest/install.html"
+        )
 
     lmin = 0
     lstride = 1
@@ -84,22 +103,24 @@ def compute_coeffs(density_func, nmax, lmax, M, r_s, args=(),
     if skip_even:
         lmin = 1
 
-    Snlm = np.zeros((nmax+1, lmax+1, lmax+1))
-    Snlm_e = np.zeros((nmax+1, lmax+1, lmax+1))
-    Tnlm = np.zeros((nmax+1, lmax+1, lmax+1))
-    Tnlm_e = np.zeros((nmax+1, lmax+1, lmax+1))
+    Snlm = np.zeros((nmax + 1, lmax + 1, lmax + 1))
+    Snlm_e = np.zeros((nmax + 1, lmax + 1, lmax + 1))
+    Tnlm = np.zeros((nmax + 1, lmax + 1, lmax + 1))
+    Tnlm_e = np.zeros((nmax + 1, lmax + 1, lmax + 1))
 
-    nquad_opts.setdefault('limit', 256)
-    nquad_opts.setdefault('epsrel', 1E-10)
+    nquad_opts.setdefault("limit", 256)
+    nquad_opts.setdefault("epsrel", 1e-10)
 
-    limits = [[0, 2*np.pi],  # phi
-              [-1, 1.],  # X (cos(theta))
-              [-1, 1.]]  # xsi
+    limits = [
+        [0, 2 * np.pi],  # phi
+        [-1, 1.0],  # X (cos(theta))
+        [-1, 1.0],
+    ]  # xsi
 
     nlms = []
-    for n in range(nmax+1):
-        for l in range(lmin, lmax+1, lstride):
-            for m in range(l+1):
+    for n in range(nmax + 1):
+        for l in range(lmin, lmax + 1, lstride):
+            for m in range(l + 1):
                 if skip_m and m > 0:
                     continue
 
@@ -109,43 +130,58 @@ def compute_coeffs(density_func, nmax, lmax, M, r_s, args=(),
         try:
             from tqdm import tqdm
         except ImportError as e:
-            raise ImportError('tqdm is not installed - you can install it '
-                              'with `pip install tqdm`.\n' + str(e))
+            raise ImportError(
+                "tqdm is not installed - you can install it "
+                "with `pip install tqdm`.\n" + str(e)
+            )
         iterfunc = tqdm
     else:
         iterfunc = lambda x: x  # noqa
 
     for n, l, m in iterfunc(nlms):
         Snlm[n, l, m], Snlm_e[n, l, m] = si.nquad(
-            Snlm_integrand, ranges=limits,
+            Snlm_integrand,
+            ranges=limits,
             args=(density_func, n, l, m, M, r_s, args),
-            opts=nquad_opts)
+            opts=nquad_opts,
+        )
 
         if not S_only:
             Tnlm[n, l, m], Tnlm_e[n, l, m] = si.nquad(
-                Tnlm_integrand, ranges=limits,
+                Tnlm_integrand,
+                ranges=limits,
                 args=(density_func, n, l, m, M, r_s, args),
-                opts=nquad_opts)
+                opts=nquad_opts,
+            )
 
     return (Snlm, Snlm_e), (Tnlm, Tnlm_e)
 
 
-def compute_coeffs_discrete(xyz, mass, nmax, lmax, r_s,
-                            skip_odd=False, skip_even=False, skip_m=False,
-                            compute_var=False):
+def compute_coeffs_discrete(
+    xyz,
+    mass,
+    nmax,
+    lmax,
+    r_s,
+    skip_odd=False,
+    skip_even=False,
+    skip_m=False,
+    compute_var=False,
+):
     """
-    Compute the expansion coefficients for representing the density distribution of input points
-    as a basis function expansion. The points, ``xyz``, are assumed to be samples from the
-    density distribution.
+    Compute the expansion coefficients for representing the density distribution
+    of input points as a basis function expansion. The points, ``xyz``, are
+    assumed to be samples from the density distribution.
 
-    Computing the coefficients involves computing triple integrals which are computationally
-    expensive. For an example of how to parallelize the computation of the coefficients, see
-    ``examples/parallel_compute_Anlm.py``.
+    Computing the coefficients involves computing triple integrals which are
+    computationally expensive. For an example of how to parallelize the
+    computation of the coefficients, see ``examples/parallel_compute_Anlm.py``.
 
     Parameters
     ----------
     xyz : array_like
-        Samples from the density distribution. Should have shape ``(n_samples, 3)``.
+        Samples from the density distribution. Should have shape ``(n_samples,
+        3)``.
     mass : array_like
         Mass of each sample. Should have shape ``(n_samples,)``.
     nmax : int
@@ -155,24 +191,25 @@ def compute_coeffs_discrete(xyz, mass, nmax, lmax, r_s,
     r_s : numeric
         Scale radius.
     skip_odd : bool (optional)
-        Skip the odd terms in the angular portion of the expansion. For example, only
-        take :math:`l=0, 2, 4, ...`
+        Skip the odd terms in the angular portion of the expansion. For example,
+        only take :math:`l=0, 2, 4, ...`
     skip_even : bool (optional)
-        Skip the even terms in the angular portion of the expansion. For example, only
-        take :math:`l=1, 3, 5, ...`
+        Skip the even terms in the angular portion of the expansion. For
+        example, only take :math:`l=1, 3, 5, ...`
     skip_m : bool (optional)
         Ignore terms with :math:`m > 0`.
     compute_var : bool (optional)
-        Also compute the variances of the coefficients. This does not compute the full covariance
-        matrix of the coefficients, just the individual variances.
-        TODO: separate function to compute full covariance matrix?
+        Also compute the variances (and covariances) of the coefficients.
 
     Returns
     -------
-    Snlm : float
+    Snlm : `~numpy.ndarray`
         The value of the cosine expansion coefficient.
-    Tnlm : float
+    Tnlm : `~numpy.ndarray`
         The value of the sine expansion coefficient.
+    STcovar : `~numpy.ndarray`, optional
+        If ``compute_var==True``, this also computes and returns the covariance
+        matrix of the coefficients.
 
     """
     lmin = 0
@@ -184,35 +221,43 @@ def compute_coeffs_discrete(xyz, mass, nmax, lmax, r_s,
     if skip_even:
         lmin = 1
 
-    Snlm = np.zeros((nmax+1, lmax+1, lmax+1))
-    Tnlm = np.zeros((nmax+1, lmax+1, lmax+1))
+    Snlm = np.zeros((nmax + 1, lmax + 1, lmax + 1))
+    Tnlm = np.zeros((nmax + 1, lmax + 1, lmax + 1))
 
     if compute_var:
-        Snlm_var = np.zeros((nmax+1, lmax+1, lmax+1))
-        Tnlm_var = np.zeros((nmax+1, lmax+1, lmax+1))
-        STnlm_var = np.zeros((nmax+1, lmax+1, lmax+1))
+        Snlm_var = np.zeros((nmax + 1, lmax + 1, lmax + 1))
+        Tnlm_var = np.zeros((nmax + 1, lmax + 1, lmax + 1))
+        STnlm_var = np.zeros((nmax + 1, lmax + 1, lmax + 1))
 
     # positions and masses of point masses
     xyz = np.ascontiguousarray(np.atleast_2d(xyz))
     mass = np.ascontiguousarray(np.atleast_1d(mass))
 
-    r = np.sqrt(np.sum(xyz**2, axis=-1))
+    r = np.sqrt(np.sum(xyz ** 2, axis=-1))
     s = r / r_s
     phi = np.arctan2(xyz[:, 1], xyz[:, 0])
     X = xyz[:, 2] / r
 
-    for n in range(nmax+1):
-        for l in range(lmin, lmax+1, lstride):
-            for m in range(l+1):
+    for n in range(nmax + 1):
+        for l in range(lmin, lmax + 1, lstride):
+            for m in range(l + 1):
                 if skip_m and m > 0:
                     continue
 
-                # logger.debug("Computing coefficients (n, l, m)=({},{},{})".format(n, l, m))
-
-                Snlm[n, l, m], Tnlm[n, l, m] = STnlm_discrete(s, phi, X, mass, n, l, m)
+                Snlm[n, l, m], Tnlm[n, l, m] = STnlm_discrete(
+                    s, phi, X, mass, n, l, m
+                )
                 if compute_var:
-                    Snlm_var[n,l,m], Tnlm_var[n,l,m], STnlm_var[n,l,m] = STnlm_var_discrete(s, phi, X, mass, n, l, m)
+                    (
+                        Snlm_var[n, l, m],
+                        Tnlm_var[n, l, m],
+                        STnlm_var[n, l, m],
+                    ) = STnlm_var_discrete(s, phi, X, mass, n, l, m)
     if compute_var:
-        return Snlm, Tnlm, np.array([[Snlm_var, STnlm_var], [STnlm_var, Tnlm_var]])
+        return (
+            Snlm,
+            Tnlm,
+            np.array([[Snlm_var, STnlm_var], [STnlm_var, Tnlm_var]]),
+        )
     else:
         return Snlm, Tnlm
