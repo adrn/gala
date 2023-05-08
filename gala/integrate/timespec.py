eb1410f8c9db33207198ce3b1a9a96cebbe93daa
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-05-08 17:12:23 -0400

    remove deprecated argument and black format

--- gala/integrate/timespec.py
+++ gala/integrate/timespec.py
@@ -8,11 +8,10 @@ from warnings import warn
 # Third-party
 import numpy as np
 
-__all__ = ['parse_time_specification']
+__all__ = ["parse_time_specification"]
 
 
-def parse_time_specification(units, dt=None, n_steps=None, nsteps=None,
-                             t1=None, t2=None, t=None):
+def parse_time_specification(units, dt=None, n_steps=None, t1=None, t2=None, t=None):
     """
     Return an array of times given a few combinations of kwargs that are
     accepted -- see below.
@@ -31,24 +30,19 @@ def parse_time_specification(units, dt=None, n_steps=None, nsteps=None,
         An array of times (dts = t[1:] - t[:-1])
 
     """
-    if nsteps is not None:
-        warn("The argument 'nsteps' is deprecated and will be removed in a future version."
-             "Use 'n_steps' instead.")
-        n_steps = nsteps
-
     if n_steps is not None:  # parse and validate n_steps
         n_steps = int(n_steps)
 
-    if hasattr(dt, 'unit'):
+    if hasattr(dt, "unit"):
         dt = dt.decompose(units).value
 
-    if hasattr(t1, 'unit'):
+    if hasattr(t1, "unit"):
         t1 = t1.decompose(units).value
 
-    if hasattr(t2, 'unit'):
+    if hasattr(t2, "unit"):
         t2 = t2.decompose(units).value
 
-    if hasattr(t, 'unit'):
+    if hasattr(t, "unit"):
         t = t.decompose(units).value
 
     # t : array_like
@@ -63,18 +57,16 @@ def parse_time_specification(units, dt=None, n_steps=None, nsteps=None,
         # dt, n_steps[, t1] : (numeric, int[, numeric])
         elif dt is not None and n_steps is not None:
             if t1 is None:
-                t1 = 0.
+                t1 = 0.0
 
-            times = parse_time_specification(units, dt=np.ones(n_steps+1)*dt,
-                                             t1=t1)
+            times = parse_time_specification(units, dt=np.ones(n_steps + 1) * dt, t1=t1)
         # dt, t1, t2 : (numeric, numeric, numeric)
         elif dt is not None and t1 is not None and t2 is not None:
             if t2 < t1 and dt < 0:
-
                 t_i = t1
                 times = []
                 ii = 0
-                while (t_i > t2) and (ii < 1E6):
+                while (t_i > t2) and (ii < 1e6):
                     times.append(t_i)
                     t_i += dt
 
@@ -84,23 +76,24 @@ def parse_time_specification(units, dt=None, n_steps=None, nsteps=None,
                 return np.array(times, dtype=np.float64)
 
             elif t2 > t1 and dt > 0:
-
                 t_i = t1
                 times = []
                 ii = 0
-                while (t_i < t2) and (ii < 1E6):
+                while (t_i < t2) and (ii < 1e6):
                     times.append(t_i)
                     t_i += dt
 
                 return np.array(times, dtype=np.float64)
 
             else:
-                raise ValueError("If t2 < t1, dt must be negative. If t1 < t2, "
-                                 "dt should be positive.")
+                raise ValueError(
+                    "If t2 < t1, dt must be negative. If t1 < t2, "
+                    "dt should be positive."
+                )
 
         # dt, t1 : (array_like, numeric)
         elif isinstance(dt, np.ndarray) and t1 is not None:
-            times = np.cumsum(np.append([0.], dt)) + t1
+            times = np.cumsum(np.append([0.0], dt)) + t1
             times = times[:-1]
 
         # n_steps, t1, t2 : (int, numeric, numeric)
