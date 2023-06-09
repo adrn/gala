3486cc58f6d65554ce45d50633e94a1128d16dcd
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-06-09 14:39:58 -0400

    add new potential model from Hunt et al. 2022, Darragh-Ford et al. 2023

--- gala/potential/potential/tests/test_special.py
+++ gala/potential/potential/tests/test_special.py
@@ -6,32 +6,47 @@
 import astropy.units as u
 import pytest
 
+from ...._cconfig import GSL_ENABLED
+from ..builtin.special import (
+    BovyMWPotential2014,
+    LM10Potential,
+    MilkyWayPotential,
+    MilkyWayPotential2022,
+)
+
 # This project
 from .helpers import CompositePotentialTestBase
-from ..builtin.special import (LM10Potential, MilkyWayPotential,
-                               BovyMWPotential2014)
-from ...._cconfig import GSL_ENABLED
 
 
 class TestLM10Potential(CompositePotentialTestBase):
     potential = LM10Potential()
-    w0 = [8., 0., 0., 0., 0.22, 0.1]
+    w0 = [8.0, 0.0, 0.0, 0.0, 0.22, 0.1]
 
 
 class TestLM10Potential2(CompositePotentialTestBase):
-    potential = LM10Potential(disk={'m': 5E10*u.Msun}, bulge={'m': 5E10*u.Msun})
-    w0 = [8., 0., 0., 0., 0.22, 0.1]
+    potential = LM10Potential(disk={"m": 5e10 * u.Msun}, bulge={"m": 5e10 * u.Msun})
+    w0 = [8.0, 0.0, 0.0, 0.0, 0.22, 0.1]
 
 
 class TestMilkyWayPotential(CompositePotentialTestBase):
     potential = MilkyWayPotential()
-    w0 = [8., 0., 0., 0., 0.22, 0.1]
+    w0 = [8.0, 0.0, 0.0, 0.0, 0.22, 0.1]
 
 
-@pytest.mark.skipif(not GSL_ENABLED,
-                    reason="requires GSL to run this test")
+class TestMilkyWayPotential2022(CompositePotentialTestBase):
+    potential = MilkyWayPotential2022()
+    w0 = [8.0, 0.0, 0.0, 0.0, 0.22, 0.1]
+
+
+@pytest.mark.skipif(not GSL_ENABLED, reason="requires GSL to run this test")
 class TestBovyMWPotential2014(CompositePotentialTestBase):
-    w0 = [8., 0., 0., 0., 0.22, 0.1]
+    w0 = [8.0, 0.0, 0.0, 0.0, 0.22, 0.1]
+    check_finite_at_origin = False
+
+    def setup_method(self):
+        self.potential = BovyMWPotential2014()
+        super().setup_method()
+
     check_finite_at_origin = False
 
     def setup_method(self):
