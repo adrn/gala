b9a3072902bc7b4a9696b78f08fcaedc05af6acd
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-08-10 10:51:22 -0400

    remove gnu99 compile arg

--- gala/potential/hamiltonian/setup_package.py
+++ gala/potential/hamiltonian/setup_package.py
@@ -11,19 +11,16 @@ def get_extensions():
     mac_incl_path = "/usr/include/malloc"
 
     cfg = defaultdict(list)
-    cfg['include_dirs'].append(np.get_include())
-    cfg['include_dirs'].append(mac_incl_path)
-    cfg['include_dirs'].append('gala/potential')
-    cfg['extra_compile_args'].append('--std=gnu99')
-    cfg['sources'].append('gala/potential/hamiltonian/chamiltonian.pyx')
-    cfg['sources'].append('gala/potential/hamiltonian/src/chamiltonian.c')
-    cfg['sources'].append('gala/potential/potential/src/cpotential.c')
-    exts.append(Extension('gala.potential.hamiltonian.chamiltonian', **cfg))
+    cfg["include_dirs"].append(np.get_include())
+    cfg["include_dirs"].append(mac_incl_path)
+    cfg["include_dirs"].append("gala/potential")
+    cfg["sources"].append("gala/potential/hamiltonian/chamiltonian.pyx")
+    cfg["sources"].append("gala/potential/hamiltonian/src/chamiltonian.c")
+    cfg["sources"].append("gala/potential/potential/src/cpotential.c")
+    exts.append(Extension("gala.potential.hamiltonian.chamiltonian", **cfg))
 
     return exts
 
 
 def get_package_data():
-
-    return {'gala.potential.hamiltonian':
-            ['src/*.h', 'src/*.c', '*.pyx', '*.pxd']}
+    return {"gala.potential.hamiltonian": ["src/*.h", "src/*.c", "*.pyx", "*.pxd"]}
--- gala/setup_package.py
+++ gala/setup_package.py
@@ -6,9 +6,8 @@ def get_extensions():
     exts = []
 
     cfg = defaultdict(list)
-    cfg['include_dirs'].append('gala')
-    cfg['extra_compile_args'].append('--std=gnu99')
-    cfg['sources'].append('gala/cconfig.pyx')
-    exts.append(Extension('gala._cconfig', **cfg))
+    cfg["include_dirs"].append("gala")
+    cfg["sources"].append("gala/cconfig.pyx")
+    exts.append(Extension("gala._cconfig", **cfg))
 
     return exts
