489482297afca5e2dfbb991e67d614d1e05ff429
Adrian Price-Whelan <adrian.prw@gmail.com>
2024-02-14 11:11:09 -0500

    formatting

--- gala/dynamics/mockstream/tests/test_mockstream.py
+++ gala/dynamics/mockstream/tests/test_mockstream.py
@@ -1,29 +1,35 @@
-import os
 import itertools
+import os
 
 # Third-party
 import astropy.units as u
 import numpy as np
 import pytest
 
+from gala.tests.optional_deps import HAS_H5PY
+
+from ....dynamics import Orbit, PhaseSpacePosition
+
 # Custom
-from ....potential import (Hamiltonian, NFWPotential, HernquistPotential,
-                           ConstantRotatingFrame)
-from ....dynamics import PhaseSpacePosition, Orbit
+from ....potential import (
+    ConstantRotatingFrame,
+    Hamiltonian,
+    HernquistPotential,
+    NFWPotential,
+)
 from ....units import galactic
 from ...nbody import DirectNBody
-from ..mockstream_generator import MockStreamGenerator
 from ..df import FardalStreamDF
-from gala.tests.optional_deps import HAS_H5PY
+from ..mockstream_generator import MockStreamGenerator
 
 
 def test_init():
-    w0 = PhaseSpacePosition(pos=[15., 0., 0]*u.kpc,
-                            vel=[0, 0, 0.13]*u.kpc/u.Myr)
-    potential = NFWPotential.from_circular_velocity(v_c=0.2, r_s=20.,
-                                                    units=galactic)
+    w0 = PhaseSpacePosition(
+        pos=[15.0, 0.0, 0] * u.kpc, vel=[0, 0, 0.13] * u.kpc / u.Myr
+    )
+    potential = NFWPotential.from_circular_velocity(v_c=0.2, r_s=20.0, units=galactic)
     H = Hamiltonian(potential)
-    df = FardalStreamDF()
+    df = FardalStreamDF(gala_modified=True)
 
     with pytest.raises(TypeError):
         MockStreamGenerator(df="some df", hamiltonian=H)
@@ -32,106 +38,115 @@ def test_init():
         MockStreamGenerator(df=df, hamiltonian=H, progenitor_potential="stuff")
 
     # Test validating the input nbody
-    nbody_w0 = PhaseSpacePosition(pos=[25., 0., 0]*u.kpc,
-                                  vel=[0, 0, 0.13]*u.kpc/u.Myr)
-    potential2 = NFWPotential.from_circular_velocity(v_c=0.2, r_s=25.,
-                                                     units=galactic)
-    nbody = DirectNBody(w0=nbody_w0, external_potential=potential2,
-                        particle_potentials=[None])
+    nbody_w0 = PhaseSpacePosition(
+        pos=[25.0, 0.0, 0] * u.kpc, vel=[0, 0, 0.13] * u.kpc / u.Myr
+    )
+    potential2 = NFWPotential.from_circular_velocity(v_c=0.2, r_s=25.0, units=galactic)
+    nbody = DirectNBody(
+        w0=nbody_w0, external_potential=potential2, particle_potentials=[None]
+    )
     gen = MockStreamGenerator(df=df, hamiltonian=H)
     with pytest.raises(ValueError):
         gen._get_nbody(w0, nbody)
 
-    frame2 = ConstantRotatingFrame([0, 0, 25.]*u.km/u.s/u.kpc, units=galactic)
-    nbody = DirectNBody(w0=nbody_w0, external_potential=potential, frame=frame2,
-                        particle_potentials=[None])
+    frame2 = ConstantRotatingFrame([0, 0, 25.0] * u.km / u.s / u.kpc, units=galactic)
+    nbody = DirectNBody(
+        w0=nbody_w0,
+        external_potential=potential,
+        frame=frame2,
+        particle_potentials=[None],
+    )
     with pytest.raises(ValueError):
         gen._get_nbody(w0, nbody)
 
     # we expect success!
-    nbody = DirectNBody(w0=nbody_w0, external_potential=potential,
-                        particle_potentials=[None])
+    nbody = DirectNBody(
+        w0=nbody_w0, external_potential=potential, particle_potentials=[None]
+    )
     new_nbody = gen._get_nbody(w0, nbody)  # noqa
 
 
 def test_run():
-    potential = NFWPotential.from_circular_velocity(v_c=0.2, r_s=20.,
-                                                    units=galactic)
+    potential = NFWPotential.from_circular_velocity(v_c=0.2, r_s=20.0, units=galactic)
     H = Hamiltonian(potential)
-    w0 = PhaseSpacePosition(pos=[15., 0., 0]*u.kpc,
-                            vel=[0, 0, 0.13]*u.kpc/u.Myr)
+    w0 = PhaseSpacePosition(
+        pos=[15.0, 0.0, 0] * u.kpc, vel=[0, 0, 0.13] * u.kpc / u.Myr
+    )
     mass = 2.5e4 * u.Msun
-    prog_pot = HernquistPotential(mass, 4*u.pc, units=galactic)
+    prog_pot = HernquistPotential(mass, 4 * u.pc, units=galactic)
 
     # The basic run:
-    df = FardalStreamDF()
+    df = FardalStreamDF(gala_modified=True)
     gen = MockStreamGenerator(df=df, hamiltonian=H)
-    stream1, _ = gen.run(w0, mass, dt=-1., n_steps=100)
+    stream1, _ = gen.run(w0, mass, dt=-1.0, n_steps=100)
 
     # Expected errors:
     with pytest.raises(TypeError):
-        gen.run(w0, mass.value, dt=-1., n_steps=100)
+        gen.run(w0, mass.value, dt=-1.0, n_steps=100)
 
     # With self-gravity
-    gen = MockStreamGenerator(df=df, hamiltonian=H,
-                              progenitor_potential=prog_pot)
-    stream2, _ = gen.run(w0, mass, dt=-1., n_steps=100)
+    gen = MockStreamGenerator(df=df, hamiltonian=H, progenitor_potential=prog_pot)
+    stream2, _ = gen.run(w0, mass, dt=-1.0, n_steps=100)
     assert not u.allclose(stream1.xyz, stream2.xyz)
 
     # Skipping release steps:
     gen = MockStreamGenerator(df=df, hamiltonian=H)
-    stream3, _ = gen.run(w0, mass, dt=-1., n_steps=100,
-                         release_every=4, n_particles=4)
-    assert stream3.shape == ((100//4 + 1) * 4 * 2, )
+    stream3, _ = gen.run(w0, mass, dt=-1.0, n_steps=100, release_every=4, n_particles=4)
+    assert stream3.shape == ((100 // 4 + 1) * 4 * 2,)
 
     # Custom n_particles:
     gen = MockStreamGenerator(df=df, hamiltonian=H)
     n_particles = np.random.randint(0, 4, size=101)
-    stream3, _ = gen.run(w0, mass, dt=-1., n_steps=100,
-                         release_every=1, n_particles=n_particles)
+    stream3, _ = gen.run(
+        w0, mass, dt=-1.0, n_steps=100, release_every=1, n_particles=n_particles
+    )
     assert stream3.shape[0] == 2 * n_particles.sum()
 
     # TODO: add nbody test
 
 
 @pytest.mark.parametrize(
-    'dt, nsteps, output_every, release_every, n_particles, trail',
-    list(itertools.product([1, -1], [16, 17],
-                           [1, 2], [1, 4], [1, 4], [True, False])))
-@pytest.mark.skipif(not HAS_H5PY, reason='h5py required for this test')
-def test_animate(tmpdir, dt, nsteps, output_every, release_every,
-                 n_particles, trail):
+    "dt, nsteps, output_every, release_every, n_particles, trail",
+    list(itertools.product([1, -1], [16, 17], [1, 2], [1, 4], [1, 4], [True, False])),
+)
+@pytest.mark.skipif(not HAS_H5PY, reason="h5py required for this test")
+def test_animate(tmpdir, dt, nsteps, output_every, release_every, n_particles, trail):
     import h5py
 
-    potential = NFWPotential.from_circular_velocity(v_c=0.2, r_s=20.,
-                                                    units=galactic)
+    potential = NFWPotential.from_circular_velocity(v_c=0.2, r_s=20.0, units=galactic)
     H = Hamiltonian(potential)
-    w0 = PhaseSpacePosition(pos=[15., 0., 0]*u.kpc,
-                            vel=[0, 0, 0.13]*u.kpc/u.Myr)
+    w0 = PhaseSpacePosition(
+        pos=[15.0, 0.0, 0] * u.kpc, vel=[0, 0, 0.13] * u.kpc / u.Myr
+    )
     mass = 2.5e4 * u.Msun
 
     # The basic run:
-    df = FardalStreamDF(trail=trail)
+    df = FardalStreamDF(gala_modified=True, trail=trail)
     gen = MockStreamGenerator(df=df, hamiltonian=H)
 
     filename = os.path.join(str(tmpdir), "test.hdf5")
-    stream, _ = gen.run(w0, mass, dt=dt, n_steps=nsteps,
-                        release_every=release_every,
-                        n_particles=n_particles,
-                        output_every=output_every, output_filename=filename,
-                        overwrite=True)
+    stream, _ = gen.run(
+        w0,
+        mass,
+        dt=dt,
+        n_steps=nsteps,
+        release_every=release_every,
+        n_particles=n_particles,
+        output_every=output_every,
+        output_filename=filename,
+        overwrite=True,
+    )
 
-    with h5py.File(filename, mode='r') as f:
-        stream_orbits = Orbit.from_hdf5(f['stream'])
-        nbody_orbits = Orbit.from_hdf5(f['nbody'])
+    with h5py.File(filename, mode="r") as f:
+        stream_orbits = Orbit.from_hdf5(f["stream"])
+        nbody_orbits = Orbit.from_hdf5(f["nbody"])
 
     noutput_times = 1 + nsteps // output_every
     if nsteps % output_every != 0:
         noutput_times += 1
 
     tail_n_particles = (1 + int(trail)) * n_particles
-    expected_shape = (noutput_times,
-                      tail_n_particles * (nsteps // release_every + 1))
+    expected_shape = (noutput_times, tail_n_particles * (nsteps // release_every + 1))
 
     assert stream_orbits.shape == expected_shape
     assert np.isfinite(stream_orbits[:, 0].xyz).all()
--- gala/dynamics/mockstream/tests/test_mockstream_class.py
+++ gala/dynamics/mockstream/tests/test_mockstream_class.py
@@ -13,9 +13,9 @@ def test_init():
     vxyz = np.random.random(size=(3, 100)) * u.km / u.s
     t1 = np.random.random(size=100) * u.Myr
 
-    lead_trail = np.empty(100, dtype='U1')
-    lead_trail[::2] = 't'
-    lead_trail[1::2] = 'l'
+    lead_trail = np.empty(100, dtype="U1")
+    lead_trail[::2] = "t"
+    lead_trail[1::2] = "l"
 
     stream = MockStream(xyz, vxyz)
     stream = MockStream(xyz, vxyz, release_time=t1)
@@ -31,23 +31,23 @@ def test_init():
 def test_one_burst():
     # Regression test: Tests a bug found by Helmer when putting all particles at
     # one timestep
-    import gala.potential as gp
     import gala.dynamics as gd
+    import gala.potential as gp
     from gala.dynamics import mockstream as ms
     from gala.units import galactic
 
     # NFW MW with v_c = 232.8 km/s @ r = 8.2 kpc
-    pot = gp.NFWPotential.from_circular_velocity(v_c=232.8*u.km/u.s,
-                                                 r_s=15*u.kpc,
-                                                 r_ref=8.2*u.kpc,
-                                                 units=galactic)
+    pot = gp.NFWPotential.from_circular_velocity(
+        v_c=232.8 * u.km / u.s, r_s=15 * u.kpc, r_ref=8.2 * u.kpc, units=galactic
+    )
 
     H = gp.Hamiltonian(pot)
 
-    prog_w0 = gd.PhaseSpacePosition(pos=[10, 0, 0.] * u.kpc,
-                                    vel=[0, 10, 0.] * u.km/u.s)
+    prog_w0 = gd.PhaseSpacePosition(
+        pos=[10, 0, 0.0] * u.kpc, vel=[0, 10, 0.0] * u.km / u.s
+    )
 
-    dt = 1*u.Myr
+    dt = 1 * u.Myr
     nsteps = 100
     orbit = H.integrate_orbit(prog_w0, dt=dt, n_steps=nsteps)
 
@@ -57,11 +57,11 @@ def test_one_burst():
     argmin = r[0:150].argmin()
     n_array[argmin] = 1000
 
-    df = ms.FardalStreamDF()
+    df = ms.FardalStreamDF(gala_modified=True)
 
-    dt = 1*u.Myr
-    prog_mass = 2.5E4 * u.Msun
-    prog_pot = gp.PlummerPotential(m=prog_mass, b=4*u.pc, units=galactic)
+    dt = 1 * u.Myr
+    prog_mass = 2.5e4 * u.Msun
+    prog_pot = gp.PlummerPotential(m=prog_mass, b=4 * u.pc, units=galactic)
 
     gen = ms.MockStreamGenerator(df, H, progenitor_potential=prog_pot)
 
