02a702985a4270327c04a61da9267f3597bdfe5e
Adrian Price-Whelan <adrian.prw@gmail.com>
2024-05-23 10:59:09 -0400

    remove very old astropy_init

--- gala/__init__.py
+++ gala/__init__.py
@@ -4,20 +4,6 @@ Gala.
 
 import sys
 
-__author__ = 'adrn <adrianmpw@gmail.com>'
+__author__ = "adrn <adrianmpw@gmail.com>"
 
-from ._astropy_init import *
-
-# Enforce Python version check during package import.
-# This is the same check as the one at the top of setup.py
-__minimum_python_version__ = "3.7"
-
-
-class UnsupportedPythonError(Exception):
-    pass
-
-
-if sys.version_info < tuple((int(val)
-                             for val in __minimum_python_version__.split('.'))):
-    raise UnsupportedPythonError(
-        f"packagename does not support Python < {__minimum_python_version__}")
+from ._version import version as __version__
