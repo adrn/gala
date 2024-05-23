d1adabf0987533be55632293d3a8be0a0c9ac234
Adrian Price-Whelan <adrian.prw@gmail.com>
2024-05-23 10:59:19 -0400

    skip if ffmpeg not installed

--- gala/dynamics/tests/test_plot.py
+++ gala/dynamics/tests/test_plot.py
@@ -1,11 +1,11 @@
 """ Test dynamics plotting functions """
 
-# Third-party
-import numpy as np
+import subprocess
+
 import astropy.units as u
+import numpy as np
 import pytest
 
-# Project
 from ..core import PhaseSpacePosition
 from ..orbit import Orbit
 from ..plot import plot_projections
@@ -19,40 +19,62 @@ def pytest_generate_tests(metafunc):
 
     norbits = 16
     object_list.append(
-        PhaseSpacePosition(pos=np.random.random(size=3),
-                           vel=np.random.random(size=3)))
+        PhaseSpacePosition(pos=np.random.random(size=3), vel=np.random.random(size=3))
+    )
     object_list.append(
-        PhaseSpacePosition(pos=np.random.random(size=(3, norbits)),
-                           vel=np.random.random(size=(3, norbits))))
+        PhaseSpacePosition(
+            pos=np.random.random(size=(3, norbits)),
+            vel=np.random.random(size=(3, norbits)),
+        )
+    )
     object_list.append(
-        PhaseSpacePosition(pos=np.random.random(size=(3, norbits))*u.kpc,
-                           vel=np.random.random(size=(3, norbits))*u.km/u.s))
+        PhaseSpacePosition(
+            pos=np.random.random(size=(3, norbits)) * u.kpc,
+            vel=np.random.random(size=(3, norbits)) * u.km / u.s,
+        )
+    )
 
     nsteps = 16
-    object_list.append(Orbit(pos=np.random.random(size=(3, nsteps)),
-                             vel=np.random.random(size=(3, nsteps)),
-                             t=np.linspace(0, 1, nsteps)))
-    object_list.append(Orbit(pos=np.random.random(size=(3, nsteps, 2)),
-                             vel=np.random.random(size=(3, nsteps, 2)),
-                             t=np.linspace(0, 1, nsteps)))
-    object_list.append(Orbit(pos=np.random.random(size=(3, nsteps))*u.kpc,
-                             vel=np.random.random(size=(3, nsteps))*u.km/u.s,
-                             t=np.linspace(0, 1, nsteps)*u.Myr))
+    object_list.append(
+        Orbit(
+            pos=np.random.random(size=(3, nsteps)),
+            vel=np.random.random(size=(3, nsteps)),
+            t=np.linspace(0, 1, nsteps),
+        )
+    )
+    object_list.append(
+        Orbit(
+            pos=np.random.random(size=(3, nsteps, 2)),
+            vel=np.random.random(size=(3, nsteps, 2)),
+            t=np.linspace(0, 1, nsteps),
+        )
+    )
+    object_list.append(
+        Orbit(
+            pos=np.random.random(size=(3, nsteps)) * u.kpc,
+            vel=np.random.random(size=(3, nsteps)) * u.km / u.s,
+            t=np.linspace(0, 1, nsteps) * u.Myr,
+        )
+    )
 
     # 2D
     object_list.append(
-        PhaseSpacePosition(pos=np.random.random(size=(2, norbits)),
-                           vel=np.random.random(size=(2, norbits))))
-    object_list.append(Orbit(pos=np.random.random(size=(2, nsteps)),
-                             vel=np.random.random(size=(2, nsteps)),
-                             t=np.linspace(0, 1, nsteps)))
+        PhaseSpacePosition(
+            pos=np.random.random(size=(2, norbits)),
+            vel=np.random.random(size=(2, norbits)),
+        )
+    )
+    object_list.append(
+        Orbit(
+            pos=np.random.random(size=(2, nsteps)),
+            vel=np.random.random(size=(2, nsteps)),
+            t=np.linspace(0, 1, nsteps),
+        )
+    )
 
-    test_names = [f'{obj.__class__.__name__}{i}'
-                  for i, obj in enumerate(object_list)]
+    test_names = [f"{obj.__class__.__name__}{i}" for i, obj in enumerate(object_list)]
 
-    metafunc.parametrize(['i', 'obj'],
-                         list(enumerate(object_list)),
-                         ids=test_names)
+    metafunc.parametrize(["i", "obj"], list(enumerate(object_list)), ids=test_names)
 
 
 def test_plot_projections(i, obj):
@@ -65,27 +87,39 @@ def test_plot_projections(i, obj):
     # Try with just 2D projection, and passing in a bunch of inputs...
     x = obj.xyz.value
     fig, axes = plt.subplots(1, 2)
-    fig = plot_projections(x[:2], autolim=True, axes=axes,  # noqa
-                           subplots_kwargs=dict(sharex=True),
-                           labels=['x', 'y'],
-                           plot_function=plt.plot,
-                           marker='o', linestyle='--', color='r')
+    fig = plot_projections(
+        x[:2],
+        autolim=True,
+        axes=axes,  # noqa
+        subplots_kwargs=dict(sharex=True),
+        labels=["x", "y"],
+        plot_function=plt.plot,
+        marker="o",
+        linestyle="--",
+        color="r",
+    )
 
 
 def test_animate(tmpdir, i, obj):
     if not isinstance(obj, Orbit):
         pytest.skip()
 
+    proc = subprocess.run(
+        ["ffmpeg -version"], shell=True, check=True, capture_output=True
+    )
+    if proc.returncode > 0:
+        pytest.skip(reason="ffmpeg not installed")
+
     # Try executing the method - unfortunately no test of the actual figure
     # drawn!
     fig, anim = obj.animate(segment_nsteps=3)
-    anim.save(tmpdir / f'anim{i}.mp4')
+    anim.save(tmpdir / f"anim{i}.mp4")
 
     # test hiding the timestep label
     fig, anim = obj.animate(segment_nsteps=3, show_time=False)
-    anim.save(tmpdir / f'anim{i}_no_time.mp4')
+    anim.save(tmpdir / f"anim{i}_no_time.mp4")
 
     if obj.ndim == 3:
         # Also try cylindrical, and sub-selecting components:
-        fig, anim = obj.cylindrical.animate(components=['rho', 'z'])
-        anim.save(tmpdir / f'anim{i}_cyl.mp4')
+        fig, anim = obj.cylindrical.animate(components=["rho", "z"])
+        anim.save(tmpdir / f"anim{i}_cyl.mp4")
