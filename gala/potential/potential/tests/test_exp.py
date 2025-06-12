a6a8beba83af2f2c30fbc08343df940f3569057b
Adrian Price-Whelan <583379+adrn@users.noreply.github.com>
2025-06-12 15:33:37 -0400

    change to local path for pyEXP to look for coeffs file

--- gala/potential/potential/tests/test_exp.py
+++ gala/potential/potential/tests/test_exp.py
@@ -7,15 +7,16 @@ import os
 # Third-party
 import astropy.units as u
 import pytest
+from astropy.utils.data import get_pkg_data_filename
 
-# This project
-from ...._cconfig import EXP_ENABLED
-from ..builtin import EXPPotential
-from .helpers import PotentialTestBase
-from ....units import SimulationUnitSystem
+import gala.dynamics as gd
+from gala._cconfig import EXP_ENABLED
+from gala.potential.potential.builtin import EXPPotential
+from gala.potential.potential.tests.helpers import PotentialTestBase
+from gala.units import SimulationUnitSystem
 
-EXP_CONFIG_FILE = "exp_basis.yml"
-EXP_COEFF_FILE = "exp_hernquist_coefs.h5"
+EXP_CONFIG_FILE = get_pkg_data_filename("exp_basis.yml")
+EXP_COEFF_FILE = get_pkg_data_filename("exp_hernquist_coefs.h5")
 
 # Use in CI to ensure tests aren't silently skipped
 FORCE_EXP_TEST = os.environ.get("GALA_FORCE_EXP_TEST", "0") == "1"
@@ -29,20 +30,27 @@ class TestEXP(PotentialTestBase):
     tol = 1e-3  # increase tolerance for gradient test
 
     exp_units = SimulationUnitSystem(mass=1e11 * u.Msun, length=2.5 * u.kpc, G=1)
-    w0 = [
-        *[-8, 0.0, 0.0],  #  * u.kpc,
-        *[0.0, 180, 0.0],  #  * u.km / u.s,
-    ]
+    _tmp = gd.PhaseSpacePosition(
+        pos=[-8, 0.0, 0.0] * u.kpc,
+        vel=[0.0, 180, 0.0] * u.km / u.s,
+    )
+    w0 = _tmp.w(exp_units)[:, 0]
     show_plots = True
     check_finite_at_origin = True
 
     def setup_method(self):
-        potential = EXPPotential(
+        assert os.path.exists(EXP_CONFIG_FILE), "EXP config file does not exist"
+        assert os.path.exists(EXP_COEFF_FILE), "EXP coeff file does not exist"
+
+        current_path = os.getcwd()
+        os.chdir(os.path.dirname(EXP_CONFIG_FILE))
+        self.potential = EXPPotential(
             config_file=EXP_CONFIG_FILE,
             coeff_file=EXP_COEFF_FILE,
             snapshot_index=0,
             units=self.exp_units,
         )
+        os.chdir(current_path)
         return super().setup_method()
 
     # TODO: deepcopy is not implemented for EXPPotential
