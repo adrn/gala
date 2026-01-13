16a708026a9d7ca105e045f8daa003ca56158e71
Adrian Price-Whelan <583379+adrn@users.noreply.github.com>
2026-01-13 15:26:18 -0500

    skip regression test if GSL not available

--- tests/regression/test_potential_timeinterpolated_539.py
+++ tests/regression/test_potential_timeinterpolated_539.py
@@ -2,10 +2,16 @@ import pickle
 
 import astropy.units as u
 import numpy as np
+import pytest
+from gala._cconfig import GSL_ENABLED
 
 import gala.potential as gp
 
 
+@pytest.mark.skipif(
+    not GSL_ENABLED,
+    reason="requires Gala compiled with GSL support",
+)
 def test_timeinterpolated_pickle(tmpdir):
     # construct a simple time-evolving NFW potential
     times = np.linspace(0, 10, 100) * u.Gyr
