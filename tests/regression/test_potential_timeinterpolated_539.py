cb4b3cf0b80c0babb688ed10eb3b00a0653fab1a
Adrian Price-Whelan <583379+adrn@users.noreply.github.com>
2026-01-13 14:05:30 -0500

    added a regression test of pickling

---
+++ tests/regression/test_potential_timeinterpolated_539.py
@@ -0,0 +1,20 @@
+import pickle
+
+import astropy.units as u
+import numpy as np
+
+import gala.potential as gp
+
+
+def test_timeinterpolated_pickle(tmpdir):
+    # construct a simple time-evolving NFW potential
+    times = np.linspace(0, 10, 100) * u.Gyr
+    masses = np.linspace(1e11, 5e11, 100) * u.Msun
+    pot = gp.TimeInterpolatedPotential(
+        gp.NFWPotential, times, m=masses, r_s=20 * u.kpc, units="galactic"
+    )
+    with open(tmpdir.join("time_interp_pot.pkl"), "wb") as f:
+        pickle.dump(pot, f)
+
+    with open(tmpdir.join("time_interp_pot.pkl"), "rb") as f:
+        pot = pickle.load(f)
